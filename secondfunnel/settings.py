d52e6db3d5ed71585089ac132822b3e438829383
Grigory Kruglov <grigory.kruglov@gmail.com>
2012-10-02 14:43:37 -0400

    enable admindocs

--- secondfunnel/settings.py
+++ secondfunnel/settings.py
@@ -128,8 +128,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admindocs',
 
     # external apps
     'storages',
