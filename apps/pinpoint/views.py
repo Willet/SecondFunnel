fb3257509fdadf27f3d19127456af91751205682
Grigory Kruglov <grigory.kruglov@gmail.com>
2012-10-12 16:50:41 -0400

    Stub view to load pinpoint campaign

--- apps/pinpoint/views.py
+++ apps/pinpoint/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+
+from apps.pinpoint.models import Campaign
+
+
+def campaign(request, campaign_id):
+    campaign = get_object_or_404(Campaign, pk=campaign_id)
+
+    return render_to_response('pinpoint/campaign.html', {
+        "campaign": campaign
+    }, context_instance=RequestContext(request))
