fb524cf2c2bc1d268dab59531d35085432167a6d
Terrance Niechciol <TNiechciol@gmail.com>
2012-10-16 17:45:02 -0400

    Got pinpoint page working with dummy data

--- apps/pinpoint/urls.py
+++ apps/pinpoint/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import patterns, url
 
 
 urlpatterns = patterns('apps.pinpoint.views',
+    url(r'(?P<campaign_id>\d+)/admin/$', 'pinpoint_admin'),
     url(r'(?P<campaign_id>\d+)/$', 'campaign',
         name='campaign'),
 )
--- apps/pinpoint/views.py
+++ apps/pinpoint/views.py
@@ -1,12 +1,33 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from django.http import HttpResponseRedirect, HttpResponse
 
 from apps.pinpoint.models import Campaign
 
+class Product(object):
+    url = "www.google.ca"
+    title = "Product Name"
+    desc = ["Dummy description"]
+    price = "$3.50"
+    img = ["http://dev.willet-tniechciol.appspot.com/static/pinpointPreview.png"]
 
 def campaign(request, campaign_id):
-    campaign = get_object_or_404(Campaign, pk=campaign_id)
+    campaign = None #get_object_or_404(Campaign, pk=campaign_id)
+    products = []
+    for i in range(100):
+        products.append(Product())
 
     return render_to_response('pinpoint/campaign.html', {
-        "campaign": campaign
+        "campaign": campaign,
+        "columns": range(4),
+        "products": products,
+        "featured": Product()
+    }, context_instance=RequestContext(request))
+
+def pinpoint_admin(request, campaign_id):
+    return render_to_response('pinpoint/admin.html', {
+        "storeUrl": campaign_id,
+        "pageNames": [],
+        "pages": [],
+        "pageDomain": "/"
     }, context_instance=RequestContext(request))
