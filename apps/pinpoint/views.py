a827335a92418e241ea8adf1f492468fe897c738
Terrance Niechciol <TNiechciol@gmail.com>
2012-10-17 16:08:26 -0400

    Added handler to create generic pinpoint page for any product

--- apps/pinpoint/views.py
+++ apps/pinpoint/views.py
@@ -2,25 +2,15 @@ from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 from django.http import HttpResponseRedirect, HttpResponse
 
-from apps.pinpoint.models import Campaign
-
-class Product(object):
-    url = "www.google.ca"
-    title = "Product Name"
-    desc = ["Dummy description"]
-    price = "$3.50"
-    img = ["http://dev.willet-tniechciol.appspot.com/static/pinpointPreview.png"]
+from apps.pinpoint.models import Campaign, BlockType, BlockContent
+from apps.assets.models import Product
+from django.contrib.contenttypes.models import ContentType
 
 def campaign(request, campaign_id):
     campaign = get_object_or_404(Campaign, pk=campaign_id)
-    products = []
-    for i in range(100):
-        products.append(Product())
-
     return render_to_response('pinpoint/campaign.html', {
         "campaign": campaign,
         "columns": range(4),
-        "featured": Product()
     }, context_instance=RequestContext(request))
 
 def pinpoint_admin(request, campaign_id):
@@ -29,4 +19,21 @@ def pinpoint_admin(request, campaign_id):
         "pageNames": [],
         "pages": [],
         "pageDomain": "/"
+    }, context_instance=RequestContext(request))
+
+def generic_page(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    store = product.store
+    block_type = BlockType()
+    block_type.name = "generic"
+    block = BlockContent()
+    block.block_type = block_type
+    block.content_type = ContentType.objects.get_for_model(Product)
+    block.object_id = product.id
+    return render_to_response('pinpoint/campaign.html', {
+        "campaign": {
+            "store": store,
+            },
+        "columns": range(4),
+        "content": block
     }, context_instance=RequestContext(request))
