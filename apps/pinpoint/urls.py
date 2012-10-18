067dd99181383648d2a54ed9d392d2e472746254
Grigory Kruglov <grigory.kruglov@gmail.com>
2012-10-18 11:57:00 -0400

    Very basic staff/store admin views and templates

--- apps/pinpoint/urls.py
+++ apps/pinpoint/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls.defaults import patterns, url
 
 
 urlpatterns = patterns('apps.pinpoint.views',
-    # url(r'(?P<store_id>\d+)/admin$', 'store_admin', name="store-admin"),
-    url(r'(?P<campaign_id>\d+)/$', 'campaign',
-        name='campaign'),
+    url(r'admin/$', 'admin', name='admin'),
+
+    url(r'admin/(?P<store_id>\d+)/$', 'store_admin', name='store-admin'),
+
+    url(r'(?P<campaign_id>\d+)/$', 'campaign', name='campaign'),
 )
--- apps/pinpoint/views.py
+++ apps/pinpoint/views.py
@@ -1,7 +1,29 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from django.http import Http404
 
 from apps.pinpoint.models import Campaign
+from apps.assets.models import Store
+
+
+@login_required
+def admin(request):
+    return render_to_response('pinpoint/admin_staff.html', {
+        "stores": request.user.store_set
+    }, context_instance=RequestContext(request))
+
+
+@login_required
+def store_admin(request, store_id):
+    store = get_object_or_404(Store, pk=store_id)
+
+    if not request.user in store.staff.all():
+        raise Http404
+
+    return render_to_response('pinpoint/admin_store.html', {
+        "store": store
+    }, context_instance=RequestContext(request))
 
 
 def campaign(request, campaign_id):
