9ed5deb979a70aadd6bde1259fedb380c2f15b0b
Terrance Niechciol <TNiechciol@gmail.com>
2012-10-18 13:03:40 -0400

    Moved fixtured to non-standard directory. Fixed formatting for pinpoint and pinAdmin templates.

--- apps/pinpoint/urls.py
+++ apps/pinpoint/urls.py
@@ -1,12 +1,6 @@
 from django.conf.urls.defaults import patterns, url
 
-urlpatterns = patterns('',
-    url(r'^login/$', 
-        'django.contrib.auth.views.login',
-        {'template_name': 'login/login.html'})
-)
-
-urlpatterns += patterns('apps.pinpoint.views',
+urlpatterns = patterns('apps.pinpoint.views',
     url(r'(?P<campaign_id>\d+)/admin/$', 'pinpoint_admin', name='pinpoint_admin'),
     url(r'generic/(?P<product_id>\d+)/$', 'generic_page', name='pinpoint_generic'),
     url(r'(?P<campaign_id>\d+)/$', 'campaign', name='campaign'),
