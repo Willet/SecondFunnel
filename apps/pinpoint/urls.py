7733376388c0477a080d4f44f995a80a157196f6
Terrance Niechciol <TNiechciol@gmail.com>
2012-10-18 11:35:31 -0400

    Added simple login page

--- apps/pinpoint/urls.py
+++ apps/pinpoint/urls.py
@@ -1,8 +1,14 @@
 from django.conf.urls.defaults import patterns, url
+from django.contrib.auth.views import login
 
+urlpatterns = patterns('',
+    url(r'^login/$', 
+        login, 
+        {'template_name': 'login/login.html'})
+)
 
-urlpatterns = patterns('apps.pinpoint.views',
-    url(r'(?P<campaign_id>\d+)/admin/$', 'pinpoint_admin'),
-    url(r'generic/(?P<product_id>\d+)/$', 'generic_page'),
+urlpatterns += patterns('apps.pinpoint.views',
+    url(r'(?P<campaign_id>\d+)/admin/$', 'pinpoint_admin', name='pinpoint_admin'),
+    url(r'generic/(?P<product_id>\d+)/$', 'generic_page', name='pinpoint_generic'),
     url(r'(?P<campaign_id>\d+)/$', 'campaign', name='campaign'),
 )
