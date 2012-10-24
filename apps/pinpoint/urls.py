6396680695e40a1f0303905942086c0c3358c369
Terrance Niechciol <TNiechciol@gmail.com>
2012-10-24 11:43:01 -0400

    Fixed url regexes

--- apps/pinpoint/urls.py
+++ apps/pinpoint/urls.py
@@ -1,24 +1,24 @@
 from django.conf.urls.defaults import patterns, url
 
 urlpatterns = patterns('apps.pinpoint.views',
-    url(r'admin/$', 'admin', name='admin'),
+    url(r'^admin/$', 'admin', name='admin'),
 
-    url(r'admin/(?P<store_id>\d+)/$', 'store_admin', name='store-admin'),
+    url(r'^admin/(?P<store_id>\d+)/$', 'store_admin', name='store-admin'),
 
-    url(r'admin/(?P<campaign_id>\d+)/analytics/$',
+    url(r'^admin/(?P<campaign_id>\d+)/analytics/$',
         'campaign_analytics_admin', name='analytics-campaign-admin'),
 
-    url(r'admin/(?P<store_id>\d+)/analytics/$',
+    url(r'^admin/(?P<store_id>\d+)/analytics/$',
         'store_analytics_admin', name='analytics-store-admin'),
 
-    url(r'admin/(?P<store_id>\d+)/new_campaign/$',
+    url(r'^admin/(?P<store_id>\d+)/new_campaign/$',
         'new_campaign', name='new-campaign-admin'),
 
-    url(r'admin/(?P<store_id>\d+)/new_campaign/(?P<block_type_id>\d+)/$',
+    url(r'^admin/(?P<store_id>\d+)/new_campaign/(?P<block_type_id>\d+)/$',
         'block_type_router', name='block-type-wizard'),
 
-    url(r'(?P<campaign_id>\d+)/$', 'campaign', name='campaign'),
+    url(r'^(?P<campaign_id>\d+)/$', 'campaign', name='campaign'),
 
-    url(r'generic/(?P<product_id>\d+)/$',
+    url(r'^generic/(?P<product_id>\d+)/$',
         'generic_page',name='pinpoint_generic'),
 )
