4dad6e26070368d8572ea4e90d197d7893be047b
Terrance Niechciol <TNiechciol@gmail.com>
2012-10-24 14:17:57 -0400

    Added test for creating a new campaign

--- fts/tests.py
+++ fts/tests.py
@@ -63,6 +63,71 @@ class Tests(LiveServerTestCase):
         body = driver.find_element_by_tag_name('body')
         self.assertIn('Username', body.text)
 
+    def test_login_logout(self):
+        driver = Tests.driver
+
+        # go to login page and log in
+        driver.get(self.live_server_url + "/accounts/login/")
+        driver.find_element_by_id("id_username").clear()
+        driver.find_element_by_id("id_username").send_keys("terrance")
+        driver.find_element_by_id("id_password").clear()
+        driver.find_element_by_id("id_password").send_keys("asdf")
+        driver.find_element_by_css_selector("input[type=\"submit\"]").click()
+
+        # make sure pinpoint admin page loaded
+        body = driver.find_element_by_tag_name('body')
+        self.assertIn('PinPoint Admin', body.text)
+
+        # log out
+        driver.find_element_by_link_text("Logout").click()
+
+        # make sure log in page loaded and it doesn't already say we're logged in
+        body = driver.find_element_by_tag_name('body')
+        self.assertIn('Username', body.text)
+
+    def test_make_new_campaign(self):
+        driver = Tests.driver
+
+        # go to login page and log in
+        driver.get(self.live_server_url + "/accounts/login/")
+        driver.find_element_by_id("id_username").clear()
+        driver.find_element_by_id("id_username").send_keys("terrance")
+        driver.find_element_by_id("id_password").clear()
+        driver.find_element_by_id("id_password").send_keys("asdf")
+        driver.find_element_by_css_selector("input[type=\"submit\"]").click()
+
+        driver.find_element_by_link_text("A Book Apart").click()
+
+        driver.find_element_by_link_text("New Page").click()
+
+        driver.find_element_by_css_selector(".block_type:nth-of-type(1) a").click()
+
+        driver.find_element_by_id("id_name").clear()
+        driver.find_element_by_id("id_name").send_keys("newcampaign")
+
+        driver.find_element_by_id("product_name").clear()
+        driver.find_element_by_id("product_name").send_keys("HTML5 FOR WEB DESIGNERS")
+
+        time.sleep(1)
+
+        driver.find_element_by_xpath('//html/body/ul/li/a[. = "HTML5 FOR WEB DESIGNERS"]').click()
+
+        time.sleep(1)
+
+        driver.find_element_by_css_selector("#product_images > li:nth-of-type(1) img").click()
+
+        driver.find_element_by_css_selector(".actions .button").click()
+
+        body = driver.find_element_by_tag_name('body')
+        self.assertIn('newcampaign', body.text)
+
+        # log out
+        driver.find_element_by_link_text("Logout").click()
+
+        # make sure log in page loaded and it doesn't already say we're logged in
+        body = driver.find_element_by_tag_name('body')
+        self.assertIn('Username', body.text)
+
     def test_password_change(self):
         driver = Tests.driver
 
