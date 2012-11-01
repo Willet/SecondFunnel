e87ada28616341bce38a8b7a025e5a2053880ad9
Nick Terwoord <nicholas.terwoord+code@gmail.com>
2012-11-01 14:54:44 -0400

    Setup static images, proper urls
    - Everything looks identical to the current live site, except for fonts (which should work when live)

--- apps/website/urls.py
+++ apps/website/urls.py
@@ -3,23 +3,23 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('apps.website.urls',
     url(
-        r'^(index.html)?$',
-        TemplateView.as_view(template_name="index.html"),
+        r'^$',
+        TemplateView.as_view(template_name="website/index.html"),
         name='website-index'
     ),
     url(
-        r'^about(.html)?$',
-        TemplateView.as_view(template_name="about.html"),
+        r'^about$',
+        TemplateView.as_view(template_name="website/about.html"),
         name='website-about'
     ),
     url(
-        r'^contact(.html)?$',
-        TemplateView.as_view(template_name="contact.html"),
+        r'^contact$',
+        TemplateView.as_view(template_name="website/contact.html"),
         name='website-contact'
     ),
     url(
-        r'^why(.html)?$',
-        TemplateView.as_view(template_name="why.html"),
+        r'^why$',
+        TemplateView.as_view(template_name="website/why.html"),
         name='website-why'
     ),
 )
