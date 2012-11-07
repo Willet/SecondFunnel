cdd1ff32e3f4da49548f02289534d6827aa1f220
Grigory Kruglov <grigory.kruglov@gmail.com>
2012-11-07 15:36:23 -0500

    Some celery analytics tasks

--- apps/analytics/tasks.py
+++ apps/analytics/tasks.py
@@ -1,8 +1,102 @@
-from celery import task
+import datetime
 
-from apps.analytics.settings import INSTALLED_ANALYTICS, ANALYTICS_CLASS_NAME
-import apps.analytics.storage_backends as storage_backends
+from celery import task, subtask
+from oauth2client.client import SignedJwtAssertionCredentials
 
+from django.contrib.contenttypes.models import ContentType
+
+from apps.analytics.storage_backends import GoogleAnalyticsBackend
+from apps.analytics.models import AnalyticsData, AnalyticsRecency
+from apps.assets.models import Store, Product
+
+
+@task()
+def redo_analytics():
+    """Erases cached analytics and recency data, starts update process"""
+    logger = redo_analytics.get_logger()
+
+    logger.info("Redoing analytics")
+
+    AnalyticsData.objects.all().delete()
+    AnalyticsRecency.objects.all().delete()
+
+    logger.info("Removed old analytics data")
+
+    return subtask(update_pinpoint_analytics).delay()
+
+
+@task()
+def update_pinpoint_analytics():
+    """
+    Figures out what analytics data we need,
+    fetches that and initiates calculations
+    """
+    logger = update_pinpoint_analytics.get_logger()
+    logger.info("Updating analytics data")
+
+    # define what we're requesting from Google Analytics
+    metrics = ['uniqueEvents']
+    dimensions = ['eventCategory', 'eventAction', 'eventLabel', 'date']
+    sort = ['date']
+
+    # initialize GA backend
+    ga = GoogleAnalyticsBackend()
+
+    # figure out data range that we'd need to fetch from GA
+    oldest_analytics_data = datetime.datetime.now().date()
+
+    # iterate through our stores and find the most out of sync
+    stores = Store.objects.all()
+
+    for store in stores:
+        store_type = ContentType.objects.get_for_model(Store)
+
+        try:
+            analytics_recency = AnalyticsRecency.objects.get(
+                content_type=store_type, object_id=store.id)
+
+        except AnalyticsRecency.DoesNotExist:
+            # no pinpoint analytics data was fetched for this store
+            # get everything, use the prior-to-launch date so that
+            # we're sure everything is in scope
+
+            oldest_analytics_data = datetime.date(2012, 9, 1)
+
+            # we're done checking for dates at this point
+            break
+
+        else:
+            # have recency data, see if it's older than oldest recency
+            if analytics_recency.last_fetched < oldest_analytics_data:
+                oldest_analytics_data = analytics_recency.last_fetched.date()
+
+    logger.info("We need to fetch analytics data from GA starting from %s",
+        oldest_analytics_data)
+
+    # @TODO: "coercing" to string functionality should be in the backend
+    raw_results = ga.results_iterator(
+        "%s" % oldest_analytics_data,
+        "%s" % datetime.datetime.now().date(),  # end date
+        metrics,
+        dimensions=dimensions,
+        sort=sort,
+    )
+
+    if not raw_results:
+        logger.info("Couldn't get analytics raw data. Nothing to do.")
+        return
+
+    for data_page in raw_results:
+        logger.info("Processing results page %s", data_page)
+
+        if not data_page.get('rows', []):
+            logger.info("No rows available.")
+            continue
+
+    #     data_parser.map_rows(data_page.get('rows'))
+
+    # for store in stores:
+    #     data_parser.reduce(store)
 
 @task()
 def analytics_periodic():
--- secondfunnel/settings.py
+++ secondfunnel/settings.py
@@ -216,6 +216,7 @@ BROKER_TRANSPORT_OPTIONS = {
     'queue_name_prefix': 'celery-',
 }
 
+GOOGLE_ANALYTICS_PROFILE = '63888985'
 
 try:
     from local_settings import *
