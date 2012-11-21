53dc25ae331a182c7292ce09e246b7817ddb5e4f
Grigory Kruglov <grigory.kruglov@gmail.com>
2012-11-21 16:34:35 -0500

    Rewrite analytics models. Much better now

--- apps/analytics/models.py
+++ apps/analytics/models.py
@@ -6,30 +6,11 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes import generic
 
 
-class AnalyticsBase(models.Model):
+class AnalyticsRecency(models.Model):
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
     parent = generic.GenericForeignKey('content_type', 'object_id')
 
-    class Meta:
-        abstract = True
-
-
-class AnalyticsData(AnalyticsBase):
-    date = models.DateTimeField()
-
-    key = models.CharField(max_length=255)
-    value = models.IntegerField()
-
-    class Meta:
-        verbose_name_plural = "Analytics Data"
-
-    def __unicode__(self):
-        return u"Analytics Data (key=%s, value=%s) for %s" % (
-            self.key, self.value, self.parent)
-
-
-class AnalyticsRecency(AnalyticsBase):
     last_fetched = models.DateTimeField(auto_now=True)
 
     class Meta:
@@ -38,3 +19,52 @@ class AnalyticsRecency(AnalyticsBase):
     def __unicode__(self):
         return u"Analytics Recency for %s: %s" % (
             self.parent, self.last_fetched)
+
+
+class AnalyticsSection(models.Model):
+    name = models.CharField(max_length=100)
+    categories = models.ManyToManyField("Category", blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    metrics = models.ManyToManyField("Metric", blank=True, null=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __unicode__(self):
+        return self.name
+
+    def metrics_count(self):
+        return self.metrics.all().count()
+
+
+class Metric(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255)
+
+    data = models.ManyToManyField(
+        "KVStore", related_name="data", blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
+
+    def data_count(self):
+        return self.data.all().count()
+
+
+class KVStore(models.Model):
+    content_type = models.ForeignKey(ContentType)
+    object_id = models.PositiveIntegerField()
+    parent = generic.GenericForeignKey('content_type', 'object_id')
+
+    key = models.CharField(max_length=255)
+    value = models.FloatField()
+    timestamp = models.DateTimeField()
+
+    def __unicode__(self):
+        return u"%s:%s (%s)" % (self.key, self.value, self.timestamp)
