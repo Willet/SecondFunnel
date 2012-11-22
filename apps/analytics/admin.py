8a4af200d4d9ecb8c24f0fc24debf71c50ac6210
Grigory Kruglov <grigory.kruglov@gmail.com>
2012-11-22 12:57:01 -0500

    Admin for new analytics models

--- apps/analytics/admin.py
+++ apps/analytics/admin.py
@@ -1,15 +1,6 @@
 from django.contrib import admin
 
-from apps.analytics.models import AnalyticsData, AnalyticsRecency
-
-
-class AnalyticsDataAdmin(admin.ModelAdmin):
-    list_display = [
-        'parent', 'date', 'key', 'value']
-
-    save_as = True
-
-admin.site.register(AnalyticsData, AnalyticsDataAdmin)
+from apps.analytics.models import Section, AnalyticsRecency, Category, Metric, KVStore
 
 
 class AnalyticsRecencyAdmin(admin.ModelAdmin):
@@ -17,3 +8,33 @@ class AnalyticsRecencyAdmin(admin.ModelAdmin):
         'parent', 'last_fetched']
 
 admin.site.register(AnalyticsRecency, AnalyticsRecencyAdmin)
+
+
+class AnalyticsBase(admin.ModelAdmin):
+    list_display = ('name', 'enabled')
+    list_filter = ('enabled',)
+
+
+class SectionAdmin(AnalyticsBase):
+    pass
+
+admin.site.register(Section, SectionAdmin)
+
+
+class CategoryAdmin(AnalyticsBase):
+    list_display = AnalyticsBase.list_display + ('metrics_count',)
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class MetricAdmin(AnalyticsBase):
+    list_display = AnalyticsBase.list_display + ('slug', 'data_count')
+    prepopulated_fields = {"slug": ("name",)}
+
+admin.site.register(Metric, MetricAdmin)
+
+
+class KVStoreAdmin(admin.ModelAdmin):
+    list_display = ('parent', 'key', 'value', 'timestamp')
+
+admin.site.register(KVStore, KVStoreAdmin)
