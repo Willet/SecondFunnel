ceffadf66ccbdd1bd8eef8a5e9fb9c0c16e8223b
Nick Terwoord <nicholas.terwoord+code@gmail.com>
2012-12-05 12:13:08 -0500

    Added cookie handling to session
    - Also, replaced urllib2 with HttpLib2 to handle cookies easier

--- apps/intentrank/views.py
+++ apps/intentrank/views.py
@@ -1,11 +1,9 @@
-import contextlib
 import json
-from urllib2 import urlopen, URLError
 from urllib import urlencode
 
-from django.core import serializers
 from django.http import HttpResponse
 from django.template import Context, Template
+import httplib2
 from apps.assets.models import Product
 
 # All requests are get requests at the moment
@@ -19,19 +17,32 @@ SUCCESS         = 200
 BAD_REQUEST     = 400
 DEFAULT_RESULTS = 12
 
-def process_intentrank_request(store, page, function_name, param_dict):
-    base_url = 'http://URL/store/{0}/page/{1}'.format(store, page)
+def process_intentrank_request(request, store, page, function_name,
+                               param_dict):
+    url = 'http://URL/store/{0}/page/{1}/{2}'.format(store, page, function_name)
     params   = urlencode(param_dict)
-    url      = '{0}/{1}?{2}'.format(base_url, function_name, params)
+
+    headers = {}
+    cookie = request.session.get('ir-cookie')
+    if cookie:
+        headers['Cookie'] = cookie
+
+    h = httplib2.Http()
+    try:
+        response, content = h.request(
+            url,
+            'GET',
+            headers=headers,
+            body=params
+        )
+    except Exception: # TODO: Replace with more specific error
+        return Product.objects.all(), SUCCESS # TODO: Replace with error again
+
+    if response.get('set-cookie'):
+        request.session['ir-cookie'] = response['set-cookie']
 
     try:
-        with contextlib.closing(urlopen(url)) as file:
-            json_str = file.read()
-    except (URLError, ValueError):
-        return Product.objects.all(), SUCCESS
-
-    try:
-        results = json.loads(json_str)
+        results = json.loads(content)
     except ValueError:
         return [], BAD_REQUEST
 
@@ -69,10 +80,12 @@ def get_seeds(request):
     seeds   = request.GET.get('seeds', '-1')
     results = request.GET.get('results', DEFAULT_RESULTS)
 
-    products, status = process_intentrank_request(store, page, 'getseeds', {
+    products, status = process_intentrank_request(
+        request, store, page, 'getseeds', {
         'seeds'  : seeds,
         'results': results
-    })
+        }
+    )
 
     result = products_to_template(products, page)
 
@@ -84,9 +97,11 @@ def get_results(request):
     page    = request.GET.get('campaign', '-1')
     results = request.GET.get('results', DEFAULT_RESULTS)
 
-    products, status = process_intentrank_request(store, page, 'getresults', {
-        'results': results
-    })
+    products, status = process_intentrank_request(
+        request, store, page, 'getresults', {
+            'results': results
+        }
+    )
 
     result = products_to_template(products, page)
 
@@ -98,7 +113,7 @@ def update_clickstream(request):
     page    = request.GET.get('campaign', '-1')
     product_id = request.GET.get('product_id')
 
-    _, status = process_intentrank_request(store, page, 'getseeds', {
+    _, status = process_intentrank_request(request, store, page, 'getseeds', {
         'product_id': product_id
     })
 
