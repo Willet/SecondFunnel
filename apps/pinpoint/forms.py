e038d38a1d207dcac893c816d8a2f714361bd8a9
Nick Terwoord <nicholas.terwoord+code@gmail.com>
2012-12-14 11:06:08 -0500

    Fixed Form inheritance
    - `ShopTheLookWizardForm` now inherits from `FeaturedProductWizardForm`

--- apps/pinpoint/forms.py
+++ apps/pinpoint/forms.py
@@ -60,37 +60,7 @@ class FeaturedProductWizardForm(forms.Form):
         return cleaned_data
 
 
-class ShopTheLookWizardForm(forms.Form):
-    name = forms.CharField(
-        max_length=255,
-        label="Page Name",
-        widget=forms.TextInput()
-    )
-
-    product_id = forms.CharField(
-        widget=forms.HiddenInput(),
-        )
-
-    page_changed = forms.CharField(
-        required=False,
-        widget=forms.HiddenInput(),
-        )
-
-    description = forms.CharField(
-        label="Product Description",
-        widget=forms.Textarea()
-    )
-
-    product_media_id = forms.CharField(
-        required=False,
-        widget=forms.HiddenInput(),
-        )
-
-    generic_media_id = forms.CharField(
-        required=False,
-        widget=forms.HiddenInput(),
-        )
-
+class ShopTheLookWizardForm(FeaturedProductWizardForm):
     ls_product_media_id = forms.CharField(
         required=False,
         widget=forms.HiddenInput(),
@@ -101,29 +71,10 @@ class ShopTheLookWizardForm(forms.Form):
         widget=forms.HiddenInput(),
         )
 
-    generic_media_list = forms.CharField(
-        required=False,
-        widget=forms.HiddenInput(),
-        )
-
-    campaign_id = forms.CharField(
-        required=False,
-        widget=forms.HiddenInput(),
-        )
-
     def clean(self):
         """Ensure that either custom or existing image is selected"""
 
         cleaned_data = super(ShopTheLookWizardForm, self).clean()
-
-        generic_media_id = cleaned_data.get("generic_media_id")
-        product_media_id = cleaned_data.get("product_media_id")
-
-        generic_media_exists = generic_media_id and GenericImage.objects.filter(pk=generic_media_id).exists()
-        product_media_exists = product_media_id and ProductMedia.objects.filter(pk=product_media_id).exists()
-
-        if not (generic_media_exists or product_media_exists):
-            raise forms.ValidationError("You must choose a product image.")
 
         ls_generic_media_id = cleaned_data.get("ls_generic_media_id")
         ls_product_media_id = cleaned_data.get("ls_product_media_id")
