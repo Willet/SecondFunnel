7c75e1963150cdf73d81c5c78043af131cc91360
Terrance Niechciol <TNiechciol@gmail.com>
2012-12-17 12:44:32 -0500

    Added makedocs command that runs epydoc.

---
+++ apps/pinpoint/management/commands/makedocs.py
@@ -0,0 +1,19 @@
+import os
+
+from django.core.management.base import NoArgsCommand
+
+
+class Command(NoArgsCommand):
+    args = ''
+    help = 'Creates documentation for the SecondFunnel project.'
+    folders_to_include = [
+        'apps/',
+        'secondfunnel/',
+        'scripts/',
+        'requirements/',
+        'manage.py',
+        'terrain.py',
+    ]
+
+    def handle_noargs(self, **kwargs):
+        os.system("epydoc -o doc " + reduce(lambda x, y: x + " " + y, self.folders_to_include))
