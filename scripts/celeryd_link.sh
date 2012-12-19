e9aa0de03e81fcbd06c5bdee85680c52c87be525
Grigory Kruglov <grigory.kruglov@gmail.com>
2012-12-19 05:14:56 -0800

    Improve/rewrite celery linkup bash script

--- scripts/celeryd_link.sh
+++ scripts/celeryd_link.sh
@@ -4,39 +4,43 @@
 # ============================================
 
 # Configuration file
-ln -s `pwd`/secondfunnel/settings/celeryd /etc/default/celeryd
-if [ "$?" -eq "1" ]
+if [ ! -h "/etc/default/celeryd" ]
 then
-    echo "celeryd config symlink already exists."
+    ln -s `pwd`/secondfunnel/settings/celeryd /etc/default/celeryd
+    echo "Created symlink for celery config"
+else
+    echo "Skipping creating symlink for celery config"
 fi
 
 # Init script
-ln -s `pwd`/scripts/celeryd /etc/init.d/celeryd
-if [ "$?" -eq "1" ]
+if [ ! -h "/etc/init.d/celeryd" ]
 then
-    echo "celeryd init script symlink already exists."
+    ln -s `pwd`/scripts/celeryd /etc/init.d/celeryd
+    chmod +x /etc/init.d/celeryd
+    echo "Created symlink for celery init script"
+else
+    echo "Skipping creating symlink for celery init script"
 fi
 
-# The init script needs to be executable:
-chmod +x /etc/init.d/celeryd
-
 # Make an unprivileged, non-password-enabled user and group to run celery
-useradd celery
-if [ "$?" -ne "0" ]
+useradd -M celery
+useradd_status=$?
+if [ useradd_status != 0 ]
 then
-    if [ "$?" -ne "9" ]
-    then
-        echo "useradd exited with an error other than duplicate user. useradd exit code: '$?'"
-        exit 1
-    fi
+    echo "useradd exited with an error code: '$useradd_status'"
+else
+    echo "Created unprivileged user 'celery'"
 fi
 
 # make a spot for the logs and the pid files
 mkdir -p /var/log/celery
 mkdir -p /var/run/celery
+echo "Ensured /var/log/celery and /var/run/celery exist"
 
 # set ownership of log/pid
 chown celery:celery /var/log/celery
 chown celery:celery /var/run/celery
+echo "Set ownership for log and pid dirs"
 
+echo "Done preliminary celery setup."
 exit 0
