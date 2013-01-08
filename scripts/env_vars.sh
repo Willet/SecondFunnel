b41245a56548c8cf088cc1fe3422c4d7583586b3
Nick Terwoord <nicholas.terwoord+code@gmail.com>
2013-01-07 21:31:47 -0500

    Forgot how `export` works

--- scripts/env_vars.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-ProductionBucket="elasticbeanstalk-us-east-1-056265713214"
-ProductionCache="cache-test.yz4kz2.0001.usw2.cache.amazonaws.com:11211"
-
-export ProductionBucket
-export ProductionCache
