5b3b7d9b0a144d09302acedb64a1ea3f55a78fd2
Grigory Kruglov <grigory.kruglov@gmail.com>
2013-01-14 14:18:47 -0500

    Remove duplicate twitter script loading

--- apps/pinpoint/static/pinpoint/js/tracking.js
+++ apps/pinpoint/static/pinpoint/js/tracking.js
@@ -109,14 +109,6 @@ var pinpointTracking = (function ($, window, document) {
     init = function() {
         setSocialShareVars({"default": true});
 
-        // load scripts
-        window.twttr = (function (d,s,id) {
-            var t, js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return; js=d.createElement(s); js.id=id;
-            js.src="//platform.twitter.com/widgets.js"; js.onload="pinpointTracking.registerTwitterListeners"; fjs.parentNode.insertBefore(js, fjs);
-            return window.twttr || (t = { _e: [], ready: function(f){ t._e.push(f) } });
-        }(document, "script", "twitter-wjs"));
-
         twttr.ready(function (twttr) {
             twttr.events.bind('tweet', function(event) {
                 pinpointTracking.registerEvent({
