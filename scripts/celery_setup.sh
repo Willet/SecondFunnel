69d044858abb1aca5e27c43b1fb1f1278e1f78b6
Grigory Kruglov <grigory.kruglov@gmail.com>
2013-01-21 13:36:34 -0500

    Rewrite & rename celery setup script. Change locations of daemons, log and pid files

---
+++ scripts/celery_setup.sh
@@ -0,0 +1,88 @@
+#!/bin/sh -e
+# ============================================
+#  celery_setup - Runs on the leader EC2 instance, creates celery user/group,
+#   creates required dirs and symlinks up things for celeryd and celerybeat daemons to work.
+# ============================================
+
+# Make an unprivileged, non-password-enabled user and group to run celery
+# AWS Beanstalk script runners are using 'set -e'
+# which we don't want in this particular instance. Disabled it
+set +e
+useradd -M celery
+useradd_status=$?
+if [ useradd_status != 0 ]
+then
+    echo "useradd exited with an error code: '$useradd_status'"
+else
+    echo "Created unprivileged user 'celery'"
+fi
+# Reenable -e
+set -e
+
+# make a spot for the logs and the pid files
+mkdir -p /var/log/celery
+mkdir -p /var/run/celery/bin
+mkdir /var/run/celery/config
+echo "Ensured /var/log/celery, /var/run/celery/bin and /var/run/celery/config exist"
+
+# set ownership of log/run dirs
+chown celery:celery /var/log/celery
+chown -R celery:celery /var/run/celery
+echo "Set ownership for log and pid dirs"
+
+# Symlink config files
+# CELERY COMMON
+if [ -h "/var/run/celery/config/celerycommon" ]
+then
+    rm /var/run/celery/config/celerycommon
+    echo "Deleted existing symlink for celerycommon config."
+fi
+
+ln -s `pwd`/secondfunnel/settings/celerycommon /var/run/celery/config/celerycommon
+echo "Created symlink for celerycommon config"
+
+# CELERYD
+if [ -h "/var/run/celery/config/celeryd" ]
+then
+    rm /var/run/celery/config/celeryd
+    echo "Deleted existing symlink for celeryd config."
+fi
+
+ln -s `pwd`/secondfunnel/settings/celeryd /var/run/celery/config/celeryd
+echo "Created symlink for celeryd config"
+
+# CELERYBEAT
+if [ -h "/var/run/celery/config/celerybeat" ]
+then
+    rm /var/run/celery/config/celerybeat
+    echo "Deleted existing symlink for celerybeat config."
+fi
+
+ln -s `pwd`/secondfunnel/settings/celerybeat /var/run/celery/config/celerybeat
+echo "Created symlink for celerybeat config"
+
+
+# Daemon celeryd
+if [ -h "/var/celery/run/bin/celeryd" ]
+then
+    rm /var/celery/run/bin/celeryd
+    echo "Deleted existing symlink for celeryd daemon script."
+fi
+
+ln -s `pwd`/scripts/celeryd /var/celery/run/bin/celeryd
+chmod +x /var/celery/run/bin/celeryd
+echo "Created symlink for celeryd daemon script"
+
+# Daemon celerybeat
+if [ -h "/var/celery/run/bin/celerybeat" ]
+then
+    rm /var/celery/run/bin/celerybeat
+    echo "Deleted existing symlink for celerybeat daemon script."
+fi
+
+ln -s `pwd`/scripts/celerybeat /var/celery/run/bin/celerybeat
+chmod +x /var/celery/run/bin/celerybeat
+echo "Created symlink for celerybeat daemon script"
+
+echo "Done preliminary celery setup."
+exit 0
