40e9c1a8cd1e15769905490184cb7c24f5a33f7f
Nick Terwoord <nicholas.terwoord+code@gmail.com>
2013-02-06 14:54:34 -0500

    Turning Debug off again

--- secondfunnel/settings/production.py
+++ secondfunnel/settings/production.py
@@ -4,7 +4,7 @@ from secondfunnel.errors import EnvironmentSettingsError
 if not all([AWS_STORAGE_BUCKET_NAME, MEMCACHED_LOCATION]):
     raise EnvironmentSettingsError()
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 COMPRESS_ENABLED = True
