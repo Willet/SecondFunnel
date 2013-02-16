45ec7611a8dfdbe601e1d980c4c6dbeb592fcbf9
Grigory Kruglov <grigory.kruglov@gmail.com>
2013-02-16 12:58:43 -0800

    Bah, forgot the import statement

--- scripts/notifyhipchat.py
+++ scripts/notifyhipchat.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
 
+import hipchat
+
 hip = hipchat.HipChat(token="675a844c309ec3227fa9437d022d05")
 hip.method("rooms/message", method="POST", parameters={"room_id": 115122, "from": "Mr. T", "message": "I pity the fool who doesn't deploy! New version is up! (mrt)"})
