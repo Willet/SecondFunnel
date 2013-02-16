76f4aedbc1894e2bc7f1c2b622163bb17de20295
Grigory Kruglov <grigory.kruglov@gmail.com>
2013-02-16 13:06:12 -0800

    Remote (mrt), as hip chat doesn't process these in notifications

--- scripts/notifyhipchat.py
+++ scripts/notifyhipchat.py
@@ -3,4 +3,4 @@
 import hipchat
 
 hip = hipchat.HipChat(token="675a844c309ec3227fa9437d022d05")
-hip.method("rooms/message", method="POST", parameters={"room_id": 115122, "from": "Mr. T", "message": "I pity the fool who doesn't deploy! New version is up! (mrt)"})
+hip.method("rooms/message", method="POST", parameters={"room_id": 115122, "from": "Mr. T", "message": "I pity the fool who doesn't deploy! New version is up!"})
