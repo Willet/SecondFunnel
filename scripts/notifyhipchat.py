507afc496e02701ebd1459eeb10cc80e578d4841
Grigory Kruglov <grigory.kruglov@gmail.com>
2013-02-16 12:44:36 -0800

    Notify 101010 room of deployments

---
+++ scripts/notifyhipchat.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+hip = hipchat.HipChat(token="675a844c309ec3227fa9437d022d05")
+hip.method("rooms/message", method="POST", parameters={"room_id": 115122, "from": "Mr. T", "message": "I pity the fool who doesn't deploy! New version is up!"})
