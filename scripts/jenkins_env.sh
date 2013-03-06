a038c5b373c889fd38c7ddda9234af254673b161
Grigory Kruglov <grigory.kruglov@gmail.com>
2013-03-06 03:03:18 -0800

    Jenkins build script is better placed within jenkins

--- scripts/jenkins_env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -ex
-cd $WORKSPACE
-virtualenv --system-site-packages ve
-source ./ve/bin/activate
-pip install -r requirements/dev.txt
-python manage.py migrate
-python manage.py jenkins
