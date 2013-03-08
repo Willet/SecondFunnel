5d26a6dd06e96f426fee628d41f9d4a46a9de752
Brian Lai <b2lai@uwaterloo.ca>
2013-03-08 16:03:02 -0500

    add variable fix

--- scripts/notifyhipchat.py
+++ scripts/notifyhipchat.py
@@ -2,17 +2,23 @@
 
 import hipchat
 
+
+def hipchat_broadcast(by='HipChat', message='Hello, World!', room_id=115122):
+    """Says random stuff on our Hipchat boards."""
+    hip = hipchat.HipChat(token="675a844c309ec3227fa9437d022d05")
+    hip.method("rooms/message", method="POST",
+               parameters={"room_id": room_id, "from": by, "message": message})
+
+
 where = ''
 try:
     import os
-    where = os.environ['HOSTNAME']
+    where = os.getenv('HOSTNAME') or os.environ['HOSTNAME']
     if where:
         where = ' on %s' % where
 except KeyError:
     pass
 
-hip = hipchat.HipChat(token="675a844c309ec3227fa9437d022d05")
-hip.method("rooms/message", method="POST",
-           parameters={"room_id": 115122, "from": "Mr. T%s" % where,
-                       "message": "I pity the fool who doesn't deploy!"
-                                  " New version is up!"})
+hipchat_broadcast(by="Mr. T%s" % where,
+                  message="I pity the fool who doesn't deploy!"
+                          " New version is up!")
