10e6179cb9b5b075970e97c0826272f937b44ea0
Grigory Kruglov <grigory.kruglov@gmail.com>
2013-03-17 16:56:35 -0700

    Use STATIC_URL for uploads to imageservice;

--- apps/utils/image_service/hooks.py
+++ apps/utils/image_service/hooks.py
@@ -1,5 +1,7 @@
 """Image Service model hooks"""
 
+from django.conf import settings
+
 from apps.utils.image_service.api import queue_processing
 
 
@@ -7,7 +9,12 @@ def media_saved(sender, instance, created, **kwargs):
     """Saves GenericImage URLs with those hosted by the ImageService"""
 
     if created:
-        image_url = instance.remote or instance.hosted
+        if instance.hosted:
+            image_url = "{0}{1}".format(settings.STATIC_URL, instance.hosted)
+        elif instance.remote:
+            image_url = instance.remote
+        else:
+            return
 
         instance.remote = queue_processing(image_url)
         if instance.remote:
