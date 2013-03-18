56c796e0e98b4f803a68e8e4f3fe5d712af70103
Grigory Kruglov <grigory.kruglov@gmail.com>
2013-03-18 14:29:40 -0700

    Refactor imageservice hooks a bit

--- apps/utils/image_service/hooks.py
+++ apps/utils/image_service/hooks.py
@@ -5,18 +5,22 @@ from django.conf import settings
 from apps.utils.image_service.api import queue_processing
 
 
+def process_image(instance):
+    if instance.hosted:
+        image_url = "{0}{1}".format(settings.STATIC_URL, instance.hosted)
+    elif instance.remote:
+        image_url = instance.remote
+    else:
+        return
+
+    instance.remote = queue_processing(image_url)
+    if instance.remote:
+        instance.hosted = None
+        instance.save()
+
+
 def media_saved(sender, instance, created, **kwargs):
     """Saves GenericImage URLs with those hosted by the ImageService"""
 
     if created:
-        if instance.hosted:
-            image_url = "{0}{1}".format(settings.STATIC_URL, instance.hosted)
-        elif instance.remote:
-            image_url = instance.remote
-        else:
-            return
-
-        instance.remote = queue_processing(image_url)
-        if instance.remote:
-            instance.hosted = None
-            instance.save()
+        process_image(instance)
