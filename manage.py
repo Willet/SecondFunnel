b2d69fc61a10be217cfa0157bd360eec2a22e6f0
Nick Terwoord <nicholas.terwoord+code@gmail.com>
2013-03-28 14:58:40 -0400

    Changes as a result of code review

--- manage.py
+++ manage.py
@@ -5,9 +5,12 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "secondfunnel.settings"
                                                     ".dev")
-
-    test_env = (os.getenv('DEFAULT_ENV') or os.getenv('PARAM1')) == 'TEST'
-    demo_env = (os.getenv('DEFAULT_ENV') or os.getenv('PARAM1')) == 'DEMO'
+    
+    # We use PARAM1 because it is the only one available in Elastic Beanstalk
+    # It should be replaced with a more reasonable environment variable like
+    # `ENVIRONMENT_TYPE` or something similar
+    test_env = os.getenv('PARAM1') == 'TEST'
+    demo_env = os.getenv('PARAM1') == 'DEMO'
 
     if test_env:
         os.environ['DJANGO_SETTINGS_MODULE'] = 'secondfunnel.settings.test'
