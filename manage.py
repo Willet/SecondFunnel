432524e36b0df7b092d471ae3b59018448f2ccaa
Nick Terwoord <nicholas.terwoord+code@gmail.com>
2013-03-28 14:38:59 -0400

    Added ability to distinguish between environments
    This pull request makes it possible to distinguish between `test` and `demo`
    environments when deployed to Amazon Elastic Beanstalk.
    
    Because of the (current) restrictions of Beanstalk, we leverage one of its
    unnamed environment variables (`PARAM1`) to denote if there is something
    special about this instance.
    
    This variable can be set within the Beanstalk web console in the **Container**
    tab.
    
    It is expected that `PARAM1` be set to either `TEST` or `DEMO`. If it is *not*
    set, then it is assumed that we are merely operating on a production instance,
    as expected.
    
    In the future, should Beanstalk become more flexible, we can use a more
    reasonable variable name such as `DEFAULT_ENV` or something else suitable.
    
    For now, the new settings files operate as follows:
    - `test.py` inherits from the `dev.py` file
    - `demo.py` inherits from the `production.py` file
    
    No changes have been made to handle different requirements.

--- manage.py
+++ manage.py
@@ -6,6 +6,15 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "secondfunnel.settings"
                                                     ".dev")
 
+    test_env = (os.getenv('DEFAULT_ENV') or os.getenv('PARAM1')) == 'TEST'
+    demo_env = (os.getenv('DEFAULT_ENV') or os.getenv('PARAM1')) == 'DEMO'
+
+    if test_env:
+        os.environ['DJANGO_SETTINGS_MODULE'] = 'secondfunnel.settings.test'
+
+    if demo_env:
+        os.environ['DJANGO_SETTINGS_MODULE'] = 'secondfunnel.settings.demo'
+
     from django.core.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
