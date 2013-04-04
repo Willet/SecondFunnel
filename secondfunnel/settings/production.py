46e50cf3b366d5e6786951d5f87a5d24317ab3c5
Grigory Kruglov <grigory.kruglov@gmail.com>
2013-04-04 16:27:34 -0700

    Switch to Oregon West region for SQS

--- secondfunnel/settings/production.py
+++ secondfunnel/settings/production.py
@@ -62,7 +62,7 @@ GOOGLE_SERVICE_ACCOUNT = '248578306350@developer.gserviceaccount.com'
 
 BROKER_URL = 'sqs://%s:%s@' % (AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
 BROKER_TRANSPORT_OPTIONS = {
-    'region': 'us-west-1',
+    'region': 'us-west-2',
     'visibility_timeout': 30,
     'polling_interval': 1,
     'queue_name_prefix': 'celery-',
