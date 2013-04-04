dfc8df3d6a4225bb105d552bd94d6d7b43393bbd
Grigory Kruglov <grigory.kruglov@gmail.com>
2013-04-04 16:42:11 -0700

    User roles decorator and execute() in fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,18 +1,24 @@
 """
 Automated deployment tasks
 """
-from fabric.api import hosts, run, cd
+from fabric.api import roles, run, cd, execute
 
-CELERY_WORKERS = ['ec2-54-244-159-249.us-west-2.compute.amazonaws.com']
+env.roledefs = {
+    'celery': ['ec2-user@ec2-54-244-159-249.us-west-2.compute.amazonaws.com'],
+}
 
-@hosts(CELERY_WORKERS)
+@roles('celery')
 def deploy_celery():
     """Deploys new code to celery workers and restarts them"""
 
     project_dir = "/home/ec2-user/pinpoint/env/SecondFunnel"
 
     with cd(project_dir):
-        run("git checkout master")
-        run("git pull origin master")
+        run("git checkout celeryservices")
+        run("git pull origin celeryservices")
 
     run("/etc/init.d/supervisord restart")
+
+def deploy():
+    """Runs all of our deployment tasks"""
+    execute(deploy_celery)
