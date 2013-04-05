fc1cc9698c3feac7d916c63189e90b5cb6389db5
Grigory Kruglov <grigory.kruglov@gmail.com>
2013-04-04 20:18:36 -0700

    Add "enable production" stuff to "run_as_prod" script

--- scripts/run_as_prod.sh
+++ scripts/run_as_prod.sh
@@ -4,14 +4,19 @@
 # use: /path/to/env_run.sh <command>
 # e.g.: /path/to/env_run.sh python main.py --arg-one --arg-two
 
+export DJANGO_SETTINGS_MODULE="secondfunnel.settings.production"
+export RDS_PORT="3306"
+export RDS_USERNAME="ebroot"
+export RDS_PASSWORD="braTh9hU"
+export RDS_DB_NAME="ebdb"
+export RDS_HOSTNAME="aa1tf2ugh38h7kf.c9ahaxgbbpcf.us-west-2.rds.amazonaws.com"
+
 WORKING_DIR=../
 ACTIVATE_PATH=../bin/activate
-ACTIVATE_PROD=scripts/enable_prod
 MY_PATH="`dirname \"$0\"`"              # relative
 MY_PATH="`( cd \"$MY_PATH\" && pwd )`"  # absolutized and normalized
 
 cd ${MY_PATH}
 cd ${WORKING_DIR}
-source ${ACTIVATE_PROD}
 source ${ACTIVATE_PATH}
 exec $@
