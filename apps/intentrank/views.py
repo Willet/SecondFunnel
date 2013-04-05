04724cdb98cca3dbca1516090ee96d518e5c1195
Grigory Kruglov <grigory.kruglov@gmail.com>
2013-04-05 16:22:48 -0700

    Use jsonp for intentrank requests; cleanup intentrank/views a bit;

--- apps/intentrank/views.py
+++ apps/intentrank/views.py
@@ -1,6 +1,5 @@
 import httplib2
 import json
-import math
 import random
 
 from urllib import urlencode
@@ -12,7 +11,10 @@ from django.http import HttpResponse
 from django.template import Context, loader, TemplateDoesNotExist
 from mock import MagicMock
 
-from apps.assets.models import Product, Store
+from apps.assets.models import Product
+
+from apps.intentrank.utils import (random_products, VideoCookie,
+    video_probability_function, ajax_jsonp)
 
 # All requests are get requests at the moment
 # URL to IntentRank looks something like this:
@@ -23,65 +25,10 @@ from apps.pinpoint.models import Campaign
 from secondfunnel.settings.common import INTENTRANK_BASE_URL
 
 SUCCESS = 200
-REDIRECT = 300
 FAIL400 = 400
-SUCCESS_STATUSES = xrange(SUCCESS, REDIRECT)
+SUCCESS_STATUSES = xrange(SUCCESS, 300)
 DEFAULT_RESULTS  = 12
 MAX_BLOCKS_BEFORE_VIDEO = 50
-
-class VideoCookie(object):
-    def __init__(self):
-        self.blocks_since_last = 0
-        self.videos_already_shown = []
-
-    def add_video(self, video_id):
-        self.videos_already_shown.append(video_id)
-        self.blocks_since_last = 0
-        return self
-
-    def add_blocks(self, blocks):
-        self.blocks_since_last += blocks
-        return self
-
-    def is_empty(self):
-        return self.blocks_since_last == 0 \
-           and self.videos_already_shown == []
-
-    def __str__(self):
-        return str(self.blocks_since_last) + ", " + str(self.videos_already_shown)
-
-
-def video_probability_function(x, m):
-    if x <= 0:
-        return 0
-    elif x >= m:
-        return 1
-    else:
-        return 1 - (math.log(m - x) / math.log(m))
-
-
-def random_products(store, param_dict, id_only=True):
-    """Returns a list of random product ids, as would be returned by IR.
-
-    Only used in development; not in production"""
-    store_id = Store.objects.get(slug__exact=store)
-    num_results = int(param_dict.get('results', DEFAULT_RESULTS))
-    results = []
-
-    while len(results) < num_results:
-        query_set = Product.objects.select_related()\
-                           .prefetch_related('lifestyleImages')\
-                           .annotate(num_images=Count('media'))\
-                           .filter(store_id__exact=store_id,
-                                   num_images__gt=0)[:num_results]
-        results_partial = list(query_set)
-
-        if id_only:
-            results_partial = map(lambda x: x.id, results_partial)
-
-        results.extend(results_partial)
-
-    return results[:num_results]
 
 
 def send_intentrank_request(request, url, method='GET', headers=None,
@@ -221,7 +168,8 @@ def get_json_data(request, products, campaign_id, seeds=None):
     if not video_cookie:
         video_cookie = request.session['pinpoint-video-cookie'] = VideoCookie()
 
-    videos = campaign.store.videos.exclude(video_id__in=video_cookie.videos_already_shown)
+    videos = campaign.store.videos.exclude(
+        video_id__in=video_cookie.videos_already_shown)
 
     # if this is the first batch of results, or the random amount is under the
     # curve of the probability function, then add a video
@@ -265,7 +213,8 @@ def get_json_data(request, products, campaign_id, seeds=None):
             related_products = item.tagged_products.all()
             if related_products:
                 json_content.update({
-                    'related-products': [x.data(raw=True) for x in related_products]
+                    'related-products': [x.data(raw=True)
+                        for x in related_products]
                 })
 
             results.insert(randrange(len(results) + 1), json_content)
@@ -286,6 +235,7 @@ def get_seeds(request, **kwargs):
     seeds   = kwargs.get('seeds', request.GET.get('seeds', '-1'))
     num_results = kwargs.get('results', request.GET.get('results',
                                                         DEFAULT_RESULTS))
+    callback = kwargs.get('callback', request.GET.get('callback', 'fn'))
 
     request.session['pinpoint-video-cookie'] = VideoCookie()
 
@@ -305,8 +255,8 @@ def get_seeds(request, **kwargs):
     if kwargs.get('raw', False):
         return result
     else:
-        return HttpResponse(json.dumps(result), mimetype='application/json',
-                            status=status)
+        return ajax_jsonp(result, callback, status=status)
+
 
 def get_results(request, **kwargs):
     """kwargs overrides request values when provided.
@@ -319,6 +269,7 @@ def get_results(request, **kwargs):
     seeds   = kwargs.get('seeds', request.GET.get('seeds', '-1'))
     num_results = kwargs.get('results', request.GET.get('results',
                                                         DEFAULT_RESULTS))
+    callback = kwargs.get('callback', request.GET.get('callback', 'fn'))
 
     results, status = process_intentrank_request(
         request, store, page, 'getresults', {
@@ -340,8 +291,7 @@ def get_results(request, **kwargs):
     if kwargs.get('raw', False):
         return result
     else:
-        return HttpResponse(json.dumps(result), mimetype='application/json',
-                            status=status)
+        return ajax_jsonp(result, callback, status=status)
 
 
 def update_clickstream(request):
@@ -349,9 +299,11 @@ def update_clickstream(request):
     store   = request.GET.get('store', '-1')
     page    = request.GET.get('campaign', '-1')
     product_id = request.GET.get('product_id')
+    callback = request.GET.get('callback', 'fn')
 
-    results, status = process_intentrank_request(request, store, page, 'updateclickstream', {
-        'productid': product_id
+    results, status = process_intentrank_request(
+        request, store, page, 'updateclickstream', {
+            'productid': product_id
     })
 
     if status in SUCCESS_STATUSES:
@@ -360,8 +312,7 @@ def update_clickstream(request):
     else:
         result = results
 
-    return HttpResponse(json.dumps(result), mimetype='application/json',
-                        status=status)
+    return ajax_jsonp(result, callback, status=status)
 
 
 def invalidate_session(request):
