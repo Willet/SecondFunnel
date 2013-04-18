43b2ee10bc47f8cc50027d7fe72b8e2aefbb9c1c
Grigory Kruglov <grigory.kruglov@gmail.com>
2013-04-17 18:37:28 -0700

    Create a notion of 'cluster name' for celery clusters. Bunch of changes to support controlling clusters by name.

--- fabfile.py
+++ fabfile.py
@@ -22,14 +22,19 @@ def flatten_reservations(reservations):
 
     return [i for i in list(chain)]
 
-def get_celery_workers():
+def get_celery_workers(cluster_type):
+    """Gets Celery workers belonging to specified cluster type"""
     ec2 = get_ec2_conn()
-    res = ec2.get_all_instances(filters={'tag:Name': 'CeleryWorker'})
+    res = ec2.get_all_instances(filters={
+        'tag:Name': 'CeleryWorker',
+        'tag:ClusterType': cluster_type
+    })
 
     # we only want running instances
     return [i for i in flatten_reservations(res) if i.state in ['running', 'pending']]
 
-def launch_celery_worker(branch):
+def launch_celery_worker(cluster_type, branch):
+    """Launches a new celery worker, adding it to an appropriate cluster"""
     ec2 = get_ec2_conn()
 
     # our celery worker AMI
@@ -56,11 +61,14 @@ def launch_celery_worker(branch):
 
     if status == "running":
         print green("Instance is running. Tagging it...")
-        ec2.create_tags([launched_instance.id], {"Name": "CeleryWorker"})
+        ec2.create_tags([launched_instance.id], {
+            "Name": "CeleryWorker",
+            "ClusterType": cluster_type
+        })
 
         # with settings(hide('stdout', 'commands')):
         # execute(deploy_celery, branch, hosts=[launched_instance.public_dns_name])
-        print yellow("Not launching celery bootstrap at this point, as it's being buggy.")
+        print yellow("Not launching celery bootstrap for branch '{0}' at this point, as it's being buggy.".format(branch))
 
         print green("Finalized new celery instance: {0}".format(
             launched_instance.id))
@@ -85,24 +93,32 @@ def stop_celery_services():
     while not "no such file" in result:
         result = run("/etc/init.d/supervisord status")
 
-def start_celery_services(environment):
+def start_celery_services(cluster_type):
     """Starts celery services and waits until they're confirmed running"""
     print green("Starting worker services...")
-    run("/etc/init.d/supervisord start {0}".format(environment))
+    run("/etc/init.d/supervisord start {0}".format(cluster_type))
 
     # wait until supervisord started celery worker and/or beat
     result = run("/etc/init.d/supervisord status")
     while "STARTING" in result:
         result = run("/etc/init.d/supervisord status")
 
-def celery_cluster_size(number_of_instances=None, branch='master'):
-    celery_workers = get_celery_workers()
+def celery_cluster_size(cluster_type, number_of_instances=None, branch='master'):
+    """Manages celery cluster of type `cluster_type`.
+    Usage:
+    Get cluster size: celery_cluster_size(type_name)
+    Set number of instances and deploy master branch onto them:
+    celery_cluster_size(type_name, number_of_instances=5, branch='master')
+
+    Type options: test or production.
+    """
+    celery_workers = get_celery_workers(cluster_type)
 
     current_size = len(celery_workers)
     if number_of_instances:
         number_of_instances = int(number_of_instances)
 
-    print green("Current celery cluster size: {0}".format(current_size))
+    print green("'{0}' celery cluster size: {1}".format(cluster_type, current_size))
 
     if number_of_instances and number_of_instances != current_size or number_of_instances == 0:
         print green("Adjusting cluster size to {0}".format(
@@ -111,7 +127,7 @@ def celery_cluster_size(number_of_instances=None, branch='master'):
         if number_of_instances > current_size:
             public_dns_names = []
             for i in range(number_of_instances - current_size):
-                new_dns = launch_celery_worker(branch)
+                new_dns = launch_celery_worker(cluster_type, branch)
                 public_dns_names.append(new_dns)
 
             # env.user is ignored w/ hosts passed in from CLI, so add it in
@@ -119,8 +135,8 @@ def celery_cluster_size(number_of_instances=None, branch='master'):
                                 public_dns_names]
             public_dns_names = ";".join(public_dns_names)
 
-            print yellow('Now run the following: fab deploy_celery:{0},hosts="{1}"'.format(
-                branch, public_dns_names))
+            print yellow('Now run the following: fab deploy_celery:{0},{1},hosts="{2}"'.format(
+                cluster_type, branch, public_dns_names))
 
         else:
             workers_to_terminate = celery_workers[:current_size - number_of_instances]
@@ -137,11 +153,18 @@ def celery_cluster_size(number_of_instances=None, branch='master'):
         print green("Finished adjusting celery cluster size to {0}".format(
             number_of_instances))
 
-def deploy_celery(branch, environment):
+def deploy_celery(cluster_type, branch):
     """Deploys new code to celery workers and restarts them"""
     print
-    print green("Deploying '{0}' to celery worker within {1} environment".format(
-        branch, environment))
+    # we only support two cluster types
+    # (since their names are linked to our environmental vars on workers
+    supported_cluster_types = ["test", "production"]
+    if cluster_type not in supported_cluster_types:
+        print red("Received cluster type: {0}. It must be one of: {1}".format(
+            cluster_type, supported_cluster_types))
+
+    print green("Deploying '{0}' to celery worker in {1} cluster".format(
+        branch, cluster_type))
 
     env_path = "/home/ec2-user/pinpoint/env"
     project_path = "{0}/SecondFunnel".format(env_path)
@@ -166,21 +189,21 @@ def deploy_celery(branch, environment):
         sudo("chmod 0755 /etc/init.d/supervisord")
 
     stop_celery_services()
-    start_celery_services(environment)
+    start_celery_services(cluster_type)
 
     print green("Success! Celery worker is running latest code from '{0}'"
     .format(branch))
 
 
-def deploy(branch='master', environment='test'):
+def deploy(branch='master', cluster_type='test'):
     """Runs all of our deployment tasks"""
 
     print green("Obtaining a list of celery workers...")
 
-    celery_workers = get_celery_workers()
+    celery_workers = get_celery_workers(cluster_type)
     celery_workers_dns = [i.public_dns_name for i in celery_workers]
 
     print yellow("Celery Worker instances: {0}".format(celery_workers_dns))
 
     with settings(hide('stdout', 'commands')):
-        execute(deploy_celery, branch, environment, hosts=celery_workers_dns)
+        execute(deploy_celery, cluster_type, branch, hosts=celery_workers_dns)
