1e83a181eff653611e0a91c13c0ffc82fb6c1e68
Grigory Kruglov <grigory.kruglov@gmail.com>
2013-04-18 15:56:35 -0700

    Make S3 links fully public; Cleanup unused stuff;

--- secondfunnel/context_processors.py
+++
@@ -1,7 +0,0 @@
-from django.conf import settings
-
-def expose_settings(request):
-    if settings.EXPOSED_SETTINGS is not None:
-        return settings.EXPOSED_SETTINGS
-    else:
-        return {}
