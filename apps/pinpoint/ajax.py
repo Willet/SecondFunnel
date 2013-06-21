5fef06e20b04bbf4d67e8bd4e46496384c0c6fe4
hkpeprah <HayfordJunior@hotmail.com>
2013-06-21 13:42:04 -0400

    Fixes.

--- apps/pinpoint/ajax.py
+++ apps/pinpoint/ajax.py
@@ -3,6 +3,8 @@ from django.core.files.base import ContentFile
 from django.core.files.images import get_image_dimensions
 from django.forms import ImageField, ValidationError
 from django.views.decorators.http import require_POST
+from django.conf import settings
+from django.http import HttpResponse
 
 from apps.assets.models import GenericImage
 from apps.pinpoint.models import Campaign
@@ -98,7 +100,7 @@ def upload_image(request):
         fileName = request.GET.get('qqfile', None)
 
         if None in (fileSize, fileName):
-            return ajax_error({'error': "emptyError")
+            return ajax_error({'error': "emptyError"})
 
         # read the file content, if it is not read when the request is multi part then the client get an error
         fileContent = uploaded(fileSize)
@@ -117,23 +119,27 @@ def upload_image(request):
 def ajax_upload_image(request):
     try:
         media = upload_image(request)
-    except:
-        return ajax_error()
+        media_id = media.id
+        media_url = media.get_url()
+    except Exception as e:
+        if isinstance(media, HttpResponse):
+            return media
+        return ajax_error({'error': str(e)})
 
     return ajax_success({
-        'media_id': media.id,
-        'url': media.get_url()
+        'media_id': media_id,
+        'url': media_url
     })
 
 
 def valid_dimensions( product_image ):
     """
     Ensures that the dimensions of the image are atleast the minimum dimensions
-    for an image.  Returns true if valid, otherwise raises an ajax_error.
+    for an image.  Returns true if valid, otherwise returns false.
     @return bool
     """
     dimensions = get_image_dimensions( product_image )
-    if  dimensions < (480, 1):
+    if  dimensions < (settings.MIN_MEDIA_WIDTH, settings.MIN_MEDIA_HEIGHT):
         # image has to be able to fit a wide block
         return False
     return True
