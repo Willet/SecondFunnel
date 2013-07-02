6eb8d83de9686b50e6ab1f31f61174d470ffa756
hkpeprah <HayfordJunior@hotmail.com>
2013-07-02 12:01:03 -0400

    Updated documentation and fixes.

--- apps/analytics/models.py
+++ apps/analytics/models.py
@@ -7,6 +7,12 @@ from django.contrib.contenttypes import generic
 
 
 class AnalyticsRecency(models.Model):
+    """
+    @ivar content_type
+    @ivar object_id
+    @ivar parent
+    @ivar last_fetched: When the analytics data was last fetched.
+    """
     content_type = models.ForeignKey(ContentType)
     object_id = models.PositiveIntegerField()
     parent = generic.GenericForeignKey('content_type', 'object_id')
@@ -22,6 +28,11 @@ class AnalyticsRecency(models.Model):
 
 
 class AnalyticsBase(models.Model):
+    """
+    @ivar name: Name of the analytic.
+    @ivar slug: Valid url generated from name.
+    @ivar enabled: Whether or not this analytic is enabled.
+    """
     name = models.CharField(max_length=255)
     slug = models.CharField(max_length=255)
     enabled = models.BooleanField(default=True)
@@ -31,7 +42,14 @@ class AnalyticsBase(models.Model):
 
 
 class Category(AnalyticsBase):
-    """Types of events, e.g. Sharing, Engagement, Awareness."""
+    """
+    Different types of analytic events, e.g. Sharing, Engagement, Awareness.  Inherits
+    from the AnalyticBase.
+
+    @ivar metrics: Different metric model that are recorded.
+    @ivar order: The object in which this model is returned when satisfying queries.
+    @ivar default: Whether or not this Category is the default category for analtyics.
+    """
     metrics = models.ManyToManyField("Metric", through="CategoryHasMetric",
                                      blank=True, null=True)
     order = models.PositiveIntegerField(default=0)
@@ -52,7 +70,10 @@ class Category(AnalyticsBase):
 
 
 class Metric(AnalyticsBase):
-    """Stores the number of data points available for category metric."""
+    """Stores the number of data points available for category metric.
+    
+    @ivar data: Different types of interaction objects.
+    """
     data = models.ManyToManyField(
         "KVStore", related_name="data", blank=True, null=True)
 
@@ -64,6 +85,13 @@ class Metric(AnalyticsBase):
 
 
 class CategoryHasMetric(models.Model):
+    """
+    @ivar category: The associated category model.
+    @ivar metric: The associated metric model.
+    @ivar order: The order in which this model is returned when satisfying queries.
+    @ivar display: Whether or not this model is rendered on analytic template pages.
+    @ivar is_meta
+    """
     category = models.ForeignKey(Category)
     metric = models.ForeignKey(Metric)
 
@@ -77,10 +105,19 @@ class CategoryHasMetric(models.Model):
 
 
 class KVStore(models.Model):
-    """TODO: document this model.
-
-    AFAIK KVStore objects are aggregated and deleted every
-    some time by celery.
+    """KVStore(Key-Value Store) is a data-pairing created by celery to record metrics
+    AFAIK KVStore objects are aggregated and deleted every some time by celery.
+    
+    @ivar content_type
+    @ivar object_id: Id of a Metric object. 
+    @ivar parent: Foreign key to a Metric object.
+    @ivar target_type
+    @ivar target_id: Id of a Category object.
+    @ivar target: Foreign key to a Category object.
+    @ivar key: The name corresponding to what type of analytic's data this is.
+    @ivar value: The data being recorded.
+    @ivar meta: The name corresponding to what source this data is from.
+    @ivar timestamp: When the pairing was created.
     """
     content_type = models.ForeignKey(ContentType,
         related_name='analytics_data')
@@ -110,6 +147,8 @@ class SharedStorage(models.Model):
     Amazon SQS has a size limit for its messages (64kb). At times we need to pass messages
     that are above that limit in size. In such cases, for convenience, we could use a "shared memory"
     approach - as opposed to breaking down and parallelizing message passing/processing, for example.
+
+    @ivar data: Our message.
     """
     data = models.TextField()
 
