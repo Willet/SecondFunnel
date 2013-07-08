0c2389a109f6f79014e147e32afce4806c17a233
Nick Terwoord <nicholas.terwoord+code@gmail.com>
2013-07-08 13:48:47 -0400

    PR Changes
    - Removed unused function

--- apps/utils/__init__.py
+++ apps/utils/__init__.py
@@ -2,9 +2,3 @@ __author__ = 'Nicholas Terwoord'
 
 def noop(*args, **kwargs):
     return None
-
-def safe_getattr(instance, key, default=None):
-    try:
-        return getattr(instance, key, default)
-    except:
-        return default
