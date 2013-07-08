51bf5ce0a67bfbc83fcab141bb3676aefc4c0719
hkpeprah <HayfordJunior@hotmail.com>
2013-07-08 15:07:42 -0400

    Fix for wrong spinner and related broken images.

--- apps/pinpoint/static/pinpoint/js/pages.js
+++ apps/pinpoint/static/pinpoint/js/pages.js
@@ -725,7 +725,7 @@ PAGES.full = (function (me, mediator) {
                     // Create a spinner image that can be used to indicate a block is loading.
                     $spinner = $('<img/>', {
                         'class': "image-loading-spinner",
-                        'style': "padding-top:100px; padding-bottom:100px; width:32px; height:32px; position:relative; left:50%;",
+                        'style': "padding-top:100px; padding-bottom:100px; width:32px !important; height:32px; position:relative; left:50%;",
                         'src': "https://s3.amazonaws.com/elasticbeanstalk-us-east-1-056265713214/images/ajax-spinner.gif"
                     });
 
@@ -748,11 +748,19 @@ PAGES.full = (function (me, mediator) {
                                 // than it's reload logic.
                                 $(window).resize();
                             }
-                        }).error(function() {
-                            $(this).parent().parent().remove();
                         });
                     });
                 }
+
+                $images.error(function(){
+                    var instance = $(this), 
+                        isAdded = setInterval(function(){
+                            if ($.contains(document.documentElement, instance[0])) {
+                                instance.parent().parent().remove();
+                                clearInterval(isAdded);
+                            }
+                        }, 500);
+                });
 
                 if ($elem.find('.lifestyle').length > 0) {
                     $elem.addClass('wide');
