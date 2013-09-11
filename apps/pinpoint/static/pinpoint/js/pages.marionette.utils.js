aca206fa44c785020b7e63aa13cfbb75461a5e72
Brian Lai <b2lai@uwaterloo.ca>
2013-09-11 15:30:52 -0400

    this is magical

--- apps/pinpoint/static/pinpoint/js/pages.marionette.utils.js
+++ apps/pinpoint/static/pinpoint/js/pages.marionette.utils.js
@@ -6,36 +6,64 @@ SecondFunnel.module("utils", function (utils, SecondFunnel) {
         regions = {},
         regionWidgets = {};
 
+    /**
+     * Cleans obviously invalid UI strings.
+     *
+     * @param {string} str
+     * @param {undefined} opts
+     * @returns {string}
+     */
     utils.safeString = function (str, opts) {
-        // trims the string and checks if it's just 'None'.
-        // more checks to come later.
         return $.trim(str).replace(/^(None|undefined|[Ff]alse|0)$/, '');
     };
 
+    /**
+     * add a predefined UI component implemented as a region.
+     * @param {string} name of the widget.
+     *                 name must be unique.
+     *                 if registerWidget is called with an existing
+     *                  widget, the old one is overwritten.
+     *
+     * @param {string} selector a jquery selector,
+     * @param {function} functionality the widget function.
+     * @returns true
+     */
     utils.registerWidget = function (name, selector, functionality) {
-        // add a predefined UI component implemented as a region.
-        // name must be unique. if registerWidget is called with an existing
-        // widget, the old one is overwritten.
         regions[name] = selector;
         regionWidgets[name] = functionality;
         broadcast('widgetRegistered', name, selector, functionality,
             regions, regionWidgets);
+        return true;  // success
     };
 
+    /**
+     * allows designers to add a custom (tile) class.
+     * if class name is already taken, the original class is overwritten.
+     *
+     * @param {string} name
+     * @param {object} defn
+     * @returns defn
+     */
     utils.addClass = function (name, defn) {
-        // allows designers to add a custom (tile) class.
-        // if class name is already taken, the original class is overwritten.
         SecondFunnel.classRegistry = SecondFunnel.classRegistry || {};
-        SecondFunnel.classRegistry[name] = defn;
+        SecondFunnel.classRegistry[_.capitalize(name)] = defn;
         broadcast('classAdded', name, defn);
         return defn;
     };
 
+    /**
+     * returns a class in the window scope and class registry,
+     *  or defaultClass if nothing else is found.
+     *
+     * @param {string} typeName e.g. 'Tile', 'TileView'
+     * @param {string} prefix e.g. 'Video'
+     * @param {object} defaultClass e.g. TileView
+     * @returns {object}|defaultClass
+     */
     utils.findClass = function (typeName, prefix, defaultClass) {
-        // returns a class in the window scope and class registry.
-        // default: optional. returns it if nothing else is found.
         var FoundClass,
-            targetClassName = _.capitalize(prefix) + _.capitalize(typeName);
+            targetClassName = _.capitalize(prefix || '') +
+                              _.capitalize(typeName || '');
         if (SecondFunnel.classRegistry[targetClassName] !== undefined) {
             // if designers want to define a new tile view, they must
             // let SecondFunnel know of its existence.
@@ -52,9 +80,14 @@ SecondFunnel.module("utils", function (utils, SecondFunnel) {
         return FoundClass;
     };
 
+    /**
+     * process widget regions.
+     * each widget function receives args (the view, the $element, option alias).
+     * TODO: tests
+     *
+     * @param {View} viewObject
+     */
     utils.runWidgets = function (viewObject) {
-        // process widget regions.
-        // each widget function receives args (the view, the $element, option alias).
         var self = viewObject;
 
         // process itself (if it is a view)
@@ -77,16 +110,23 @@ SecondFunnel.module("utils", function (utils, SecondFunnel) {
         });
     };
 
+    /**
+     * returns a url that is either
+     *    - the url, if it is not an image service url, or
+     *    - an image url pointing to one that is at least as wide as
+     *      minWidth, or
+     *    - an image url pointing to one that is at most as wide as
+     *      the window width, or
+     *    - if minWidth is ridiculously large, master.jpg.
+     * if scalePolicy is "max", then the image served is always smaller
+     *    than requested.
+     *
+     * @param {string} url
+     * @param {int} minWidth
+     * @param {min|max|undefined} scalePolicy
+     * @returns {string}
+     */
     utils.pickImageSize = function (url, minWidth, scalePolicy) {
-        // returns a url that is either
-        //   - the url, if it is not an image service url, or
-        //   - an image url pointing to one that is at least as wide as
-        //     minWidth, or
-        //   - an image url pointing to one that is at most as wide as
-        //     the window width, or
-        //   - if minWidth is ridiculously large, master.jpg.
-        // if scalePolicy is "max", then the image served is always smaller
-        //   than requested.
         var i,
             prevKey = 'pico',
             maxLogicalSize = Math.min($window.width(), $window.height()),
--- apps/pinpoint/static/pinpoint/js/pages.marionette.utils.spec.js
+++ apps/pinpoint/static/pinpoint/js/pages.marionette.utils.spec.js
@@ -41,5 +41,122 @@ describe("utils", function () {
             expect(module.safeString(" 0 ")).toEqual('');
             expect(module.safeString(" 0")).toEqual('');
         });
+
+        it("should not do anything when keywords are present, but between words", function () {
+            expect(module.safeString("None was lost")).toEqual("None was lost");
+            expect(module.safeString("There was None")).toEqual("There was None");
+            expect(module.safeString("True  False Maybe")).toEqual("True  False Maybe");
+            expect(module.safeString("$50.00")).toEqual("$50.00");
+        });
+    });
+
+    describe('registerWidget', function () {
+        it("should return true if it succeeds", function () {
+            expect(module.registerWidget('derp', '', function () {})).toEqual(true);
+        });
+
+        it("should return anything but true if it fails", function () {
+            // this call never fails
+        });
+    });
+
+    describe('addClass', function () {
+        var defn = function () { return 5; };
+
+        it("should return its own definition if it succeeds", function () {
+            expect(module.addClass('derp', defn)).toEqual(defn);
+        });
+
+        it("should actually add something", function () {
+            expect(app.classRegistry.Derp).toEqual(defn);
+        });
+
+        it("should return anything but the definition if it fails", function () {
+            // this call never fails
+        });
+    });
+
+    describe('findClass', function () {
+        var app = SecondFunnel,
+            defn = function () { return 5; },
+            defn2 = function () { return 6; };
+
+        app.utils.addClass('derp', defn);
+        app.utils.addClass('derp2', defn2);
+
+        it("should return the correct class", function () {
+            expect(app.utils.findClass('derp')()).toEqual(5);
+            expect(app.utils.findClass('derp2')()).toEqual(6);
+        });
+    });
+
+    describe('pickImageSize', function () {
+        it("should do nothing on nothing", function () {
+            expect(module.pickImageSize()).toEqual(undefined);
+        });
+
+        it("should do nothing on wrong link", function () {
+            var link = 'http://ohai.ca/~/dev/null';
+            expect(module.pickImageSize(link)).toEqual(link);
+
+            link = 'http://placekitten.com/300/200';
+            expect(module.pickImageSize(link)).toEqual(link);
+        });
+
+        it("should do the right thing on IS links", function () {
+            var link = 'http://images.secondfunnel.com/a/b/c/master.jpg';
+            expect(module.pickImageSize(link, 10))
+                .toEqual('http://images.secondfunnel.com/a/b/c/pico.jpg');
+
+            expect(module.pickImageSize(link, 15))
+                .toEqual('http://images.secondfunnel.com/a/b/c/pico.jpg');
+
+            expect(module.pickImageSize(link, 16))
+                .toEqual('http://images.secondfunnel.com/a/b/c/pico.jpg');
+
+            expect(module.pickImageSize(link, 17))
+                .toEqual('http://images.secondfunnel.com/a/b/c/icon.jpg');
+
+            expect(module.pickImageSize(link, 31))
+                .toEqual('http://images.secondfunnel.com/a/b/c/icon.jpg');
+        });
+
+        it("should be resolution-aware (i.e. unless you're running this test " +
+            "on a retina macbook or something," +
+            "master.jpg is not returned)", function () {
+            var link = 'http://images.secondfunnel.com/a/b/c/master.jpg';
+            expect(module.pickImageSize(link, 32))
+                .toEqual('http://images.secondfunnel.com/a/b/c/icon.jpg');
+
+            expect(module.pickImageSize(link, 1500))
+                .not.toEqual('http://images.secondfunnel.com/a/b/c/master.jpg');
+
+            expect(module.pickImageSize(link, 1500))
+                .not.toEqual('http://images.secondfunnel.com/a/b/c/master.jpg');
+
+            expect(module.pickImageSize(link, 2047))
+                .not.toEqual('http://images.secondfunnel.com/a/b/c/master.jpg');
+
+            expect(module.pickImageSize(link, 2048))
+                .not.toEqual('http://images.secondfunnel.com/a/b/c/master.jpg');
+
+            expect(module.pickImageSize(link, 2049))
+                .not.toEqual('http://images.secondfunnel.com/a/b/c/master.jpg');
+        });
+
+        it("should handle min/max parameters", function () {
+            var link = 'http://images.secondfunnel.com/a/b/c/master.jpg';
+            expect(module.pickImageSize(link, 31))
+                .toEqual('http://images.secondfunnel.com/a/b/c/icon.jpg');
+
+            expect(module.pickImageSize(link, 32))
+                .toEqual('http://images.secondfunnel.com/a/b/c/icon.jpg');
+
+            expect(module.pickImageSize(link, 33))
+                .toEqual('http://images.secondfunnel.com/a/b/c/thumb.jpg');
+
+            expect(module.pickImageSize(link, 33, 'max'))
+                .toEqual('http://images.secondfunnel.com/a/b/c/icon.jpg');
+        });
     });
 });
