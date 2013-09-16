4218dcc8d30f7f829df239b3d9c1139c78aa0d4c
Ahmed Al-Sudani <alsudani.ahmed@gmail.com>
2013-09-16 19:46:44 -0400

    Added IntentRank tests

--- apps/pinpoint/static/pinpoint/js/pages.marionette.ir.spec.js
+++ apps/pinpoint/static/pinpoint/js/pages.marionette.ir.spec.js
@@ -1,10 +1,24 @@
 describe('intentRank', function () {
+    // tested for campaigns
+
     var module = SecondFunnel.intentRank,
-        originalModule= $.extend({}, module);
+        originalModule= $.extend({}, module),
+        results, // to test fetching content
+        done, // to test fetching content
+        setResults = function (received) {
+            results = received;
+            done = true;
+        }
 
     beforeEach(function () {
+        // restore original state
         module = $.extend({},originalModule);
         module.initialize(PAGES_INFO);
+        module.options.type='campaign';
+
+        // reset variables used to test fetching
+        results = undefined;
+        done = undefined;
     });
 
     describe('initialize', function () {
@@ -32,26 +46,48 @@ describe('intentRank', function () {
             expect(module.options.campaign).toEqual(97);
             module.options.categories = [{id:-5}];
             module.changeCategory(-5);
-            //noinspection JSValidateTypes
             expect(module.options.campaign).toEqual(-5);
         });
     });
 
     describe('getResultsOnline', function () {
         it('should fetch content when asked to', function() {
-            var results,
-                done;
-            function setResults(received) {
-                results = received;
-                done = true;
-            }
-            module.options.type='campaign';
             module.getResultsOnline(module.options,setResults);
             waitsFor(function() {return done},
                 'fetching results', 6000);
             runs(function () {
                 expect(results && results.length).toBeTruthy();
             });
+        });
+
+        it('should fetch n results when told to', function() {
+            function setResults(received) {
+                results = received;
+                done = true;
+            }
+            module.options.IRResultsCount = 4;
+            module.getResultsOnline(module.options,setResults);
+
+            waitsFor(function() {return done},
+                'fetching results', 6000);
+            runs(function() {
+                expect(results && results.length).toBe(4);
+            });
+        });
+
+        it('should use its backup results if it does not receive anything from the server', function() {
+            function test_fn () {
+                module.base_url = 1;
+                module.getResultsOnline(module.options,setResults);
+                waitsFor(function() {return done},
+                    'fetching backup results',500);
+                runs(function() {
+                    expect(results && results.length).toBeTruthy();
+                    console.log(results);
+                });
+            }
+            module.getResultsOnline(module.options,test_fn);
+            waitsFor(function() {return done})
         });
     });
 });
