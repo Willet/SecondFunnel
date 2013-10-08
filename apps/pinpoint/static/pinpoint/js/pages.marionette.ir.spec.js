8f963016c1c8a5cf47c02749f0f908e96680d3c8
Ahmed Al-Sudani <alsudani.ahmed@gmail.com>
2013-10-08 18:16:02 -0400

    Shut up, JSLint

--- apps/pinpoint/static/pinpoint/js/pages.marionette.ir.spec.js
+++ apps/pinpoint/static/pinpoint/js/pages.marionette.ir.spec.js
@@ -1,4 +1,7 @@
-// idea: test that overrides are obeyed but not neccessary
+/*global describe, SecondFunnel, _, $, it, should, sinon, sinon, runs, waitsFor,
+  beforeEach, expect, spyOn
+ */
+
 describe('intentRank', function () {
     "use strict";
     // tested for campaigns
@@ -11,31 +14,31 @@ describe('intentRank', function () {
             results = received;
             done = true;
         },
-        areWeDone = function() {return done;},
+        areWeDone = function () {return done; },
         delay = 100,
         replaced = [], // used to reset state
 
         // Probably not necessary, but I can't help it
         // How to use:
-        // replace(object.property).with(replacement)
+        // replace(object.property)['with'](replacement)
         // reset() restores original
 
         replace = function (object, property) {
             var _orig = object[property];
-            return  {
-                'with': function(replacement) {
-                    replaced.unshift({'object': object,'property':
-                        property,'orig': _orig});
+            return {
+                'with': function (replacement) {
+                    replaced.unshift({'object': object, 'property':
+                        property, 'orig': _orig});
                     object[property] = replacement;
                     return object[property];
                 }
             };
         },
 
-        reset = function() {
-            if(replaced.length) {
-                _.each(replaced, function(element, index, list) {
-                    element.object[element['property']] = element.orig;
+        reset = function () {
+            if (replaced.length) {
+                _.each(replaced, function (element, index, list) {
+                    element.object[element.property] = element.orig;
                 });
             }
             replaced = [];
@@ -43,27 +46,28 @@ describe('intentRank', function () {
 
         mockAjaxSuccess = function (parameters) {
             var results = [],
-            deferred = new $.Deferred();
-            for (var i = 0; i < parameters.data.results; ++i) {
+                deferred = new $.Deferred(),
+                i;
+            for (i = 0; i < parameters.data.results; ++i) {
                 results.push({});
             }
             expected = results;
             // make it asynchronous
-            if(parameters.success) {
-                setTimeout(function () {parameters.success(results)}, delay);
+            if (parameters.success) {
+                setTimeout(function () {parameters.success(results); }, delay);
             }
-            setTimeout(function() {deferred.resolve(results)}, delay);
+            setTimeout(function () {deferred.resolve(results); }, delay);
             return deferred.promise();
         },
 
         mockAjaxZeroResults = function (parameters) {
             var results = [],
-            deferred = new $.Deferred();
+                deferred = new $.Deferred();
             // make it asynchronous
-            if(parameters.success) {
-                setTimeout(function () {parameters.success(results)}, delay);
+            if (parameters.success) {
+                setTimeout(function () {parameters.success(results); }, delay);
             }
-            setTimeout(function() {deferred.resolve(results)}, delay);
+            setTimeout(function () {deferred.resolve(results); }, delay);
             return deferred.promise();
         },
 
@@ -72,9 +76,9 @@ describe('intentRank', function () {
 
             // make it asynchronous
             if (parameters.success) {
-                setTimeout(function () {parameters.error('jqXHR', 'textStatus', 'errorThrown')}, delay);
+                setTimeout(function () {parameters.error('jqXHR', 'textStatus', 'errorThrown'); }, delay);
             }
-            setTimeout(function() {deferred.reject('jqXHR', 'textStatus', 'errorThrown')}, delay);
+            setTimeout(function () {deferred.reject('jqXHR', 'textStatus', 'errorThrown'); }, delay);
 
             // TODO: not sure this actually works. Need to investigate
             return deferred.promise();
@@ -85,7 +89,7 @@ describe('intentRank', function () {
         reset();
 
         //campaign mode
-        module.options.type='campaign';
+        module.options.type = 'campaign';
 
         // reset variables used to test fetching
         results = undefined;
@@ -98,7 +102,7 @@ describe('intentRank', function () {
 
     describe('changeCategory', function () {
 
-        it('should not change the category if it\'s not a number', function() {
+        it('should not change the category if it\'s not a number', function () {
             var oldCategory = module.options.campaign;
             module.changeCategory('don\'t');
             expect(module.options.campaign).toEqual(oldCategory);
@@ -106,7 +110,7 @@ describe('intentRank', function () {
             expect(module.options.campaign).toEqual(oldCategory);
         });
 
-        it('should not change the category if it\'s legal but not valid (not meant for this campaign)', function() {
+        it('should not change the category if it\'s legal but not valid (not meant for this campaign)', function () {
             var oldCategory = module.options.campaign;
             module.changeCategory(3);
             expect(module.options.campaign).toEqual(oldCategory);
@@ -115,62 +119,62 @@ describe('intentRank', function () {
         it('should change the category if it\'s a number and it\'s present', function () {
             module.changeCategory(97);
             expect(module.options.campaign).toEqual(97);
-            module.options.categories = [{id:-5}];
+            module.options.categories = [{id: -5}];
             module.changeCategory(-5);
             expect(module.options.campaign).toEqual(-5);
         });
     });
 
-    describe('getResults', function() {
-        it('should call getResultsOnline if SecondFunnel.options.page.offline is falsy', function() {
-            spyOn(module,'getResultsOnline');
-            var testOptionsPage = $.extend(true,{},SecondFunnel.options.page);
+    describe('getResults', function () {
+        it('should call getResultsOnline if SecondFunnel.options.page.offline is falsy', function () {
+            spyOn(module, 'getResultsOnline');
+            var testOptionsPage = $.extend(true, {}, SecondFunnel.options.page);
             testOptionsPage.offline = false;
-            replace(SecondFunnel.options,'page').with(testOptionsPage);
-            module.getResults(module.options,setResults);
+            replace(SecondFunnel.options, 'page')['with'](testOptionsPage);
+            module.getResults(module.options, setResults);
             expect(module.getResultsOnline).toHaveBeenCalled();
         });
 
         it('should call getResultsOffline if SecondFunnel.options.page.offline is truthy', function () {
-            spyOn(module,'getResultsOffline');
-            var testOptionsPage = $.extend(true,{},SecondFunnel.options.page);
+            spyOn(module, 'getResultsOffline');
+            var testOptionsPage = $.extend(true, {}, SecondFunnel.options.page);
             testOptionsPage.offline = true;
-            replace(SecondFunnel.options,'page').with(testOptionsPage);
+            replace(SecondFunnel.options, 'page')['with'](testOptionsPage);
             module.options.type = 'campaign';
-            module.getResults(module.options,setResults);
+            module.getResults(module.options, setResults);
             expect(module.getResultsOffline).toHaveBeenCalled();
         });
     });
 
-    describe('getResultsOffline',function() {
-        it('should exists', function() {
+    describe('getResultsOffline', function () {
+        it('should exists', function () {
             expect(module.getResultsOffline).toBeDefined();
         });
 
-        it('should pass results to callback', function() {
+        it('should pass results to callback', function () {
             module.getResultsOffline(module.options).always(setResults);
             waitsFor(areWeDone, 500, 'callback function to be called');
         });
 
-        it('should fetch content when asked to', function() {
-            var fakeResults = ['here','goes','content'];
-            replace(module.options, 'backupResults').with(fakeResults);
+        it('should fetch content when asked to', function () {
+            var fakeResults = ['here', 'goes', 'content'];
+            replace(module.options, 'backupResults')['with'](fakeResults);
             module.getResultsOffline(module.options).always(setResults);
-            waitsFor(function () {return done;});
-            runs( function () {
+            waitsFor(function () {return done; });
+            runs(function () {
                 // assuming it does not return more items than it has stored;
                 expect(results.length).toBeGreaterThan(0);
             });
         });
 
         it('should fetch 3 results when asked to', function () {
-            var n = 3;
-            var fakeResults = ['here','goes','content','more','content'];
-            replace(module.options, 'backupResults').with(fakeResults);
-            replace(module.options, 'IRResultsCount').with(n);
+            var n = 3,
+                fakeResults = ['here', 'goes', 'content', 'more', 'content'];
+            replace(module.options, 'backupResults')['with'](fakeResults);
+            replace(module.options, 'IRResultsCount')['with'](n);
             module.getResultsOffline(module.options).always(setResults);
-            waitsFor(function () {return done;});
-            runs( function () {
+            waitsFor(function () {return done; });
+            runs(function () {
                 // assuming it does not return more items than it has stored;
                 expect(results.length).toEqual(3);
             });
@@ -178,14 +182,14 @@ describe('intentRank', function () {
     });
 
     describe('getResultsOnline', function () {
-         it('should pass results to callback', function() {
+        it('should pass results to callback', function () {
             module.getResultsOnline(module.options).always(setResults);
             waitsFor(areWeDone, 500, 'callback function to be called');
         });
 
-        it('should fetch content when asked to', function() {
-            sinon.stub($,'ajax',mockAjaxSuccess);
-            sinon.stub($,'jsonp',mockAjaxSuccess);
+        it('should fetch content when asked to', function () {
+            sinon.stub($, 'ajax', mockAjaxSuccess);
+            sinon.stub($, 'jsonp', mockAjaxSuccess);
             module.getResultsOnline(module.options).always(setResults);
             waitsFor(areWeDone, 'fetching results', 6000);
             runs(function () {
@@ -193,58 +197,58 @@ describe('intentRank', function () {
             });
         });
 
-        it('should fetch 4 results when told to', function() {
+        it('should fetch 4 results when told to', function () {
             sinon.stub($, 'ajax', mockAjaxSuccess);
             sinon.stub($, 'jsonp', mockAjaxSuccess);
             var n = 4;
-            replace(module.options, 'IRResultsCount').with(n);
+            replace(module.options, 'IRResultsCount')['with'](n);
             module.getResultsOnline(module.options).always(setResults);
 
             waitsFor(areWeDone, 'fetching results', 6000);
-            runs(function() {
+            runs(function () {
                 expect(results.length).toBe(n);
             });
         });
 
-        it('should fetch 55 results when told to', function() {
+        it('should fetch 55 results when told to', function () {
             sinon.stub($, 'ajax', mockAjaxSuccess);
             sinon.stub($, 'jsonp', mockAjaxSuccess);
             var n = 55;
-            replace(module.options, 'IRResultsCount').with(n);
+            replace(module.options, 'IRResultsCount')['with'](n);
             module.getResultsOnline(module.options).always(setResults);
 
             waitsFor(areWeDone, 'fetching results', 6000);
-            runs(function() {
+            runs(function () {
                 expect(results.length).toBe(n);
             });
         });
 
 
-        it('should use its backup results if server returns nothing', function() {
+        it('should use its backup results if server returns nothing', function () {
             sinon.stub($, 'ajax', mockAjaxZeroResults);
             sinon.stub($, 'jsonp', mockAjaxZeroResults);
-            var backupResults = [{'backup':'backup'}];
-            replace(module.options, 'backupResults').with(backupResults);
+            var backupResults = [{'backup': 'backup'}];
+            replace(module.options, 'backupResults')['with'](backupResults);
             module.getResultsOnline(module.options).always(setResults);
 
-            waitsFor(areWeDone, 'fetching backup results',500);
-            runs(function() {
+            waitsFor(areWeDone, 'fetching backup results', 500);
+            runs(function () {
                 expect(results).toEqual(backupResults);
 
             });
         });
 
-        it('should use its backup results if we receive an Ajax error', function (){
+        it('should use its backup results if we receive an Ajax error', function () {
             sinon.stub($, 'ajax', mockAjaxError);
             sinon.stub($, 'jsonp', mockAjaxError);
-            var backupResults = [{'backup':'backup'}];
-            replace(module.options, 'backupResults').with(backupResults);
+            var backupResults = [{'backup': 'backup'}];
+            replace(module.options, 'backupResults')['with'](backupResults);
             module.getResultsOnline(module.options).always(setResults);
 
             waitsFor(areWeDone, 'fetching backup results', 500);
-            runs(function() {
+            runs(function () {
                 expect(results).toEqual(module.options.backupResults);
-            })
-        })
+            });
+        });
     });
 });
