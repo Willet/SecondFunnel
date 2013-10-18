aa3a8883b32484c928e48c941a1d07bd77c75352
Brian Lai <b2lai@uwaterloo.ca>
2013-10-17 20:41:42 -0400

    bypass bucket operation

--- apps/static_pages/hooks.py
+++ apps/static_pages/hooks.py
@@ -7,21 +7,44 @@ from apps.assets.models import Store
 from apps.pinpoint.models import Campaign
 
 from apps.static_pages.tasks import (generate_static_campaign,
-    create_bucket_for_store)
+    generate_static_campaign_now, create_bucket_for_store,
+    create_bucket_for_store_now)
 
 def campaign_saved(sender, instance, created, **kwargs):
     """
     Uploads saved campaign to S3
     """
+    try:
+        generate_static_campaign.delay(instance.store.id, instance.id)
+        return
+    except:
+        pass  # TODO: investigate [Errno 111] Connection refused
 
-    generate_static_campaign.delay(instance.id)
+    try:
+        generate_static_campaign_now(instance.store.id, instance.id)
+        return
+    except:
+        pass  # TODO: investigate [Errno 111] Connection refused
 
 
 def store_saved(sender, instance, created, **kwargs):
     """
-    Creates a static website bucket for the saved store, if one doesn't already exist
+    Creates a static website bucket for the saved store, if one
+    doesn't already exist.
+
+    instance is a store.
     """
-    create_bucket_for_store.delay(instance.id)
+    try:
+        create_bucket_for_store.delay(instance.id)
+        return
+    except:
+        pass  # TODO: investigate [Errno 111] Connection refused
+
+    try:
+        create_bucket_for_store_now(instance.id)
+    except:
+        pass
+
 
 post_save.connect(store_saved, sender=Store)
 post_save.connect(campaign_saved, sender=Campaign)
