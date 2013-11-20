889d0b602ff348273f6a9a298cbd18e7b0a621b0
Ahmed <alsudani.ahmed@gmail.com>
2013-11-20 14:23:11 -0500

    Rewrite to use Tastypie's test client rather than Django's

--- apps/api/tests/test_proxy.py
+++ apps/api/tests/test_proxy.py
@@ -1,14 +1,23 @@
 from django.test import TestCase
-from django.test import Client
+from tastypie.test import TestApiClient
+from .test_config import *
 
-class proxyTest(TestCase):
+class ProxyTest(TestCase):
+    # TODO skip if login test fails
 
     # Should return 200 if valid credentials
+    def test_valid_request(self):
+        client = TestApiClient()
+        response = client.post(login_url, format='json', data=valid_login)
+
+        response = client.get(restricted_url)
+        self.assertEqual(200, restricted_url)
 
     # should return 401 if invalid credentials
     def test_unauthorized(self):
-        client = Client()
-        response = client.get('/proxy')
+        client = TestApiClient()
+        response = client.get(proxy_url + '/store/38')
+        self.assertEqual(401, response.status_code, 'Proxy did not return 401 to unauthorized request')
 
     # should return 404 if authorized and resource DNE
 
