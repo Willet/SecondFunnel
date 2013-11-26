42a534d28a1384897d748fcc918cfa7d3b41f854
Alex Riedler <alex@riedler.ca>
2013-11-26 14:19:03 -0800

    Fix issue with trailing slash and PATCH arguments.

--- apps/api/views.py
+++ apps/api/views.py
@@ -201,7 +201,7 @@ def proxy_content(request, store_id, page_id, content_id):
 def reject_content(request, store_id, content_id):
     payload = json.dumps({'status': 'rejected'})
 
-    r = ContentGraphClient.store(store_id).content(content_id).PATCH(payload)
+    r = ContentGraphClient.store(store_id).content(content_id).PATCH(data=payload)
 
     response = HttpResponse(content=r.content, status=r.status_code)
 
@@ -215,7 +215,7 @@ def reject_content(request, store_id, content_id):
 def undecide_content(request, store_id, content_id):
     payload = json.dumps({'status': 'needs-review'})
 
-    r = ContentGraphClient.store(store_id).content(content_id).PATCH(payload)
+    r = ContentGraphClient.store(store_id).content(content_id).PATCH(data=payload)
 
     response = HttpResponse(content=r.content, status=r.status_code)
 
@@ -229,7 +229,7 @@ def undecide_content(request, store_id, content_id):
 def approve_content(request, store_id, content_id):
     payload = json.dumps({'status': 'approved'})
 
-    r = ContentGraphClient.store(store_id).content(content_id).PATCH(payload)
+    r = ContentGraphClient.store(store_id).content(content_id).PATCH(data=payload)
 
     response = HttpResponse(content=r.content, status=r.status_code)
 
