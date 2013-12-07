67656699f04c0baf80bad043229b7312ee4c8553
Kevin Simpson <ktsimpso@gmail.com>
2013-12-06 19:08:16 -0500

    CM-25 Refactored tests.

--- apps/api/tests/test_page.py
+++ apps/api/tests/test_page.py
@@ -70,40 +70,33 @@ class AuthenticatedPageAddAllContentTests(AuthenticatedResourceTestCase):
         self.mock_url_pattern = r'store/\d+/page/\d+/content/\d+/?'
         self.url = '/graph/v1/store/%s/page/%s/content/add_all' % (self.store_id, self.page_id)
 
-    @mock.patch.object(requests.Session, 'request')
-    def test_all_good(self, mock_request):
-        mock_request = configure_hammock_request(mock_request, {
-            self.mock_url_pattern: (
-                {'status': 200, 'content-type': 'application/json'},
-                json.dumps({})
-            )
-        })
-
-        response = self.api_client.put(self.url, format='json', data=self.content_data)
+        #Seting up mocks
+        self.MockResponse = namedtuple('MockResponse', ['status_code', 'content', 'headers'])
+        def side_effect(*args, **kwargs):
+            return self.MockResponse(status_code=200, content='', headers={})
         
-        self.assertTrue(mock_request.called, 'Mock request was never made')
-        self.assertEqual(mock_request.call_count, len(self.content_data), 'Mock was not called the correct number of times')
+        self.mock_request = mock.Mock(side_effect=side_effect)
+        self.mocks = mock.patch.object(requests.Session, 'request', self.mock_request)
+        self.mocks.start()
+        self.addCleanup(self.mocks.stop)
+
+    def test_all_good(self):
+        response = self.api_client.put(self.url, format='json', data=self.content_data)
+        self.assertTrue(self.mock_request.called, 'Mock request was never made')
+        self.assertEqual(self.mock_request.call_count, len(self.content_data), 'Mock was not called the correct number of times')
         
         for i in range(len(self.content_data)):
-            self.assertEqual(mock_request.call_args_list[i][0], 
+            self.assertEqual(self.mock_request.call_args_list[i][0], 
                 ('put', settings.CONTENTGRAPH_BASE_URL + '/store/%s/page/%s/content/%s' % (self.store_id, self.page_id, self.content_data[i])))
 
         self.assertHttpOK(response)
 
-    @mock.patch.object(requests.Session, 'request')
-    def test_not_authenticated(self, mock_request):
-        mock_request = configure_hammock_request(mock_request, {
-            self.mock_url_pattern: (
-                {'status': 200, 'content-type': 'application/json'},
-                json.dumps({})
-            )
-        })        
-
+    def test_not_authenticated(self):
         client = TestApiClient()
         response = client.put(self.url, format='json', data=self.content_data)
 
-        self.assertFalse(mock_request.called, 'Mock request was still called when user was not logged in')
-        self.assertEqual(mock_request.call_count, 0)
+        self.assertFalse(self.mock_request.called, 'Mock request was still called when user was not logged in')
+        self.assertEqual(self.mock_request.call_count, 0)
 
         self.assertHttpUnauthorized(response)
         self.assertEqual(response._headers['content-type'][1], 'application/json')
@@ -111,54 +104,43 @@ class AuthenticatedPageAddAllContentTests(AuthenticatedResourceTestCase):
             'error': 'Not logged in'
         }), response.content)
 
-    @mock.patch.object(requests.Session, 'request')
-    def test_bad_request_method(self, mock_request):
-        mock_request = configure_hammock_request(mock_request, {
-            self.mock_url_pattern: (
-                {'status': 200, 'content-type': 'application/json'},
-                json.dumps({})
-            )
-        })
+    def test_bad_request_method(self):
+        verbs = [
+            {
+                'verb': 'get'
+            },
+            {
+                'verb': 'post',
+                'data': True
+            },
+            {
+                'verb': 'patch',
+                'data': True
+            },
+            {
+                'verb': 'delete'
+            }
+        ]
 
-        response = self.api_client.get(self.url, format='json')
-        self.assertFalse(mock_request.called, 'Mock request was still called when user was not logged in')
-        self.assertEqual(mock_request.call_count, 0)
-        self.assertHttpMethodNotAllowed(response)
+        for verb in verbs:
+            data = None
+            if 'data' in verb:
+                data = self.content_data
+            
+            response = getattr(self.api_client, verb['verb'])(self.url, format='json', data=data)
+            self.assertFalse(self.mock_request.called, 'Mock request was still called when user was not logged in')
+            self.assertEqual(self.mock_request.call_count, 0)
+            self.assertHttpMethodNotAllowed(response)
 
-        response = self.api_client.post(self.url, format='json', data=self.content_data)
-        self.assertFalse(mock_request.called, 'Mock request was still called when user was not logged in')
-        self.assertEqual(mock_request.call_count, 0)
-        self.assertHttpMethodNotAllowed(response)
-
-        response = self.api_client.patch(self.url, format='json', data=self.content_data)
-        self.assertFalse(mock_request.called, 'Mock request was still called when user was not logged in')
-        self.assertEqual(mock_request.call_count, 0)
-        self.assertHttpMethodNotAllowed(response)
-
-        response = self.api_client.delete(self.url, format='json')
-        self.assertFalse(mock_request.called, 'Mock request was still called when user was not logged in')
-        self.assertEqual(mock_request.call_count, 0)
-        self.assertHttpMethodNotAllowed(response)
-
-    @mock.patch.object(requests.Session, 'request')
-    def test_bad_json(self, mock_request):
-        mock_request = configure_hammock_request(mock_request, {
-            self.mock_url_pattern: (
-                {'status': 200, 'content-type': 'application/json'},
-                json.dumps({})
-            )
-        })
-
+    def test_bad_json(self):
         response = self.api_client.put(self.url, format='json', data={
             "abc": "def"
         })
-
-        self.assertFalse(mock_request.called, 'Mock request was still called when bad json data was provided')
+        self.assertFalse(self.mock_request.called, 'Mock request was still called when bad json data was provided')
         self.assertHttpApplicationError(response)
 
         response = self.api_client.put(self.url, format='xml', data='&$dsadsad""rfwefsf{}[]dffds')
-
-        self.assertFalse(mock_request.called, 'Mock request was still called when bad json data was provided')
+        self.assertFalse(self.mock_request.called, 'Mock request was still called when bad json data was provided')
         self.assertHttpApplicationError(response)
 
         response = self.api_client.put(self.url, format='json', data=[
@@ -169,13 +151,10 @@ class AuthenticatedPageAddAllContentTests(AuthenticatedResourceTestCase):
                 "ghi": "jkl"
             }
         ])
-
-        self.assertFalse(mock_request.called, 'Mock request was still called when bad json data was provided')
+        self.assertFalse(self.mock_request.called, 'Mock request was still called when bad json data was provided')
         self.assertHttpApplicationError(response)
 
-    @mock.patch.object(requests.Session, 'request')
-    def test_remote_errors(self, mock_request):
-        MockResponse = namedtuple('MockResponse', ['status_code', 'content', 'headers'])
+    def test_remote_errors(self):
         inject_variables = {
             'calls': 0
         }
@@ -183,31 +162,20 @@ class AuthenticatedPageAddAllContentTests(AuthenticatedResourceTestCase):
         def side_effect(*args, **kwargs):
             if inject_variables['calls'] == 0:
                 inject_variables['calls'] += 1
-                return MockResponse(
-                    status_code = 200,
-                    content = '',
-                    headers = {}
-                )
+                return self.MockResponse(status_code = 200, content = '', headers = {})
             elif inject_variables['calls'] == 1:
                 inject_variables['calls'] += 1
-                return MockResponse(
-                    status_code = 500,
-                    content = '',
-                    headers = {}
-                )
+                return self.MockResponse(status_code = 500, content = '', headers = {})
             elif inject_variables['calls'] == 2:
                 self.fail('Mock called too many times')
         
-        mock_request.side_effect = side_effect
+        self.mock_request.side_effect = side_effect
 
         response = self.api_client.put(self.url, format='json', data=self.content_data)
-
-        self.assertTrue(mock_request.called, 'Mock request was never made')
-        self.assertEqual(mock_request.call_count, 2, 'Mock was not called the correct number of times')
-
-        self.assertEqual(mock_request.call_args_list[0][0], 
+        self.assertTrue(self.mock_request.called, 'Mock request was never made')
+        self.assertEqual(self.mock_request.call_count, 2, 'Mock was not called the correct number of times')
+        self.assertEqual(self.mock_request.call_args_list[0][0], 
                 ('put', settings.CONTENTGRAPH_BASE_URL + '/store/%s/page/%s/content/%s' % (self.store_id, self.page_id, self.content_data[0])))
-        self.assertEqual(mock_request.call_args_list[1][0], 
+        self.assertEqual(self.mock_request.call_args_list[1][0], 
                 ('put', settings.CONTENTGRAPH_BASE_URL + '/store/%s/page/%s/content/%s' % (self.store_id, self.page_id, self.content_data[1])))
-
         self.assertHttpApplicationError(response)
