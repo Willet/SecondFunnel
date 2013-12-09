3584a1e28650c02ccbbe23f6df8d52076d803711
Ahmed Al-Sudani <alsudani.ahmed@gmail.com>
2013-12-09 14:43:37 -0500

    Disable hover events while viewer scrolls

--- apps/pinpoint/static/pinpoint/js/pages.preinit.js
+++ apps/pinpoint/static/pinpoint/js/pages.preinit.js
@@ -363,3 +363,20 @@ receive = function (eventName) {
 debugOp = function () {
     console.debug('%O, %O', this, arguments);
 };
+
+(function() {
+    'use strict';
+    var body = document.body,
+        timer;
+
+    window.addEventListener('scroll', function() {
+        clearTimeout(timer);
+        if(!body.classList.contains('disable-hover')) {
+            body.classList.add('disable-hover')
+        }
+
+        timer = setTimeout(function(){
+            body.classList.remove('disable-hover')
+        },250);
+    }, false);
+}());
