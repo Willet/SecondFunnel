35a68a52a47a524f8979138b01d210ec86e084af
Brian Lai <b2lai@uwaterloo.ca>
2013-12-11 12:04:36 -0500

    add 300s interval to content polling

--- secondfunnel/settings/common.py
+++ secondfunnel/settings/common.py
@@ -119,11 +119,12 @@ AWS_SQS_QUEUE_NAME = AWS_SNS_TOPIC_NAME  # by default, same as the sns name
 AWS_SQS_POLLING_QUEUES = [
     # https://willet.atlassian.net/browse/CM-125
     {'queue_name': 'product-update-notification-queue',
-     'handler': 'handle_product_queue_items',
+     'handler': 'handle_product_update_notification_messages',
      'interval': 300},
     # https://willet.atlassian.net/browse/CM-126
     {'queue_name': 'content-update-notification-queue',
-     'handler': 'handle_content_update_notification_messages'},
+     'handler': 'handle_content_update_notification_messages',
+     'interval': 300},
     # https://willet.atlassian.net/browse/CM-127
     {'queue_name': 'tile-generator-notification-queue',
      'handler': 'handle_tile_generator_update_notification_messages'},
