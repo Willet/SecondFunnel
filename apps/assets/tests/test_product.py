ac9e51cab7bbee30e9140979d6fc57155937a054
Brian Lai <b2lai@uwaterloo.ca>
2013-12-11 16:54:54 -0500

    CM-125 change mock target

--- apps/assets/tests/test_product.py
+++ apps/assets/tests/test_product.py
@@ -11,14 +11,15 @@ class ProductNotificationQueueTestSuite(TestCase):
     """Running these two tests from PyCharm might cause "False is not True"
     and "True is not False" mix-ups. (It might have been an isolated case?)
     """
-    @mock.patch('apps.static_pages.aws_utils.sqs_poll')
+    @mock.patch('boto.sqs.connection.SQSConnection.receive_message')
     @mock.patch('apps.contentgraph.models.TileConfigObject'
                 '.mark_tile_for_regeneration')
-    def test_update_tileconfig_fail(self, mocked_method, mock_poll):
+    def test_update_tileconfig_fail(self, mocked_mark_tile_for_regeneration,
+                                    mock_receive_message):
         """checks if no fake queue item calls the regeneration initiator."""
         def side(*args, **kwargs):
             return []
-        mock_poll.side_effect = side
+        mock_receive_message.side_effect = side
 
         queues = settings.AWS_SQS_POLLING_QUEUES[settings.AWS_SQS_REGION_NAME]
         product_update_queue = queues['product-update-notification-queue']
@@ -26,17 +27,19 @@ class ProductNotificationQueueTestSuite(TestCase):
         fetch_queue(product_update_queue, interval=-1)
 
         # nothing in the 'queue' to process
-        self.assertFalse(mocked_method.called)
+        self.assertTrue(mock_receive_message.called)
+        self.assertFalse(mocked_mark_tile_for_regeneration.called)
 
 
-    @mock.patch('apps.static_pages.aws_utils.sqs_poll')
+    @mock.patch('boto.sqs.connection.SQSConnection.receive_message')
     @mock.patch('apps.contentgraph.models.TileConfigObject'
                 '.mark_tile_for_regeneration')
-    def test_update_tileconfig_success(self, mocked_method, mock_poll):
+    def test_update_tileconfig_success(self, mocked_mark_tile_for_regeneration,
+                                       mock_receive_message):
         """checks if a fake queue item calls the regeneration initiator."""
         def side(*args, **kwargs):
             return [RawMessage(body='{"page-id": 1, "product-id": 2}')]
-        mock_poll.side_effect = side
+        mock_receive_message.side_effect = side
 
         queues = settings.AWS_SQS_POLLING_QUEUES[settings.AWS_SQS_REGION_NAME]
         product_update_queue = queues['product-update-notification-queue']
@@ -44,4 +47,5 @@ class ProductNotificationQueueTestSuite(TestCase):
         fetch_queue(product_update_queue, interval=-1)
 
         # one item in the 'queue' to process
-        self.assertTrue(mocked_method.called)
+        self.assertTrue(mock_receive_message.called)
+        self.assertTrue(mocked_mark_tile_for_regeneration.called)
