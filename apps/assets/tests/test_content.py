db1553e6271b1b2fa179c19cd46d3a32232af07f
Brian Lai <b2lai@uwaterloo.ca>
2013-12-12 11:06:09 -0500

    CM-126 add (the same) tests (as product tileconfigs)

---
+++ apps/assets/tests/test_content.py
@@ -0,0 +1,51 @@
+import mock
+
+from boto.sqs.message import RawMessage
+from django.conf import settings
+from django.utils.unittest.case import TestCase
+
+from apps.api.tasks import fetch_queue
+
+
+class ProductNotificationQueueTestSuite(TestCase):
+    """Running these two tests from PyCharm might cause "False is not True"
+    and "True is not False" mix-ups. (It might have been an isolated case?)
+    """
+    @mock.patch('boto.sqs.connection.SQSConnection.receive_message')
+    @mock.patch('apps.contentgraph.models.TileConfigObject'
+                '.mark_tile_for_regeneration')
+    def test_update_tileconfig_fail(self, mocked_mark_tile_for_regeneration,
+                                    mock_receive_message):
+        """checks if no fake queue item calls the regeneration initiator."""
+        def side(*args, **kwargs):
+            return []
+        mock_receive_message.side_effect = side
+
+        queues = settings.AWS_SQS_POLLING_QUEUES[settings.AWS_SQS_REGION_NAME]
+        content_update_queue = queues['content-update-notification-queue']
+
+        fetch_queue(content_update_queue, interval=-1)
+
+        # nothing in the 'queue' to process
+        self.assertTrue(mock_receive_message.called)
+        self.assertFalse(mocked_mark_tile_for_regeneration.called)
+
+
+    @mock.patch('boto.sqs.connection.SQSConnection.receive_message')
+    @mock.patch('apps.contentgraph.models.TileConfigObject'
+                '.mark_tile_for_regeneration')
+    def test_update_tileconfig_success(self, mocked_mark_tile_for_regeneration,
+                                       mock_receive_message):
+        """checks if a fake queue item calls the regeneration initiator."""
+        def side(*args, **kwargs):
+            return [RawMessage(body='{"page-id": 1, "content-id": 2}')]
+        mock_receive_message.side_effect = side
+
+        queues = settings.AWS_SQS_POLLING_QUEUES[settings.AWS_SQS_REGION_NAME]
+        content_update_queue = queues['content-update-notification-queue']
+
+        fetch_queue(content_update_queue, interval=-1)
+
+        # one item in the 'queue' to process
+        self.assertTrue(mock_receive_message.called)
+        self.assertTrue(mocked_mark_tile_for_regeneration.called)
