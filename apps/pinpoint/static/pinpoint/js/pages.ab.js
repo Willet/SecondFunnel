718fe3f1f961090a9f2caa4534fa38b98ac68903
Ford Peprah <hkpeprah@uwaterloo.ca>
2014-02-13 14:48:16 -0500

    Add comments about implementation and usage.

--- apps/pinpoint/static/pinpoint/js/pages.ab.js
+++ apps/pinpoint/static/pinpoint/js/pages.ab.js
@@ -1,6 +1,5 @@
 /*global App, $, Backbone, Marionette, console, _, GA_CUSTOMVAR_SCOPE.VISITOR  */
 var ALPHANUMERIC = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
 /**
  * @module ab
  */
@@ -8,42 +7,54 @@ App.module("ab", function (ab, App) {
     "use strict";
     var pageId = App.option('page:id'),
         pageName = App.option('url', pageId),
-        tracker = App.module.tracker;
+        tracker = App.module.tracker,
+        setCustomVar = function (index, test) {
+            window.ga('set', 'dimension' + index, test);
+        },
+        is = function (device) {
+            if (device === 'mobile') {
+                return App.support.mobile();
+            } else if (device && device.length) {
+                return (new RegExp(device, 'i')).test(window.navigator.userAgent);
+            }
+            return true;
+        };
 
-    this.setCustomVar = function (index, test) {
-        window.ga('set', 'dimension' + index, test);
-    };
-
-    this.is = function (device) {
-        if (device === 'mobile') {
-            return App.support.mobile();
-        } else if (device && device.length) {
-            return (new RegExp(device, 'i')).test(window.navigator.userAgent);
-        }
-        return true;
-    };
-
-    this.registerTest = function (method, device) {
-        var index, result,
+    /**
+     * Registers a new tests, tests registered should be of the form
+     *     (method, device, index, arguments ....)
+     * where index specifies the dimension where the result should be stored.
+     *
+     * @returns this
+     */
+    this.registerTest = function (method, device, index) {
+        var result,
             args = Array.prototype.slice.call(arguments);
-        args = args.slice(2);
-        index = args.shift();
+        args = args.slice(3);
 
-        if (this.is(device)) {
+        if (is(device)) {
             if (this.hasOwnProperty(method)) {
                 result = this[method].apply(this,  args);
             } else if (_.isFunction(method)) {
                 result = method.apply(this, args);
             }
         }
-
+        // If given a variant, store it
         if (result) {
-            this.setCustomVar(index, result);
+            setCustomVar(index, result);
         }
         return this;
     };
 
-    // TODO: Style should support multivariate
+    /**
+     * Specifies a style (styles) to apply to the specified selector
+     * When registered, should be of form
+     *    ...selector, styles, multivariates)
+     * where styles is either an array or individual tile and multivariates is
+     * an array of probabilities (optional, uniform if omitted)
+     *
+     * @returns string
+     */
     this.style = function (selector, style, multivariate) {
         var s = $('<style type="text/css"></style>');
         style = this.multivariate(style, multivariate);
@@ -51,9 +62,19 @@ App.module("ab", function (ab, App) {
             s.text(selector + " { " + style + " } ");
             $('head').append(s);
         }
+        // TODO: Should not be storing the style in the analytics
         return style;
     };
 
+    /**
+     * Specifies template(s) to select from to replace the source template
+     * When registered, should be of form
+     *     ...sourceTemplate, replacement(s), multivariate)
+     * where sourceTemplate and replacement templates are selectors; replacement
+     * can be an array, multivariate is probabilities (uniform if omitted)
+     *
+     * @returns string
+     */
     this.template = function (source, replacement, multivariate) {
         var tmp, templates;
 
@@ -68,6 +89,13 @@ App.module("ab", function (ab, App) {
         return ALPHANUMERIC[templates.indexOf(tmp)];
     };
 
+    /**
+     * Selects from an array if options is an array given probabilities,
+     * otherwise uniform.  If options is not an array, selects with a
+     * probability of 50%.
+     *
+     * @returns object
+     */
     this.multivariate = function (options, probability) {
         var index = 0,
             ratio = Math.random();
