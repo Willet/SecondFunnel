0a648d861c8db7e5965a1b11cdb2332dbf730e9c
Brian Lai <b2lai@uwaterloo.ca>
2014-02-19 16:05:50 -0500

    add migrations

---
+++ apps/static_pages/migrations/0001_initial.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'StaticLog'
+        db.create_table(u'static_pages_staticlog', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('object_id', self.gf('django.db.models.fields.PositiveIntegerField')()),
+            ('key', self.gf('django.db.models.fields.CharField')(max_length=2)),
+            ('timestamp', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'static_pages', ['StaticLog'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'StaticLog'
+        db.delete_table(u'static_pages_staticlog')
+
+
+    models = {
+        u'static_pages.staticlog': {
+            'Meta': {'object_name': 'StaticLog'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'key': ('django.db.models.fields.CharField', [], {'max_length': '2'}),
+            'object_id': ('django.db.models.fields.PositiveIntegerField', [], {}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['static_pages']
