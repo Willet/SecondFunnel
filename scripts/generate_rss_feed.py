7d2f5747e4580c66d847fc02df1c71ad011151b8
Christopher Papke <chris@willetinc.com>
2014-02-25 22:22:43 -0800

    update rss feed generator

--- scripts/generate_rss_feed.py
+++ scripts/generate_rss_feed.py
@@ -3,65 +3,44 @@
 #argv - 1: store-id, 2: page-id
 
 import argparse
-import time
 import urllib2
 import hashlib
-import apps.static_pages.aws_utils as aws_utils
 
-from time import gmtime, strftime
-from apps.assets.models import Store, Image, Page, Tile
-
-RESULTS = 35
-
-FEED_NAME = 'feed.rss'
+from time import gmtime, strftime, time
+from apps.assets.models import Image, Page, Tile
 
 
-def main():
-    parser = argparse.ArgumentParser(
-        description='Generate a Flipboard RSS Feed.'
-    )
-    parser.add_argument('page_id', type=int, help='Page Identifier')
-    parser.add_argument('bucket', help='Page bucket')
-    parser.add_argument('folder', help='Page folder')
+def main(page, results=35, feed_name='feed.rss'):
+    store = page.store
 
-    args, unknown = parser.parse_known_args()
-
-    url = 'http://' + args.bucket + '/' + args.folder
+    url = 'http://' + store.slug + '.secondfunnel.com/' + page.url_slug + '/'
 
     feed = ''
-    for line in generate_feed(args.page_id, url):
+    for line in generate_feed(page, url, results, feed_name):
         feed += line
 
-    upload_feed(feed, args.bucket, args.folder)
-
-    notify_superfeedr(args.bucket, args.folder)
+    return feed
 
 
-def notify_superfeedr(bucket, folder):
-    url = 'http://second-funnel.superfeedr.com?hub.mode=publish&hub.url=http://' + bucket + '/' + folder + FEED_NAME
+def notify_superfeedr(bucket, folder, feed_name='feed.rss'):
+    url = 'http://second-funnel.superfeedr.com?hub.mode=publish&hub.url=http://' + bucket + '/' + folder + feed_name
     r = urllib2.urlopen(url, data=' ') # sending data to make this a post request
 
 
-def upload_feed(xml, bucket, folder):
-    aws_utils.upload_to_bucket(bucket, folder + FEED_NAME, xml, content_type='application/rss+xml', public=True, do_gzip=False)
-
-
-def tile_to_XML(url, tile):
+def tile_to_XML(url, tile, current_time):
     product = tile.products.first()
     content = tile.content.first()
 
     image = Image.objects.filter(content_ptr_id=content.id).first()
 
     yield '<title>' + product.name + '</title>\n'
-    url_parts = url.split('/')
-    campaign = url_parts[-1] or url_parts[-2]
-    yield '<link>' + url + '?utm_source=flipboard&utm_medium=rss-feed&utm_campaign=' + campaign + '#' + str(tile.old_id) + '</link>\n'
+    yield '<link>' + url + '#' + str(tile.old_id) + '</link>\n'
 
     m = hashlib.md5()
     m.update(str(tile.id))
 
     yield '<guid isPermaLink="false">' + m.hexdigest() + '</guid>\n'
-    yield '<pubDate>' + strftime('%a, %d %b %Y %H:%M:%S +0000', gmtime()) + '</pubDate>\n'
+    yield '<pubDate>' + strftime('%a, %d %b %Y %H:%M:%S +0000', gmtime(current_time)) + '</pubDate>\n'
     yield '<content:encoded><![CDATA[\n'
     yield '\t<figure>\n'
     img = '\t\t<img src="' + image.url.replace('master', '1024x1024')
@@ -73,25 +52,27 @@ def tile_to_XML(url, tile):
     yield ']]></content:encoded>\n'
 
 
-def generate_channel(page_id, url):
-    page = Page.objects.get(id=page_id)
+def generate_channel(page, url, results=35, feed_name='feed.rss'):
 
     yield '<title>' + page.name + '</title>\n'
     yield '<link>' + url + '</link>\n'
     yield '<description></description>\n'
     yield '<language>en-us</language>\n'
-    yield '<atom:link rel="self" href="' + url + FEED_NAME + '" xmlns="http://www.w3.org/2005/Atom"/>\n'
+    yield '<atom:link rel="self" href="' + url + feed_name + '" xmlns="http://www.w3.org/2005/Atom"/>\n'
     yield '<atom:link rel="hub" href="http://second-funnel.superfeedr.com/" xmlns="http://www.w3.org/2005/Atom"/>\n'
 
-    for tile in Tile.objects.filter(feed_id=page.feed_id, template='image')[0:RESULTS]:
+    current_time = time()
+
+    for tile in Tile.objects.filter(feed_id=page.feed_id, template='image')[0:results]:
         yield '<item>\n'
-        for tile_line in tile_to_XML(url, tile):
+        for tile_line in tile_to_XML(url, tile, current_time):
             yield '\t' + tile_line
+        print 'hai'
+        current_time -= 1
         yield '</item>\n'
-        time.sleep(1)
 
 
-def generate_feed(page_id, url):
+def generate_feed(page, url, results=35, feed_name='feed.rss'):
     yield '<rss version="2.0"\n'
     yield '\t xmlns:content="http://purl.org/rss/1.0/modules/content/"\n'
     yield '\t xmlns:dc="http://purl.org/dc/elements/1.1/"\n'
@@ -100,11 +81,20 @@ def generate_feed(page_id, url):
     yield '\t xmlns:georss="http://www.georss.org/georss">\n'
 
     yield '\t<channel>\n'
-    for channel_line in generate_channel(page_id, url):
+    for channel_line in generate_channel(page, url, results, feed_name):
         yield '\t\t' + channel_line
     yield '\t</channel>\n'
     yield '</rss>'
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(
+        description='Generate a Flipboard RSS Feed.'
+    )
+    parser.add_argument('page_id', type=int, help='Page Identifier')
+
+    args, unknown = parser.parse_known_args()
+
+    page = Page.objects.get(id=args.page_id)
+
+    print main(page, args.bucket, args.folder)
