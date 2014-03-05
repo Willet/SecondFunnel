128ec0672d7ed392b695cace2c0d3116577d239d
Brian Lai <b2lai@uwaterloo.ca>
2014-03-05 11:14:09 -0500

    common-ise settings

--- secondfunnel/settings/production.py
+++ secondfunnel/settings/production.py
@@ -1,8 +1,4 @@
 from common import *
-from secondfunnel.errors import EnvironmentSettingsError
-
-if not all([AWS_STORAGE_BUCKET_NAME, MEMCACHED_LOCATION]):
-    raise EnvironmentSettingsError()
 
 ENVIRONMENT = "production"
 DEBUG = False
@@ -21,87 +17,19 @@ AWS_HEADERS =  {
     'Vary': 'Accept-Encoding',
 }
 
-# dict of queues by region to poll regularly, using celery beat.
-# corresponding handlers need to be imported in apps.api.tasks
-AWS_SQS_POLLING_QUEUES = {
-    'us-west-2': {
-        # https://willet.atlassian.net/browse/CM-125
-        'product-update-notification-queue':
-            {'queue_name': 'product-update-notification-queue',
-             'handler': 'handle_product_update_notification_message',
-             'interval': 300},
-
-        # https://willet.atlassian.net/browse/CM-126
-        'content-update-notification-queue':
-            {'queue_name': 'content-update-notification-queue',
-             'handler': 'handle_content_update_notification_message',
-             'interval': 300},
-
-        # https://willet.atlassian.net/browse/CM-127
-        'tile-generator-notification-queue':
-            {'queue_name': 'tile-generator-notification-queue',
-             'handler': 'handle_tile_generator_update_notification_message',
-             'interval': 5},
-
-        # https://willet.atlassian.net/browse/CM-128
-        'ir-config-generator-notification-queue':
-            {'queue_name': 'ir-config-generator-notification-queue',
-             'handler': 'handle_ir_config_update_notification_message'},
-
-        # https://willet.atlassian.net/browse/CM-124
-        'scraper-notification-queue':
-            {'queue_name': 'scraper-notification-queue',
-             'handler': 'handle_scraper_notification_message'},
-
-        'page-generator':
-            {'queue_name': 'page-generator',
-             'handler': 'handle_page_generator_notification_message'},
-    }
-}
-
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'django.contrib.admin',
-    'django.contrib.admindocs',
-    'django.contrib.humanize',
-
-    # external apps
-    'djcelery',
-    'storages',
-    'south',
-    'django_extensions',
-    'tastypie',
-    'ajax_forms',
-    'compressor',
-    'corsheaders',
-
-    # our apps
-    'apps.api',
-    'apps.assets',
-    'apps.pinpoint',
-    'apps.intentrank',
-    'apps.contentgraph',
-    'apps.website',
-    'apps.static_pages',
-    'apps.utils',
-)
-
+WEBSITE_BASE_URL = 'http://tng-test.secondfunnel.com'
+INTENTRANK_BASE_URL = 'http://tng-test.secondfunnel.com'
 CONTENTGRAPH_BASE_URL = 'http://contentgraph-test.elasticbeanstalk.com/graph'
-
-# if secondfunnel-test.elasticbeanstalk.com
-# SESSION_COOKIE_DOMAIN = '.elasticbeanstalk.com'
-# if test.secondfunnel.com (recommended)
-SESSION_COOKIE_DOMAIN = '.secondfunnel.com'
+AWS_STORAGE_BUCKET_NAME = os.getenv('TestBucket', 'elasticbeanstalk-us-east-1-056265713214')
+INTENTRANK_CONFIG_BUCKET_NAME = 'intentrank-config-test'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = 'http://' + AWS_STORAGE_BUCKET_NAME + '.s3.amazonaws.com/'
+STATIC_URL = '/static/'.format(WEBSITE_BASE_URL)
 COMPRESS_URL = STATIC_URL
+
+# or '.elasticbeanstalk.com'
+SESSION_COOKIE_DOMAIN = '.secondfunnel.com'
 
 STALE_TILE_QUEUE_NAME = 'tileservice-worker-queue'
 
@@ -129,11 +57,40 @@ BROKER_TRANSPORT_OPTIONS = {
 }
 CELERYBEAT_SCHEDULER = 'djcelery.schedulers.DatabaseScheduler'
 
-INSTAGRAM_CLIENT_ID = 'be95027932f64f4aaa465ffed160a8fb'
-INSTAGRAM_CLIENT_SECRET = 'aac059c1acb341d3b44b9139dc106dbe'
+# dict of queues by region to poll regularly, using celery beat.
+# corresponding handlers need to be imported in apps.api.tasks
+AWS_SQS_POLLING_QUEUES = {
+    'us-west-2': {
+        # https://willet.atlassian.net/browse/CM-125
+        'product-update-notification-queue':
+            {'queue_name': 'product-update-notification-queue',
+             'handler': 'handle_product_update_notification_message',
+             'interval': 5},
 
-TUMBLR_CONSUMER_KEY = 'Kr9rF0bykYZ2J2ncURUkwG0BBAEaDy7VGGzWZnVjna0bKPbRTn'
-TUMBLR_CONSUMER_SECRET = 'aLK2zeDeTbITx03iFSPntIBRV9EIel6pl2Sp9ARbyMIEykGHXF'
+        # https://willet.atlassian.net/browse/CM-126
+        'content-update-notification-queue':
+            {'queue_name': 'content-update-notification-queue',
+             'handler': 'handle_content_update_notification_message',
+             'interval': 5},
 
-GOOGLE_OAUTH2_CLIENT_ID = '218187505707-usif3u64gdr68rposjubk4461elk1e5c.apps.googleusercontent.com'
-GOOGLE_OAUTH2_CLIENT_SECRET = 'O_C-_zo1dXQQkg989-LwJNbt'
+        # https://willet.atlassian.net/browse/CM-127
+        'tile-generator-notification-queue':
+            {'queue_name': 'tile-generator-notification-queue',
+             'handler': 'handle_tile_generator_update_notification_message',
+             'interval': 5},
+
+        # https://willet.atlassian.net/browse/CM-128
+        'ir-config-generator-notification-queue':
+            {'queue_name': 'ir-config-generator-notification-queue',
+             'handler': 'handle_ir_config_update_notification_message'},
+
+        # https://willet.atlassian.net/browse/CM-124
+        'scraper-notification-queue':
+            {'queue_name': 'scraper-notification-queue',
+             'handler': 'handle_scraper_notification_message'},
+
+        'page-generator':
+            {'queue_name': 'page-generator',
+             'handler': 'handle_page_generator_notification_message'},
+    }
+}
