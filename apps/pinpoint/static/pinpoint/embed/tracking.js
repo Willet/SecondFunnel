8362a4c47dda845333eb61fb09682ebbe17de3f3
Nicholas Terwoord <nicholas.terwoord+code@gmail.com>
2014-03-10 15:03:53 -0400

    Add cookie code

--- apps/pinpoint/static/pinpoint/embed/tracking.js
+++ apps/pinpoint/static/pinpoint/embed/tracking.js
@@ -1,5 +1,34 @@
 (function(){
     try {
+        var createCookie, readCookie, eraseCookie;
+
+        // Cookie code from: http://www.quirksmode.org/js/cookies.html
+        createCookie = function (name,value,days) {
+            if (days) {
+                var date = new Date();
+                date.setTime(date.getTime()+(days*24*60*60*1000));
+                var expires = "; expires="+date.toGMTString();
+            }
+            else var expires = "";
+            document.cookie = name+"="+value+expires+"; path=/";
+        }
+
+        readCookie = function readCookie (name) {
+            var nameEQ = name + "=";
+            var ca = document.cookie.split(';');
+            for(var i=0;i < ca.length;i++) {
+                var c = ca[i];
+                while (c.charAt(0)==' ') c = c.substring(1,c.length);
+                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length,c.length);
+            }
+            return null;
+        }
+
+        eraseCookie = function (name) {
+            createCookie(name, "", -1);
+        }
+        // End cookie code
+
         // Load Perfect Audience
         (function() {
             window._pa = window._pa || {};
