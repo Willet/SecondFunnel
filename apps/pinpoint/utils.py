4732691feb77da786f2b192784cbdfaa005aa434
Christopher Papke <chris@willetinc.com>
2014-03-11 11:24:17 -0700

    added trailing comma

--- apps/pinpoint/utils.py
+++ apps/pinpoint/utils.py
@@ -125,7 +125,7 @@ def render_campaign(page_id, request, store_id=0):
         "ir_base_url": ir_base_url,
         "ga_account_number": settings.GOOGLE_ANALYTICS_PROPERTY,
         "url": page.get('url', ''),
-        "related_to_tile": request.GET.get('related', '')
+        "related_to_tile": request.GET.get('related', ''),
     }
 
     context = RequestContext(request, attributes)
