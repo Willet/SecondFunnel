616475d6085903b41b822686e6b42f14600141c6
Ford <ford.peprah@uwaterloo.ca>
2014-03-24 13:39:32 -0400

    Update upload script.

--- scripts/upload_to_cloudinary.py
+++ scripts/upload_to_cloudinary.py
@@ -1,9 +1,17 @@
 import os
 import sys
+import hashlib
+import httplib2
+import urlparse
 from itertools import chain
 
-from apps.imageservice.tasks import process_image_now
+import cloudinary.utils
+from django.conf import settings
+
+from apps.imageservice.utils import create_image
+from apps.pinpoint.utils import read_remote_file
 from apps.assets.models import ProductImage, Image
+from apps.imageservice.tasks import process_image_now
 
 
 def upload():
@@ -14,26 +22,49 @@ def upload():
     """
     secondfunnel_path = "http://images.secondfunnel.com/"
     result_list = list(chain(Image.objects.all(), ProductImage.objects.all()))
+    base_url = "http:{0}".format(settings.CLOUDINARY_BASE_URL)
 
     for img in result_list:
         source = img.url
         # Get the relative path to use with Cloudinary
         if secondfunnel_path not in source:
-            print "Skipping Image: %s" % source
+            print "Skipping Image, Already Processed: %s" % source
             continue
         path = source.replace(secondfunnel_path, "")
         path = os.path.join(*(path.split("/")[:-2]))
 
         # Process the image now
         try:
-            data = process_image_now(source, path)
+            image, _ = read_remote_file(source)
+            image = create_image(image) # create the image
+
+            # Determine the folder and get the appropriate cloudinary url
+            folder = hashlib.sha224(image.tobytes()).hexdigest()
+            folder = os.path.join(path, folder)
+            url, fragments = base_url, [
+                "/image/upload/",
+                folder,
+                "/master.{0}".format(image.format.lower().replace("jpeg", "jpg"))
+            ]
+
+            for fragment in fragments:
+                url += fragment
+
+            # Check for existance of the file
+            c = httplib2.Http()
+            response = c.request(url, "HEAD")
+            if response[0]['status'] < 400:
+                print "Skipping Image, Already Exists: %s" % source
+                img.url = url
+            else:
+                data = process_image_now(source, path)
+                print "Processed Image: %s" % source
+                img.url = data.get('url')
 
         except IOError:
             print "Failed to process Image: %s" % source
             continue
 
-        print "Processed Image: %s" % source
-        img.url = data.get('url')
         img.save()
 
     print "Successfully transfered to Cloudinary."
