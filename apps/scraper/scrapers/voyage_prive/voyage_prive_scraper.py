8cb5f047437411b4dc10ce246744a0c3f13807cf
Brian Lai <b2lai@uwaterloo.ca>
2014-04-21 14:22:13 -0400

    scrape all images on site

--- apps/scraper/scrapers/voyage_prive/voyage_prive_scraper.py
+++ apps/scraper/scrapers/voyage_prive/voyage_prive_scraper.py
@@ -25,8 +25,8 @@ class VoyagePriveCategoryScraper(ProductCategoryScraper):
             direct_site_name = node.find_element_by_xpath('./nom-fournisseur').text
             direct_site_image = node.find_element_by_xpath('./image-fournisseur').text
             try:
-                product = Product.objects.get(sku=sku)
-            except Product.DoesNotExist:
+                product = Product.objects.filter(sku=sku)[0]
+            except (Product.DoesNotExist, IndexError):
                 product = Product(sku=sku)
             product.store = self.store
             product.name = node.find_element_by_xpath('./titre').text
@@ -47,17 +47,35 @@ class VoyagePriveCategoryScraper(ProductCategoryScraper):
             })
             products.append(product)
             images.append(node.find_element_by_xpath('./image').text)
+
+        # stage 2 of VP scraping? load actual page
         print('loading url ' + url)
-        self.driver.get(url)
-        print('url loaded')
-        for i in range(len(products)):
-            product = products[i]
-            image = images[i]
-            try:
-                item = self.driver.find_element_by_xpath('//div[contains(div/h2/a/@href, "/{0}/")]'.format(product.sku))
-            except NoSuchElementException:
-                continue
-            product.save()
-            product.default_image = self._process_image(image, product)
-            product.save()
-            yield {'product': product}
+        for page in [url, url + '?p=2']:  # hack, gets pages 1 and 2
+            self.driver.get(page)
+            print('url loaded')
+            for i in range(len(products)):
+                product = products[i]
+                image = images[i]
+                try:
+                    # all 'products' have a link inside that contains the sku
+                    item = self.driver.find_element_by_xpath('//div[contains(div/h2/a/@href, "/{0}/")]'.format(product.sku))
+                except NoSuchElementException:
+                    pass  # no product image
+
+                try:
+                    # type unicode
+                    all_image_urls = [
+                        x.get_attribute('data-original')
+                        for x in self.driver.find_elements_by_xpath(
+                            '//div/figure/div[@data-id="{0}"]//li/a/img'.format(
+                                product.sku))
+                    ]
+                    product.product_images = [self._process_image(url, product)
+                                              for url in all_image_urls]
+                except NoSuchElementException:
+                    continue
+
+                product.save()
+                product.default_image = self._process_image(image, product)
+                product.save()
+                yield {'product': product}
