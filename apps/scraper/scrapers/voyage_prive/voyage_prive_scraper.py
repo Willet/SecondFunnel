ab6fe3ac9dcc566553f0b5072a7a62dc383de113
Brian Lai <b2lai@uwaterloo.ca>
2014-04-22 13:43:07 -0400

    change in_stock wrt feed

--- apps/scraper/scrapers/voyage_prive/voyage_prive_scraper.py
+++ apps/scraper/scrapers/voyage_prive/voyage_prive_scraper.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 import re
 
-from apps.assets.models import Product
+from apps.assets.models import Product, Store
 
 from apps.scraper.scrapers import ProductCategoryScraper
 from selenium.common.exceptions import NoSuchElementException
@@ -14,13 +14,16 @@ class VoyagePriveCategoryScraper(ProductCategoryScraper):
         return 'http://www.officiel-des-vacances.com/week-end'
 
     def scrape(self, url, **kwargs):
+        store = Store.objects.get(slug='voyage-prive')
         products = []
         images = []
+        skus = []  # list of product skus that are in the feed (which may or may not be in status (statut) 1
         print('loading url http://www.officiel-des-vacances.com/partners/catalog.xml')
         self.driver.get('http://www.officiel-des-vacances.com/partners/catalog.xml')
         print('url loaded')
         for node in self.driver.find_elements_by_xpath('//nodes/node'):
             sku = node.find_element_by_xpath('./id').text
+            skus.append(sku)
             direct_site_name = node.find_element_by_xpath('./nom-fournisseur').text
             direct_site_image = node.find_element_by_xpath('./image-fournisseur').text
             try:
@@ -39,14 +42,27 @@ class VoyagePriveCategoryScraper(ProductCategoryScraper):
 
             # in no position of the product name is 'jusqu'à' a useful term to keep
             product.name = product.name.replace(u", jusqu'à", '')
-            product.url = node.find_element_by_xpath('./url').text
+            product.name = product.name.strip(' ,')  # remove spaces, commas, ...
+
+            product.url = node.find_element_by_xpath('./url-detail').text
             product.price = u'€' + node.find_element_by_xpath('./prix').text
+
+            # this is the default
+            product.in_stock = (node.find_element_by_xpath('./statut').text is '1')
+
             product.attributes.update({
                 'direct_site_name': direct_site_name,
                 'direct_site_image': direct_site_image,
             })
             products.append(product)
             images.append(node.find_element_by_xpath('./image').text)
+
+        # mark all products not in feed as 'not in stock'
+        for product in Product.objects.filter(store=store):
+            if not product.sku in skus:
+                print "Product {0} no longer in stock!".format(product.sku)
+                product.in_stock = False
+                product.save()
 
         # stage 2 of VP scraping? load actual page
         print('loading url ' + url)
@@ -59,51 +75,51 @@ class VoyagePriveCategoryScraper(ProductCategoryScraper):
 
             for idx, product in enumerate(products):
                 image = images[idx]
-
-                if not str(product.sku) in page_str:
-                    print "product {0} is not on the page; skipping".format(product.sku)
-                    continue
-
-                try:
-                    # find the review for this product
-                    review_el = self.driver.find_element_by_xpath(
-                            '//div'
-                            '/div[contains(@class, "viewp-product-editorialist-{0}")]'
-                            '/blockquote'.format(product.sku))
-                    review_text = review_el.text or review_el.get_attribute('innerHTML')
-                    product.attributes.update({"review_text": review_text})
-                    print "Saved product {0} with review found".format(product.sku)
+                if image:  # save paired default image
                     product.save()
-                except NoSuchElementException:
-                    print "No review was found for product {0}".format(product.sku)
+                    try:
+                        product.default_image = self._process_image(image, product)
+                    except Exception as err:
+                        print "PV image CDN died"
 
-                try:
-                    # all 'products' have a link inside that contains the sku
-                    item = self.driver.find_element_by_xpath('//div[contains(div/h2/a/@href, "/{0}/")]'.format(product.sku))
-                    product.default_image = self._process_image(image, product)
-                    product.save()
-                except NoSuchElementException:
-                    print "product {0} is not on this page".format(product.sku)
+                # if product is on the page, provide product with extra information
+                # like images and reviews
+                if str(product.sku) in page_str:
+                    try:
+                        # find the review for this product
+                        review_el = self.driver.find_element_by_xpath(
+                                '//div'
+                                '/div[contains(@class, "viewp-product-editorialist-{0}")]'
+                                '/blockquote'.format(product.sku))
+                        review_text = review_el.text or review_el.get_attribute('innerHTML')
+                        product.attributes.update({"review_text": review_text})
+                        print "Saved product {0} with review found".format(product.sku)
+                        product.save()
+                    except NoSuchElementException:
+                        print "No review was found for product {0}".format(product.sku)
 
-                try:
-                    # type unicode
-                    all_image_urls = [x.get_attribute('data-original') for x in
-                                      self.driver.find_elements_by_xpath(
-                            '//div/figure/div[@data-id="{0}"]//li/a/img'.format(
-                                product.sku))]
-                    if all_image_urls:
-                        print "Adding more image urls to product: {0}".format(
-                            all_image_urls)
+                    try:  # type unicode
+                        all_image_urls = [x.get_attribute('data-original') for x in
+                                          self.driver.find_elements_by_xpath(
+                                '//div/figure/div[@data-id="{0}"]//li/a/img'.format(
+                                    product.sku))]
+                        if all_image_urls:
+                            print "Adding more image urls to product: {0}".format(
+                                all_image_urls)
 
-                        product.product_images = [
-                            self._process_image(image_url, product)
-                            for image_url in all_image_urls
-                        ]
-                    else:
-                        print "? found product on the page, but it had no images"
-                except NoSuchElementException:
-                    print "product images not found on the page"
-                    continue
+                            product.product_images = []
+                            for image_url in all_image_urls:
+                                try:
+                                    product.product_images.add(
+                                        self._process_image(image_url, product))
+                                except Exception as err:
+                                    print "PV image CDN died"
+
+                        else:
+                            print "? found product on the page, but it had no images"
+                    except NoSuchElementException:
+                        print "product images not found on the page"
+                        continue
 
                 product.save()
                 yield {'product': product}
