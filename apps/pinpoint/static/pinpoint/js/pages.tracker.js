fbb96cd81fc498c39da5c8cd0bb231ac1ec2a9d7
Nick Terwoord <nicholas.terwoord+code@gmail.com>
2014-05-08 18:20:35 -0400

    Fuck it, we're doing it live

--- apps/pinpoint/static/pinpoint/js/pages.tracker.js
+++ apps/pinpoint/static/pinpoint/js/pages.tracker.js
@@ -591,7 +591,7 @@ App.module("tracker", function (tracker, App) {
         // this (verbatim) code creates window.ga
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
         (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        m=s.getElementsByTagName(o)[0];a.async=0;a.src=g;m.parentNode.insertBefore(a,m)
         })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
     });
 
