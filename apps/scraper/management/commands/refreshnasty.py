506d360e6d7156a1972192479baee23794ad6587
DrTeePot <tristanpotter@rogers.com>
2014-05-21 16:21:37 -0400

    fixed tab problem, also new line at end

--- apps/scraper/management/commands/refreshnasty.py
+++ apps/scraper/management/commands/refreshnasty.py
@@ -12,14 +12,14 @@ class Command(BaseCommand):
         Otherwise, scrapes all urls listed in all text files in the urls folder.
         """
 
-    p = ProductImage.objects.filter(product__store=5)
-    for image in p:
-        delete_cloudinary_resource(get_public_id(image.url))
-        data = process_image(image.original_url, create_image_path(5),
-                             remove_background='#FFF')
-        image.url = data.get('url')
-        image.file_type = data.get('format')
-        image.dominant_color = data.get('dominant_colour')
-        # save the image
-        image.save()
-    print "finished"
+        p = ProductImage.objects.filter(product__store=5)
+        for image in p:
+            delete_cloudinary_resource(get_public_id(image.url))
+            data = process_image(image.original_url, create_image_path(5),
+                                 remove_background='#FFF')
+            image.url = data.get('url')
+            image.file_type = data.get('format')
+            image.dominant_color = data.get('dominant_colour')
+            # save the image
+            image.save()
+        print "finished"
