3960cae229b1bfb8b3bd9ed4da6cf871043b0256
Brian Lai <b2lai@uwaterloo.ca>
2014-05-22 17:55:47 -0400

    Prevent scraper from running if already scraping

--- apps/scraper/views.py
+++ apps/scraper/views.py
@@ -1,3 +1,5 @@
+import socket
+
 from django.contrib.auth.decorators import login_required
 from django.core.management import call_command
 from django.http import HttpResponse
@@ -7,19 +9,21 @@ from django.views.decorators.cache import never_cache
 from django.views.generic import View
 
 from apps.assets.models import Feed, Store
-from apps.utils import async
+from apps.utils import async, get_processes
 
 
 class ScrapeNowView(View):
     """Scrape a single url without going into Jenkins."""
     stores = Store.objects.all()
     feeds = Feed.objects.all()
+    hostname = socket.gethostname()
 
     @method_decorator(login_required)
     @method_decorator(never_cache)
     def get(self, request):
         """The "Add URL" UI"""
         return render_to_response('scraper/index.html', {
+            "hostname": self.hostname,
             "stores": self.stores,
             "feeds": self.feeds,
         })
@@ -29,8 +33,22 @@ class ScrapeNowView(View):
     def post(self, request):
         """The submission controller"""
 
+        attributes = {
+            "hostname": self.hostname,
+            "stores": self.stores,
+            "feeds": self.feeds,
+            "msg": "If valid, the URL will be scraped.",
+        }
+
         if not request.POST.get('store-id'):
             return HttpResponse(status=400)
+
+        running_processes = get_processes()
+        for process in running_processes:
+            if "phantomjs" in process:
+                attributes['msg'] = "The scraper is currently busy. " \
+                                    "Please try again later."
+                return render_to_response('scraper/index.html', attributes)
 
         kwds = {"store-id": request.POST.get('store-id')}
         if request.POST.get('url'):
@@ -40,11 +58,7 @@ class ScrapeNowView(View):
 
         self._run_scraper(**kwds)
 
-        return render_to_response('scraper/index.html', {
-            "stores": self.stores,
-            "feeds": self.feeds,
-            "msg": "If valid, the URL will be scraped."
-        })
+        return render_to_response('scraper/index.html', attributes)
 
     def _run_scraper(self, **kwds):
         @async
