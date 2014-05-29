d3b9d19475fc4632c389ba681d86a577021e6240
Brian Lai <b2lai@uwaterloo.ca>
2014-05-29 15:58:40 -0400

    add hardcoded conversion id/url

--- apps/tracking/templates/tracking/js/conversion-loader.js
+++ apps/tracking/templates/tracking/js/conversion-loader.js
@@ -2,6 +2,9 @@
 ;(function (window, document, undefined) {
     "use strict";
 
+    // conversion id for nastygal
+    window.WILLET_CONVERSION_ID = '271C54CB40964B26BD0593C4E24EF1C3';
+
     if (document === undefined) {
         // the thing that ran this script was not a browser.
         return;
@@ -9,7 +12,8 @@
 
     try {
         var scriptTag = document.createElement('script'),
-            scriptSrc = 'http://secondfunnel.com/tracker/conversion.js',
+            // load script from https domain
+            scriptSrc = 'https://s3-us-west-2.amazonaws.com/static-misc-secondfunnel/js/conversion/nastygal.js',
             firstScriptTag = document.getElementsByTagName('script')[0];
 
         scriptTag.async = 1;
