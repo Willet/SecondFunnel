e43c792131512b2a29f3c09d4188d954a10de0da
Brian Lai <b2lai@uwaterloo.ca>
2014-05-29 13:31:35 -0400

    refactoring

--- apps/tracking/static/tracking/js/nastygal-conversion.js
+++ apps/tracking/static/tracking/js/nastygal-conversion.js
@@ -1,114 +1,90 @@
-// jshint ignore: start
-var ClickMeter_conversion_value,
-    ClickMeter_conversion_id = '271C54CB40964B26BD0593C4E24EF1C3',
-    ClickMeter_conversion_commission = '0.00',
-    ClickMeter_conversion_parameter = 'empty',
-    convId;
+/*global window, document, undefined*/
+/**
+ * "indicating we did not write this garbage :)"
+ */
+;(function (window, document, undefined) {
+    "use strict";
+    var guessPrice,
+        loadPage;
 
-function guessPrice() {
-    var price;
-
-    if (window.s_amc_hb) {
-        try {
-            price = window.s_amc_hb.contextData.hb_orderData_totalPrice.toString();
-        } catch (e) {}
-        try {
-            if (!price) {
-                price = window.s_amc_hb.contextData.hb_cartData_totalPrice.toString();
-            }
-        } catch (e) {}
-        try {
-            if (!price) {
-                price = window.s_amc_hb.hb_orderData_totalPrice.toString();
-            }
-        } catch (e) {}
-        try {
-            if (!price) {
-                price = window.s_amc_hb.hb_orderData_totalPrice.toString();
-            }
-        } catch (e) {}
+    if (document === undefined) {
+        // the thing that ran this script was not a browser.
+        return;
     }
-    if (window.dataLayer && window.dataLayer.length) {
-        try {
-            if (!price) {
-                price = window.dataLayer[0].transactionTotal.toString();
+
+    guessPrice = function () {
+        var price,
+            hybrisVars = window.s_amc_hb;
+
+        if (hybrisVars) {
+            price = hybrisVars.hb_orderData_totalPrice ||
+                    hybrisVars.hb_cartData_totalPrice;
+            if (!price && hybrisVars.contextData) {
+                price = hybrisVars.contextData.hb_orderData_totalPrice ||
+                        hybrisVars.contextData.hb_cartData_totalPrice;
             }
-        } catch (e) {}
-    }
-    return price;
-}
-
-function getParam(name) {
-    name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-    var regexS = "[\\?&]" + name + "=([^&#]*)";
-    var regex = new RegExp(regexS);
-    var scripts = document.getElementsByTagName('script');
-    var index = scripts.length - 1;
-    var myScript = scripts[index];
-    var results = regex.exec(myScript.src.toLowerCase());
-    if (results == null) {
-        return "";
-    } else {
-        return results[1];
-    }
-}
-
-function loadPage() {
-    var params = '';
-    if (typeof ClickMeter_conversion_value !== 'undefined' && ClickMeter_conversion_value != null) {
-        var convValue = parseFloat(ClickMeter_conversion_value.replace(',', '.'));
-        if (!isNaN(convValue)) {
-            params = '&val=' + convValue;
+        } else if (window.dataLayer && window.dataLayer.length >= 1) {
+            price = window.dataLayer[0].transactionTotal;
         }
-    }
-    if (typeof ClickMeter_conversion_parameter !== 'undefined' && ClickMeter_conversion_parameter != null && ClickMeter_conversion_parameter != '') {
-        params = params + '&param=' + ClickMeter_conversion_parameter;
-    }
-    if (typeof ClickMeter_conversion_id !== 'undefined' && ClickMeter_conversion_id != null && ClickMeter_conversion_id != '') {
-        convId = ClickMeter_conversion_id;
-    }
-    var panel = document.createElement('div');
-    panel.setAttribute('id', 'conversion_container');
-    panel.style.height = '0px';
-    panel.style.width = '0px';
-    panel.style.overflow = 'hidden';
 
-    var iFrame = document.createElement('iframe');
+        try {
+            return price.toString();
+        } catch (e) {}
+        return "0.00";
+    };
 
-    if ("https:" === document.location.protocol) {
-        iFrame.setAttribute('src', 'https://clickmeter.com/conversion.aspx?id=' + convId + params);
-    } else {
-        iFrame.setAttribute('src', 'http://clickmeter.com/conversion.aspx?id=' + convId + params);
-    }
+    loadPage = function () {
+        var clickMeterConversionValue = guessPrice(),
+            clickMeterConversionId = '271C54CB40964B26BD0593C4E24EF1C3',
+            clickMeterConversionParameter = 'empty',
+            conversionBase = 'http://clickmeter.com/conversion.aspx',
+            convValue,
+            iframe = document.createElement('iframe'),
+            index,
+            myScript,
+            panel = document.createElement('div'),
+            params = '',
+            scripts;
 
-    iFrame.setAttribute('frameborder', '0');
-    iFrame.setAttribute('height', '0');
-    iFrame.setAttribute('width', '0');
-    iFrame.setAttribute('noresize', '0');
-    iFrame.setAttribute('id', 'convframe');
-    iFrame.setAttribute('name', 'convframe');
-    iFrame.setAttribute('allowtransparency', 'true');
-    var scripts = document.getElementsByTagName('script');
-    var index = scripts.length - 1;
-    var myScript = scripts[index];
-    panel.appendChild(iFrame);
-    myScript.parentNode.appendChild(panel);
-    if (typeof ClickMeter_conversion_value !== 'undefined') {
-        ClickMeter_conversion_value = null;
-    }
-    if (typeof ClickMeter_conversion_parameter !== 'undefined') {
-        ClickMeter_conversion_parameter = null;
-    }
-    if (typeof ClickMeter_conversion_id !== 'undefined') {
-        ClickMeter_conversion_id = null;
-    }
-}
+        if (clickMeterConversionValue !== null) {
+            convValue = parseFloat(clickMeterConversionValue.replace(',', '.'));
+            if (!isNaN(convValue)) {
+                params = '&val=' + convValue;
+            }
+        }
+        params = params + '&param=' + clickMeterConversionParameter;
 
-ClickMeter_conversion_value = guessPrice();
-ClickMeter_conversion_id = '271C54CB40964B26BD0593C4E24EF1C3';
-ClickMeter_conversion_commission = '0.00';
-ClickMeter_conversion_parameter = 'empty';
-convId = getParam('id');
+        panel.setAttribute('id', 'conversion_container');
+        panel.style.height = '0px';
+        panel.style.width = '0px';
+        panel.style.overflow = 'hidden';
 
-loadPage();
-// jshint ignore: end
+        if ("https:" === document.location.protocol) {
+            iframe.setAttribute('src', conversionBase + '?id=' +
+                clickMeterConversionId + params);
+        } else {
+            iframe.setAttribute('src', conversionBase + '?id=' +
+                clickMeterConversionId + params);
+        }
+
+        iframe.setAttribute('frameborder', '0');
+        iframe.setAttribute('height', '0');
+        iframe.setAttribute('width', '0');
+        iframe.setAttribute('noresize', '0');
+        iframe.setAttribute('id', 'convframe');
+        iframe.setAttribute('name', 'convframe');
+        iframe.setAttribute('allowtransparency', 'true');
+
+        scripts = document.getElementsByTagName('script');
+        index = scripts.length - 1;
+        myScript = scripts[index];
+        panel.appendChild(iframe);
+        myScript.parentNode.appendChild(panel);
+    };
+
+    try {
+        loadPage();
+    } catch (err) {
+        // #fail
+    }
+}(window, window && window.document));
