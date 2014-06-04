8fbb3e9ab64527b6359c281723603304ac431b67
Nick Terwoord <nicholas.terwoord+code@gmail.com>
2014-06-04 14:44:22 -0400

    Scrapy - Port NastyGal Scraper
    Ports the existing NastyGal scraper to Scrapy.
    
    (Will be porting the old scrapers over one-by-one)

---
+++ apps/scrapy/spiders/nastygal.py
@@ -0,0 +1,46 @@
+from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
+from scrapy.contrib.spiders import Rule
+from scrapy.selector import Selector
+from scrapy_webdriver.http import WebdriverRequest
+from urlparse import urlparse
+from apps.scrapy.items import ScraperProduct
+from apps.scrapy.spiders.webdriver import SecondFunnelScraper, WebdriverCrawlSpider
+from apps.scrapy.utils.itemloaders import ScraperProductLoader
+
+
+class NastyGalSpider(SecondFunnelScraper, WebdriverCrawlSpider):
+    name = 'nastygal'
+    allowed_domains = ['nastygal.com']
+    start_urls = ['http://www.nastygal.com/']
+    start_urls_separator = '|'
+    rules = [
+        Rule(
+            SgmlLinkExtractor(restrict_xpaths='//a[@class="product-link"]'),
+            'parse_product', follow=False
+        )
+    ]
+
+    store_slug = name
+
+    def __init__(self, *args, **kwargs):
+        super(NastyGalSpider, self).__init__(*args, **kwargs)
+
+    # For some reason, Always defaults to regular requests...
+    # So, we override...
+    def start_requests(self):
+        return [WebdriverRequest(url) for url in self.start_urls]
+
+    def parse_product(self, response):
+        sel = Selector(response)
+
+        l = ScraperProductLoader(item=ScraperProduct(), response=response)
+        l.add_css('url', 'link[rel="canonical"]::attr(href)')
+        l.add_css('sku', '.product-style::text', re='Style #:(\d+)')
+        l.add_css('name', 'h1.product-name::text')
+        l.add_css('price', '.current-price::text', re='\$(.*)')
+        l.add_value('in_stock', True)
+
+        l.add_css('description', '.product-description')
+        l.add_css('image_urls', '.carousel img::attr(src)')
+
+        yield l.load_item()
