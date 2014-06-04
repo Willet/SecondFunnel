ebebdfb5565b0b5f5593a426d1af3349d3352d1a
Nick Terwoord <nicholas.terwoord+code@gmail.com>
2014-06-04 16:43:00 -0400

    Remove unused Gap methods, rules

--- apps/scrapy/spiders/gap.py
+++ apps/scrapy/spiders/gap.py
@@ -1,5 +1,3 @@
-from urlparse import urljoin
-from scrapy import log
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from scrapy.contrib.spiders import Rule
 from scrapy.selector import Selector
@@ -14,12 +12,6 @@ class GapSpider(SecondFunnelScraper, WebdriverCrawlSpider):
     allowed_domains = ['gap.com']
     start_urls = ['http://www.gap.com/']
     rules = [
-        # Rule(SgmlLinkExtractor(allow=[
-        #     r'\/browse\/subDivision.do\?.*?cid=\d+'
-        # ]), 'parse_division'),
-        # Rule(SgmlLinkExtractor(allow=[
-        #     r'\/browse\/category.do\?.*?cid=\d+'
-        # ]), 'parse_category'),
         Rule(SgmlLinkExtractor(allow=[
             r'/browse/product.do\?.*?pid=\d+'
         ]), 'parse_product', follow=False)
@@ -66,36 +58,6 @@ class GapSpider(SecondFunnelScraper, WebdriverCrawlSpider):
             urls.append(WebdriverRequest(url))
 
         return urls
-
-    def parse_division(self, response):
-        sel = Selector(response)
-        base_url = response.url
-
-        divisions = sel.css('.category > a')
-
-        for division in divisions:
-            url = division.css('::attr(href)').extract_first().strip()
-            text = division.css('::text').extract_first().strip()
-            msg = u'PARSE_DIVISION - {category}: {url}'.format(url=url,
-                                                               category=text)
-            log.msg(msg, level=log.DEBUG)
-
-            yield WebdriverRequest(urljoin(base_url, url))
-
-    def parse_category(self, response):
-        sel = Selector(response)
-        base_url = response.url
-
-        products = sel.css('.productCatItem > a')
-
-        for product in products:
-            url = product.css('::attr(href)').extract_first().strip()
-            text = product.css('::text').extract_first().strip()
-            msg = u'PARSE CATEGORY - {product}: {url}'.format(url=url,
-                                                              product=text)
-            log.msg(msg, level=log.DEBUG)
-
-            yield WebdriverRequest(urljoin(base_url, url))
 
     def parse_product(self, response):
         sel = Selector(response)
