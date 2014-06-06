34c5a493c3fa337dfebd1fa9c78f0d2ebe1182d2
Nick Terwoord <nicholas.terwoord+code@gmail.com>
2014-06-06 17:10:09 -0400

    Cleanup SurLaTable parsing

--- apps/scrapy/spiders/surlatable.py
+++ apps/scrapy/spiders/surlatable.py
@@ -21,7 +21,7 @@ class SurlatableSpider(SecondFunnelCrawlScraper, WebdriverCrawlSpider):
     store_slug = name
 
     def is_product_page(self, response):
-        is_product_page = re.search('(\d+)', response.url)
+        is_product_page = re.search('.*product/PRO-.*', response.url)
         return is_product_page
 
     def parse_product(self, response):
@@ -35,41 +35,31 @@ class SurlatableSpider(SecondFunnelCrawlScraper, WebdriverCrawlSpider):
         """
 
         sel = Selector(response)
-
         l = ScraperProductLoader(item=ScraperProduct(), response=response)
         l.add_value('url', response.url)
-
-        # http://doc.scrapy.org/en/latest/topics/loaders.html#scrapy.contrib.loader.ItemLoader.add_value
-        l.add_value("sku", response.url, re='(\d+)')
-        sku = l.get_output_value("sku")
-
         l.add_css('name', 'h1.name::text')
-
-        # TODO: Sanitize output with bleach
         l.add_css('description', '#description span.boxsides')
+        l.add_value("sku", response.url, re='(\d+)')
 
         attributes = {}
 
-        try:
-            sale_price = sel.css('li.sale::text')[1].extract().strip()
-            price = sel.css('li.regular::text')[0].extract().replace('\n', '').replace('\t', '').replace('Was:', '')
+        sale_price = sel.css('li.sale::text').re_first('(\$\d+\.\d+)')
+
+        if sale_price:
             attributes['sale_price'] = sale_price
-        except IndexError:
-            price = sel.css('li.price::text')[-1].extract().replace('\n', '').replace('\t', '')
-
-        # some variable-price like "$10 - $20"...
-        if '-' in price:
-            price = price[:price.index("-")].strip()
-
-        l.add_value('price', price)
+            l.add_css('price', 'li.regular::text', re='(\$\d+\.\d+)')
+        else:
+            l.add_css('price', 'li.price::text', re='(\$\d+\.\d+)')
 
         # TODO: more Image URLs
         image_urls = [
             # this is unfortunately reverse-engineered (and 1079 seems to be a magic number)
-            "http://www.surlatable.com//images/customers/c1079/PRO-{sku}/PRO-{sku}_detail/main_variation_Default_view_1_426x426.jpg".format(sku=sku)
+            "http://www.surlatable.com//images/customers/c1079/PRO-{sku}/PRO-{sku}_detail/main_variation_Default_view_1_426x426.jpg".format(
+                sku=l.get_output_value("sku")
+            )
         ]
-        l.add_value('image_urls', image_urls)
 
+        l.add_value('image_urls', image_urls)
         l.add_value('attributes', attributes)
 
         yield l.load_item()
