5690a216577fad095577dab081b23875908ead70
Alex Riedler <alex@riedler.ca>
2014-06-18 11:51:20 -0400

    cleanup hipchat script

--- scripts/notifyhipchat.py
+++ scripts/notifyhipchat.py
@@ -1,24 +1,39 @@
 #!/usr/bin/env python
 
-import os, sys
+import os
+import sys
 
 import hipchat
 
+HIPCHAT_API_TOKEN = "675a844c309ec3227fa9437d022d05"
+HIPCHAT_CODE_ROOM_ID = 115122
+HIPCHAT_DEV_ROOM_ID = 115128
 
-def hipchat_broadcast(by='HipChat', message='Hello, World!', room_id=115122):
+
+def hipchat_broadcast(by='HipChat', message='Hello, World!', room_id=HIPCHAT_CODE_ROOM_ID):
     """Says random stuff on our Hipchat boards."""
-    hip = hipchat.HipChat(token="675a844c309ec3227fa9437d022d05")
-    hip.method("rooms/message", method="POST",
-               parameters={"room_id": room_id, "from": by,
-                           "message": message, "message_format": "text"})
-
+    hip = hipchat.HipChat(token=HIPCHAT_API_TOKEN)
+    hip.method(
+        "rooms/message",
+        method="POST",
+        parameters={
+            "room_id": room_id,
+            "from": by,
+            "message": message,
+            "message_format": "text"}
+    )
 
 where = os.getenv('PARAM1')
 if len(sys.argv) > 3:
     hipchat_broadcast(by=sys.argv[1], message=sys.argv[2], room_id=sys.argv[3])
 elif where == 'TEST':
-    hipchat_broadcast(by="Philip", message="(notsureif) New version on %s?" % (where),
-                      room_id=115128)  # code
+    hipchat_broadcast(
+        by="Philip",
+        message="(notsureif) New version on %s?" % (where),
+        room_id=HIPCHAT_DEV_ROOM_ID
+    )
 else:
-    hipchat_broadcast(by="Hubert", message="(goodnews) New version on %s!" % (
-        where if where else "PRODUCTION"))
+    hipchat_broadcast(
+        by="Hubert",
+        message="(goodnews) New version on %s!" % (where if where else "PRODUCTION")
+    )
