2aa0bda0fe1c3ca41cbb2383f12e78d19015a63e
Alex Riedler <alex@riedler.ca>
2014-07-25 22:07:45 -0400

    Some small fixes to the ansible playbooks.

--- ansible/userdata.sh
+++ ansible/userdata.sh
@@ -4,12 +4,6 @@
 # setup the below environment variables and set on AMI's
 # within AWS Console
 
-echo "
-Host github.com
-  ForwardAgent yes
-  StrictHostKeyChecking no
-" >> ~/.ssh/config
-
 APP_ENVIRONMENT=production
 PLAYBOOK=ansible/webservers.yml
 TAGS=deploy
@@ -25,17 +19,15 @@ eval `ssh-agent`
 # add the deploy key to the running ssh-agent
 ssh-add $DEPLOY_KEY
 
-# add github.com as a known_hosts (so we can ansible-playbook against it)
-ssh-keyscan -t rsa,dsa github.com 2>&1 | sort -u - ~/.ssh/known_hosts > ~/.ssh/tmp_hosts
-cat ~/.ssh/tmp_hosts >> ~/.ssh/known_hosts
-
+# create temporary environment file
 echo -e "[$APP_ENVIRONMENT]\nlocalhost" > $INVENTORY_FILE
 
 # git clone the repo (for ansible playbooks),
-# and then run the ansible playbook
+# and then run the ansible playbook against the localhost
 cd ~/
 git clone --branch $GIT_BRANCH --single-branch --depth 1 $GIT_REPO $REPO_NAME
 cd $REPO_NAME
 ansible-playbook -i $INVENTORY_FILE -e app_environment=$APP_ENVIRONMENT $PLAYBOOK -t $TAGS
-cd ~/
+
+# cleanup
 rm -rf SecondFunnel $INVENTORY_FILE
