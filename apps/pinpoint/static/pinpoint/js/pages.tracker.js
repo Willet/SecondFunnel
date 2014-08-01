0710265802734fbe9f6de14f8eb385e2f27ed395
DrTeePot <tristanpotter@rogers.com>
2014-08-01 12:03:11 -0400

    Changed default property to umbrella

--- apps/pinpoint/static/pinpoint/js/pages.tracker.js
+++ apps/pinpoint/static/pinpoint/js/pages.tracker.js
@@ -92,12 +92,6 @@ App.module("tracker", function (tracker, App) {
                 }
             }
 
-            // if gap sends us too many visitors --> track tiles less often.
-            // 0.2 is arbitrary
-            if (App.option('store:slug', '').toLowerCase() === 'gap' &&
-                Math.random() < 0.2) {
-                return $.Deferred().promise();
-            }
             return $.ajax({
                 'url': App.option('IRSource') + '/page/' +
                        App.option('page:id') + '/tile/' + interactionType,
