ad965c1f5081ad978a87be4bca8cc10883fe186c
Alex Lang <wilde.jagd@hotmail.com>
2014-10-09 17:33:53 -0400

    removed garbage

--- apps/scrapy/spiders/aeropostale.py
+++ apps/scrapy/spiders/aeropostale.py
@@ -30,61 +30,24 @@ class AeropostaleSpider(SecondFunnelCrawlScraper, WebdriverCrawlSpider):
 
         return sel.css('#productPage')
 
-    # def parse_start_url(self, response):
-    #     if self.is_product_page(response):
-    #         return self.parse_product_html(response)
-    #     else:
-    #         return self.parse_xml(response)
-    
-    # def parse_xml(self, response):
-    #     print "parsing xml"
-    #     sel = Selector(response)
-
-    #     entries = sel.css('entry')
-    #     print len(entries), "entries"
-        
-    #     for entry in entries:
-    #         l = ScraperProductLoader(item=ScraperProduct(), response=response)
-
-    #         attributes = {}
-    #         gender = entry.css('gender::text').extract()
-    #         if gender:
-    #             attributes['categories'] = {
-    #                 "female": ["girls"],
-    #                 "male": ["boys"]
-    #             }.get(gender[0], [])
-
-    #         given_url = entry.css('link::attr(href)').extract()[0]
-    #         id = urlparse.parse_qs(urlparse.urlparse(given_url).query)['entry'][0]
-    #         url = self.product_base_url.format(id=id)
-
-    #         request = WebdriverRequest(url, callback=self.parse_product)
-    #         request.meta['attributes'] = attributes
-
-    #         yield request
-
     def parse_product(self, response):
-        print 1
         sel = Selector(response)
 
         l = ScraperProductLoader(item=ScraperProduct(), response=response)
         # attributes = response.meta.get('attributes', {})
         
         sku = urlparse.parse_qs(urlparse.urlparse(response.url).query)['productId'][0]
-        print 2
 
         l.add_css('name', '.right h2::text')
         l.add_css('price', '.price .now::text', re='(\d+)')
         l.add_value('sku', sku)
         l.add_css('details', '.product-description')
         l.add_css('url', 'link[rel="canonical"]::attr(href)')
-        print 3
 
         base_img = self.root_url + sel.css('img.zoom::attr(src)').extract()[0]
         colors = sel.css('ul.swatches.clearfix li img::attr(src)').re('-(\d+)_')
         img_urls = [re.sub('-\d+enh', '-'+color+'enh', base_img) for color in colors]
         l.add_value('image_urls', img_urls)
-        print 4
 
         attributes = {}
         
@@ -96,18 +59,15 @@ class AeropostaleSpider(SecondFunnelCrawlScraper, WebdriverCrawlSpider):
             categories.append('girls')
         elif genders[1].css('current'):
             categories.append('boys') 
-        print 5
  
         if float(l.get_output_value('price')) < 10:
             categories.append('under $10')
         elif float(l.get_output_value('price')) < 20:
             categories.append('under $20')
-        print 6
 
         gender = sel.css('.hasChildren.active a::text').extract()
         if gender:
             categories.append(gender[0])
-        print 7
 
         attributes['categories'] = categories
 
