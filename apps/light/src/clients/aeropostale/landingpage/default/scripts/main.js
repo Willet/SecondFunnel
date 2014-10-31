feb2db5483dcd7716b0ca4da22760d47fbe04aa9
Alex Lang <wilde.jagd@hotmail.com>
2014-10-31 15:18:55 -0400

    Added padding to shop-now button if not on windows

--- apps/light/src/clients/aeropostale/landingpage/default/scripts/main.js
+++ apps/light/src/clients/aeropostale/landingpage/default/scripts/main.js
@@ -4,3 +4,8 @@ var Backbone = require('backbone');
 var Marionette = require('backbone.marionette');
 var bootstrap = require('bootstrap.dropdown'); // for menu-bar drop down on mobile
 require('landingpage');
+(function() {
+    if (navigator.platform.toLowerCase().indexOf('windows') == -1) {
+        $(".tile .buy .button, .previewContainer .buy .button").css({'padding-top': '18px'});
+    }
+})()
