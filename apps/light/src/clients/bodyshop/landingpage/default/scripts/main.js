a53193098c1cb7749c98267b98e702b276a52042
Fraser Harris <fraser.harris@gmail.com>
2014-11-19 14:24:59 -0800

    body shop

---
+++ apps/light/src/clients/bodyshop/landingpage/default/scripts/main.js
@@ -0,0 +1,6 @@
+var $ = require('jquery');
+var _ = require('underscore');
+var Backbone = require('backbone');
+var Marionette = require('backbone.marionette');
+var bootstrap = require('bootstrap.dropdown'); // for menu-bar drop down on mobile
+require('landingpage');
