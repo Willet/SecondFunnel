78ffa6cfc6b535474e1a192affecfdcdc345baca
Fraser Harris <fraser.harris@gmail.com>
2014-11-19 23:34:34 -0800

    Fixed all the Shop Now buttons with UTM params

--- apps/light/src/clients/aeropostale/landingpage/default/scripts/main.js
+++ apps/light/src/clients/aeropostale/landingpage/default/scripts/main.js
@@ -50,6 +50,28 @@ App.module('core', require('./views'));
             ].join(" "));
     }
 
+    App.utils.addUrlTrackingParameters = function (url) {
+        var trackingCode = {
+            'for-her':                 'for_her',
+            'for-him':                 'for_him',
+            'under-$10':               'under_10',
+            'under-$10|girls':         'under_10_for_her',
+            'under-$10|guys':          'under_10_for_him',
+            'under-$20':               'under_20',
+            'under-$20|girls':         'under_20_for_her',
+            'under-$20|guys':          'under_20_for_him',
+            'stocking-stuffers':       'stocking_stuffers',
+            'stocking-stuffers|girls': 'stocking_stuffers_for_her',
+            'stocking-stuffers|guys':  'stocking_stuffers_for_him'
+        }
+        var params = { 
+            'utm_source': 'giftguide',
+            'utm_medium': 'site',
+            'utm_campaign': trackingCode[ App.intentRank.options.category ]
+        }
+        return ( url + (_.indexOf(url,'?') ? '&' : '?') + $.param(params) );
+    };
+
     // ### Aero mobile nav ###
     // requires a new change category function because sub-categories
     // are categories, not filters on categories
