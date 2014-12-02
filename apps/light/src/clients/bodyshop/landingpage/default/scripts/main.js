8bdf3ac948c39a3a8356ef726708793a8077d371
Fraser Harris <fraser.harris@gmail.com>
2014-12-01 16:07:52 -0800

    Added body shop UTM params

--- apps/light/src/clients/bodyshop/landingpage/default/scripts/main.js
+++ apps/light/src/clients/bodyshop/landingpage/default/scripts/main.js
@@ -13,6 +13,7 @@ var Page = require('landingpage'),
     App = Page.App;
 
 // Import client customizations
+App.module('utils', require('./utils'));
 App.module('core', require('./views'));
 
 // Run Application
@@ -28,21 +29,4 @@ App.start();
     catArea.append( $snowflakes.append( catArea.children().detach() ) );
     // Make category area sticky
     catArea.waypoint('sticky');
-
-    App.utils.addUrlTrackingParameters = function (url) {
-        var trackingCode = {
-            'for-her':                 'for_her',
-            'for-him':                 'for_him',
-            'under-$10':               'under_10',
-            'under-$20':               'under_20',
-            'stocking-stuffers':       'stocking_stuffers'
-        };
-        var params = { 
-            'utm_source': 'giftguide',
-            'utm_medium': 'site',
-            'utm_campaign': trackingCode[ App.intentRank.options.category ] || 'for_her'
-        };
-
-        return App.utils.urlAddParams(url, params);
-    };
 }());
