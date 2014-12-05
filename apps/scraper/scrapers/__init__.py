9a3c0bc77fbd0868e71d4fff87b63f402d2a2b76
Alex Lang <wilde.jagd@hotmail.com>
2014-12-05 17:26:09 -0500

    delete references to previously deleted crap

--- apps/scraper/scrapers/__init__.py
+++ apps/scraper/scrapers/__init__.py
@@ -1,6 +1,4 @@
 from apps.scraper.scrapers.scraper import Scraper, ProductScraper, ProductDetailScraper, ProductCategoryScraper, ContentScraper, ContentDetailScraper, ContentCategoryScraper
-from apps.scraper.scrapers.content.pinterest_scraper import PinterestPinScraper, PinterestAlbumScraper
 from apps.scraper.scrapers.gap.gap_stl_scraper import STLScraper
 from apps.scraper.scrapers.gap.gap_product_scrapers import GapProductScraper, GapCategoryScraper
 from apps.scraper.scrapers.gap.styldby_scraper import StyldByFilterScraper, StyldByPartnersScraper
-from apps.scraper.scrapers.voyage_prive.voyage_prive_scraper import VoyagePriveCategoryScraper, VoyagePriveWeekEndScraper, VoyagePriveSejourScraper
