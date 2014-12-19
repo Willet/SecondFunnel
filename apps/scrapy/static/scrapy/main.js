7827a657e6ade65e3331dc944a05939f62533347
Alex Lang <wilde.jagd@hotmail.com>
2014-12-19 13:50:43 -0500

    spacing

--- apps/scrapy/static/scrapy/main.js
+++ apps/scrapy/static/scrapy/main.js
@@ -1,56 +1,56 @@
 var a = 1;
 
 var run = function() {
-	var csv = $('#pseudo-spreadsheet').val();
-	var lines = csv.split('\n');
-	lines = lines.filter(function(x){
-		return x.trim().length > 0;
-	});
-	var delim = $('#delimiter').val();
+    var csv = $('#pseudo-spreadsheet').val();
+    var lines = csv.split('\n');
+    lines = lines.filter(function(x){
+        return x.trim().length > 0;
+    });
+    var delim = $('#delimiter').val();
 
-	var categories = {}
+    var categories = {}
 
-	var warning = $(".warning");
-	warning.text("");
+    var warning = $(".warning");
+    warning.text("");
 
-	for (var i=0; i<lines.length; i++) {
-		line = lines[i].split(delim);
+    for (var i=0; i<lines.length; i++) {
+        line = lines[i].split(delim);
 
-		// validation
-		if (line.length != 3) {
-			warning.text("<- Look you fool");
-			return;
-		}
+        // validation
+        if (line.length != 3) {
+            warning.text("<- Look you fool");
+            return;
+        }
 
-		var url = line[0].trim(),
-		    cat = line[1].trim(),
-		    priority = line[2].trim();
+        var url = line[0].trim(),
+            cat = line[1].trim(),
+            priority = line[2].trim();
 
-		categories[cat] = categories[cat] || []
-		categories[cat].push(url);
-	}
- 	console.log(encodeURIComponent(JSON.stringify(categories[cat])));
-	for (cat in categories) {
-		console.warn(1);
-		$.ajax({
-			url: "scrape",
-			type: "GET",
-			data: {
-				'category': cat,
-				'urls': encodeURIComponent(JSON.stringify(categories[cat]))
-			},
-			//dataType: "json",  // expected RETURN TYPE! (not type of "data" param)
-			success: function(data, status) {
-				console.warn(2);
-				console.warn(status);
-				console.warn(data);
-				a = JSON.parse(data);
-			},
-			error: function(obj, status, error) {
-				console.warn(obj);
-				console.warn(status);
-				console.warn(error);
-			}
-		})
-	}
+        categories[cat] = categories[cat] || []
+        categories[cat].push(url);
+    }
+     console.log(encodeURIComponent(JSON.stringify(categories[cat])));
+    for (cat in categories) {
+        console.warn(1);
+        $.ajax({
+            url: "scrape",
+            type: "GET",
+            data: {
+                'category': cat,
+                'urls': encodeURIComponent(JSON.stringify(categories[cat]))
+            },
+            //dataType: "json",  // expected RETURN TYPE! (not type of "data" param)
+            success: function(data, status) {
+                console.warn(2);
+                console.warn(status);
+                console.warn(data);
+                a = JSON.parse(data);
+            },
+            error: function(obj, status, error) {
+                console.warn(obj);
+                console.warn(status);
+                console.warn(error);
+            }
+        })
+    }
 }
