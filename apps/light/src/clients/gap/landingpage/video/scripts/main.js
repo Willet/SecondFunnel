cad483a5857fcf0d83b158f6f8e902d160fb77b7
mdgnkm <ys.madigankim@gmail.com>
2015-01-27 11:46:11 -0500

    Created video files for gap from defaults.

---
+++ apps/light/src/clients/gap/landingpage/video/scripts/main.js
@@ -0,0 +1,15 @@
+/**
+ *  GAP PAGE
+ **/
+"use strict";
+
+var $ = require('jquery');
+var _ = require('underscore');
+var waypoints = require("jquery-waypoints") // register $.fn.waypoint
+
+var Page = require('landingpage'),
+    App = Page.App;
+
+// Run Application
+App.init.initialize();
+App.start();
