1141cc3ac9a932ba41646bf1dda7c96688be5c3a
Fraser Harris <fraser.harris@gmail.com>
2015-02-03 04:11:18 -0800

    Gap SpringIsWeird modifications

---
+++ apps/light/src/clients/gap/landingpage/video/scripts/core.views.coffee
@@ -0,0 +1,16 @@
+"use strict"
+
+module.exports = (module, App, Backbone, Marionette, $, _) ->
+    module.HeroContent.prototype.events =
+        'click #more-button': ->
+            @$("#more-button").attr("style", "display: none;")
+            table = @$(".thumbnail-table")[0]
+            thumbnailTemplate = "<td><div class='thumbnail-item'>
+                    <div class='thumbnail-image' style='background-image: url(\"<%= thumbnail.url %>\");'></div>
+                    <p>Episode <%= i + 1 %> <br><%= thumbnail.date %></p>
+                </div></td>"
+            if table
+                for thumbnail, i in @model.attributes.thumbnails when i >= 2
+                    thumbnailElem = _.template(thumbnailTemplate, { "thumbnail" : thumbnail, "i" : i })
+                    table.insertRow(-1).innerHTML = thumbnailElem
+            return
---
+++ apps/light/src/clients/gap/landingpage/video/scripts/utils.js
@@ -0,0 +1,44 @@
+"use strict";
+
+require('jquery-deparam');
+
+module.exports = function (module, App, Backbone, Marionette, $, _) {
+    /**
+     *
+     * @param {string} url
+     *
+     * @returns {string} url
+     */
+    module.generateAdClickUrl = function (dest_url) {
+        var redirect_url = App.option('clickUrl');
+        redirect_url += encodeURIComponent(dest_url);
+        return redirect_url;
+    };
+
+    /**
+     * ALL CLICKS TO EXTERNAL URLS SHOULD GO THROUGH THIS FUNCTION
+     * 
+     * Opens url in correct window w/ tracking parameters appended & Emits tracking event
+     *
+     * @param {string} url
+     */
+    module.openUrl = function (targetUrl) {
+        var click_url, dest_url,
+            windowParams = $.extend({}, $.deparam( window.location.search.substr(1) ));
+
+        // The unique ad click passthrough url is irrelvant for tracking
+        if (windowParams['click']) {
+            delete windowParams['click'];
+        }
+
+        dest_url = module.urlAddParams(targetUrl, windowParams);
+        dest_url = module.addUrlTrackingParameters(dest_url);
+        
+        // Track destination url
+        App.vent.trigger("tracking:click", dest_url);
+
+        click_url = module.generateAdClickUrl(dest_url);
+        window.open(click_url, "_blank");
+        return;
+    };
+};
