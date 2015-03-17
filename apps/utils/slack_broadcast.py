3b9284f91146a839f1de17396cd34e3d0dcb5a42
Fraser Harris <fraser.harris@gmail.com>
2015-03-16 22:19:04 -0700

    Bug fixing?

--- apps/utils/slack_broadcast.py
+++ apps/utils/slack_broadcast.py
@@ -1,15 +1,13 @@
-#   We use a client library (python-simple-hipchat) to interface
-#   with the standard Hipchat API.  It is listed as a library on 
-#   Hipchat's old api v1, but not on its api v2.  Nonetheless it
-#   works fine.
-#   library docs: https://github.com/kurttheviking/python-simple-hipchat
-#   Hipchat API docs: https://www.hipchat.com/docs/api, look under "rooms/message"
+#   Library docs: https://github.com/iktakahiro/slackpy
+#   Slack API docs: https://api.slack.com/incoming-webhooks
+import slackpy as slack
 
-import slackpy
+
 INCOMING_WEB_HOOK = "https://hooks.slack.com/services/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"
 
-def slack(channel, sender, message, level="info"):
+
+def msg(channel, sender, message, level="info"):
     if level not in ['info','warn','error']:
         level = 'info'
-    s = slackpy.SlackLogger(INCOMING_WEB_HOOK, channel, sender)
+    s = slack.SlackLogger(INCOMING_WEB_HOOK, channel, sender)
     getattr(s, level)(message=message)
