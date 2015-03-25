549773509de3755492b0a13c3aad771df67846e1
mdgnkm <ys.madigankim@gmail.com>
2015-03-25 00:23:09 -0400

    Forgot to add utils.js.

---
+++ apps/light/src/clients/surlatable/landingpage/mothersday/scripts/utils.js
@@ -0,0 +1,18 @@
+"use strict";
+
+require('jquery-deparam');
+
+module.exports = function (module, App, Backbone, Marionette, $, _) {
+    /**
+     * ALL CLICKS TO EXTERNAL URLS SHOULD GO THROUGH THIS FUNCTION
+     * 
+     * Opens url in correct window w/ tracking parameters appended & Emits tracking event
+     *
+     * @param {string} url
+     */
+    module.openUrl = function (targetUrl) {
+        App.vent.trigger("tracking:click", targetUrl);
+        window.open(targetUrl, module.openInWindow());
+        return;
+    };
+};
