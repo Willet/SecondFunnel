2f966b34132a6e7b114e2cd19df6d30ac5f3f878
mdgnkm <ys.madigankim@gmail.com>
2015-04-23 13:42:48 -0400

    Removed `/` from result url that caused a redirect.

--- apps/scrapy/urls.py
+++ apps/scrapy/urls.py
@@ -4,7 +4,7 @@ urlpatterns = patterns(
     'apps.scrapy.views',
     url(r'^$', 'index'),
     url(r'^(?P<page_slug>[-\w]+)/$', 'page'),
-    url(r'^(?P<page_slug>[-\w]+)/result/(?P<job_id>.+)/$', 'result'),
+    url(r'^(?P<page_slug>[-\w]+)/result/(?P<job_id>.+)$', 'result'),
 
     url(r'^(?P<page_slug>[-\w]+)/scrape', 'scrape'),
     url(r'^(?P<page_slug>[-\w]+)/prioritize', 'prioritize'),
