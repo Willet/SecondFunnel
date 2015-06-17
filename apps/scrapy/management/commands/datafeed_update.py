f103039bbe5c86e67b652cc8b949bcaa2481cef1
Fraser Harris <fraser.harris@gmail.com>
2015-06-16 22:02:22 -0700

    Completed algorithm to generate similar products for Sur La Table

--- apps/scrapy/management/commands/datafeed_update.py
+++ apps/scrapy/management/commands/datafeed_update.py
@@ -1,8 +1,11 @@
+import random
+
 from django.core.management.base import BaseCommand
 from django.db import transaction
 from optparse import make_option
 
-from apps.assets.models import Page, Product
+from apps.assets.models import Page, Product, ProductImage
+from apps.imageservice.utils import get_filetype
 from apps.scrapy.datafeed.cj import download_product_datafeed, load_product_datafeed, delete_product_datafeed
 from apps.scrapy.logging import notify_slack, upload_to_s3
 
@@ -27,19 +30,111 @@ class Command(BaseCommand):
         Url slug of the page to update
     """
     @staticmethod
-    def update_product(product, data, results):
+    def update_product_cj_fields(product, data):
         product.price = float(data['PRICE'])
         product.sale_price = float(data['SALEPRICE'])
         product.in_stock = True if data['INSTOCK'] == 'yes' else False
         product.attributes['cj_link'] = data['BUYURL']
+
+    def update_or_create_similar_product(self, data, store):
+        """
+        Try to find similar product based on SKU, then updatd with data
+        If created, generate product image
+        """
+        try:
+            product = Product.objects.get(sku=data['SKU'],
+                                          store= store)
+        except Product.DoesNotExist:
+            product = Product(sku= data['SKU'],
+                              url= data['THIRDPARTYCATEGORY'],
+                              name= data['NAME'],
+                              store= store)
+
+        # Update
+        self.update_product_cj_fields(product, data)
         product.save()
 
-        if not product.in_stock:
-            results['logging/items out of stock'].append(product.url)
-            #print 'logging/items out of stock: {}'.format(product.url)
-        else:
-            results['logging/items updated'].append(product.url)
-            #print 'logging/items updated: {}'.format(product.url)
+        if not product.product_images.count():
+            # Add one product image
+            # TODO: utilize cloudinary
+            product_image_url = data['ARTIST']
+            product_image = ProductImage(product= product,
+                                         url= product_image_url,
+                                         original_url= product_image_url,
+                                         file_type= get_filetype(product_image_url),
+                                         attributes= {
+                                            'sizes': {
+                                                'master': {
+                                                    'width': 430,
+                                                    'height': 430,
+                                                },
+                                            },
+                                         })
+            product_image.save()
+            product.default_image = product_image
+            product.save()
+
+        return product
+
+    def get_similar_products_data(self, product_data):
+        """
+        Get similar product data for the product in product_data
+        """
+        mapping = ("ADVERTISERCATEGORY", product_data["ADVERTISERCATEGORY"])
+        # get similar products data, gaurenteed to be at least the same product
+        (similar_products_data, _) = self.lookup_table.find(mappings=[mapping], first=False)
+        # remove same product from similar_products
+        similar_products_data.remove(product_data)
+        # remove out of stock similar products
+        similar_products_data = [ sp for sp in similar_products_data if sp['INSTOCK'] == 'yes' ]
+
+        return similar_products_data
+
+    def update_similar_products(self, product, data, max_num=3):
+        """
+        Update existing similar products and replace sold out similar products
+        If there are no similar products, generate max_num of new ones
+        """
+        exclude_skus = []
+        generate_similar_products_num = 3
+
+        if product.similar_products.count():
+            sold_out_products = []
+
+            # Update existing similar products
+            for sp in product.similar_products.all():
+                (sp_data, _) = self.lookup_table.find(mappings=[("SKU", sp.sku)], first=True)
+                self.update_product_cj_fields(sp, sp_data)
+                sp.save()
+
+                exclude_skus.append(sp.sku)
+
+                if not sp.in_stock:
+                    sold_out_products.append(sp)
+
+            # Removed sold out products
+            product.similar_products.remove(*sold_out_products)
+            generate_similar_products_num = max_num - product.similar_products.count()
+
+
+        if generate_similar_products_num:
+            # Generate new similar products
+            similar_products_data = self.get_similar_products_data(data)
+            # Filter out existing similar products
+            similar_products_data = [ sp for sp in similar_products_data if sp['SKU'] not in exclude_skus ]
+            try:
+                # Randomly select 3 similar products out of the options
+                similar_products_data = random.sample(similar_products_data, generate_similar_products_num)
+            except ValueError:
+                pass # Less than 3 similar products
+            new_similar_products = [ self.update_or_create_similar_product(sp, product.store) for sp in similar_products_data ]
+            product.similar_products.add(*new_similar_products)
+    
+    def update_product(self, product, data):
+        self.update_product_cj_fields(product, data)
+
+        if product.in_stock:
+            self.update_similar_products(product, data)
     
     def handle(self, url_slug, **options):
         page = Page.objects.get(url_slug=url_slug)
@@ -56,11 +151,16 @@ class Command(BaseCommand):
         product_datafeed_filename = download_product_datafeed(page.store)
 
         # Import data feed & generate lookup table
-        lookup_table = load_product_datafeed(filename=product_datafeed_filename,
-                                             collect_fields=["SKU", "NAME", "DESCRIPTION", "PRICE",
-                                                             "SALEPRICE", "BUYURL", "INSTOCK", "ADVERTISERCATEGORY",
-                                                             "THIRDPARTYCATEGORY"],
-                                             lookup_fields=["SKU","NAME","THIRDPARTYCATEGORY"])
+        # THIRDPARTYCATEGORY - product url
+        # ADVERTISERCATEGORY - product category breadcrumb
+        # ARTIST - large product image url
+        self.lookup_table = load_product_datafeed(filename=product_datafeed_filename,
+                                            collect_fields=["SKU", "NAME", "DESCRIPTION", "PRICE",
+                                                            "SALEPRICE", "BUYURL", "INSTOCK",
+                                                            "ADVERTISERCATEGORY", "THIRDPARTYCATEGORY",
+                                                            "ARTIST"],
+                                             lookup_fields=["SKU","NAME","THIRDPARTYCATEGORY",
+                                                            "ADVERTISERCATEGORY"])
         try:
             products = page.feed.get_all_products()
             print u"Found {} products for {}".format(len(products), url_slug)
@@ -68,22 +168,14 @@ class Command(BaseCommand):
             # Find product in product data feed & update
             with transaction.atomic():
                 for product in products:
-                    data = None
+                    match = field = val = None
                     try:
-                        lookup_attempts = [
-                            ("SKU", product.sku),
-                            ("NAME", product.name.encode('ascii', errors='ignore')),
-                            ("THIRDPARTYCATEGORY", product.url),
-                        ]
-                        for (field, val) in lookup_attempts:
-                            try:
-                                key = lookup_table[field][val]
-                                data = lookup_table['hash'][key]
-                            except KeyError:
-                                pass
-                            else:
-                                if data:
-                                    break
+                        match = self.lookup_table.find(mappings=[
+                                ("SKU", product.sku),
+                                ("NAME", product.name.encode('ascii', errors='ignore')),
+                                ("THIRDPARTYCATEGORY", product.url),
+                            ],
+                            first=True)
                     except Exception as e:
                         errors = results['logging/errors']
                         msg = '{}: {}'.format(e.__class__.__name__, e)
@@ -93,11 +185,21 @@ class Command(BaseCommand):
                         results['logging/errors'] = errors
                         print 'logging/errors: {}'.format(msg)
                     else:
+                        data, field = match
                         if data:
+                            # Found matching product
                             print u"{} match: {}".format(field, product.url)
-                            self.update_product(product, data, results)
+
+                            self.update_product(product, data)
+                            product.save()
+
+                            if product.in_stock:
+                                results['logging/items updated'].append(product.url)
+                            else:
+                                results['logging/items out of stock'].append(product.url)
                         else:
                             print u"\tMatch FAILED: {} {}".format(product.name.encode('ascii', errors='ignore'), product.url)
+                            # Out of stock items often just disappear from the feeds
                             if product.in_stock:
                                 product.in_stock = False
                                 product.save()
---
+++ apps/scrapy/models.py
@@ -0,0 +1,71 @@
+
+class LookupTable(object):
+    """
+    A one-to-many lookup table that supports 
+     - an arbitary number of lookup fields
+     - arbitrary data
+
+    Note: the lookup table does not gaurentee the uniqueness of entries
+    """
+    def __init__(self, lookup_fields):
+        self._field_lookups = {}
+        self._hash_table = {}
+
+        for f in lookup_fields:
+            self._field_lookups[f] = {}
+
+    def add(self, entry, mappings):
+        """
+        adds entry with the lookups in mappings
+
+        entry - arbitrary data
+        mappings - a list of tuples with field:key lookups
+
+        raises: KeyError if lookup field was not initialized
+        """
+        h = id(entry)
+
+        self._hash_table[h] = entry
+        for (field, key) in mappings:
+            try:
+                self._field_lookups[field]
+            except KeyError:
+                raise KeyError('Lookup field does not exist')
+
+            try:
+                hashes = self._field_lookups[field][key]
+            except KeyError:
+                hashes = []
+            hashes.append(h)
+            self._field_lookups[field][key] = hashes
+
+    def find(self, mappings, first=False):
+        """
+        searches for matching entry in order of mappings list
+        stops search with first successful field:key mapping and will return
+        all entries for that field:key
+
+        mappings - a list of tuples with fields and lookup keys
+        first - if True, return only the first matching entry
+        """
+        entries = []
+        for (field, key) in mappings:
+            try:
+                self._field_lookups[field]
+            except KeyError:
+                raise KeyError('Lookup field does not exist')
+            try:
+                hashes = self._field_lookups[field][key]
+            except KeyError:
+                pass
+            else:
+                for h in hashes:
+                    entries.append(self._hash_table[h])
+                if first:
+                    return (entries[0], field)
+                else:
+                    return (entries, field)
+        # Didn't find anything
+        return (None, None)
+
+
