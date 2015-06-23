d6a04df2a3c528f291607e3b21686716aaccf0fd
Fraser Harris <fraser.harris@gmail.com>
2015-06-23 14:02:00 -0700

    Fixed vagrant settings missing bug

--- manage.py
+++ manage.py
@@ -5,12 +5,12 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "secondfunnel.settings.dev")
 
-    # APP_ENVIRONMENT must be set by the deploy script
-    # allowed values: production, stage, dev
-    environment_type = os.getenv('APP_ENVIRONMENT', '') or 'dev'
+    # AWS_GROUP must be set by the deploy script
+    # allowed values: production, stage
+    environment_type = os.getenv("AWS_GROUP", "") or "dev"
 
-    os.environ['DJANGO_SETTINGS_MODULE'] = \
-        'secondfunnel.settings.{0}'.format(environment_type.lower())
+    os.environ["DJANGO_SETTINGS_MODULE"] = \
+        "secondfunnel.settings.{0}".format(environment_type.lower())
 
     from django.core.management import execute_from_command_line
 
--- secondfunnel/settings/__init__.py
+++ secondfunnel/settings/__init__.py
@@ -1,2 +1,1 @@
-# each .py configuration in this file can be run on EBS by specifying
-# the PARAM1 environment variable.
+# each .py configuration in this file controlled by the AWS_GROUP environment variable.
