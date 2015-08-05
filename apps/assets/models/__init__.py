24979cab8d21b2ee29c92211d25ab6becaa62c94
Fraser Harris <fraser.harris@gmail.com>
2015-08-05 13:38:45 -0700

    Revert "Cleanup/separating models"

--- apps/assets/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from .core import BaseModel, SerializableMixin
-from .structure import Store, Theme, Page, Feed, Category, Tile
-from .elements import Product, ProductImage, Tag, Content, Image, Gif, Video, Review
