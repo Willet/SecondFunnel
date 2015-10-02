3dc3a33b340c038e233d29588cf7b4748d885c1c
Fraser Harris <fraser.harris@gmail.com>
2015-10-02 15:36:00 -0700

    Added new m2m_changed signal reciever for tiles

--- apps/assets/signals.py
+++ apps/assets/signals.py
@@ -43,13 +43,16 @@ def content_saved(sender, **kwargs):
 
 @receiver(m2m_changed)
 def content_m2m_changed(sender, **kwargs):
-    """Generate cache for IR tiles if related products or content changed."""
+    """Re-generate cache for IR tiles if contained product or content has its 
+       related products changed
+       ex: Tile -> Product -> similar products updated
+           Til -> Content -> tagged products updated
+    """
     added_or_removed_keys = kwargs.get('pk_set') or [] # for some signals, pk_set is None
     
-    if (sender is Content.tagged_products.through or
-            sender is Product.similar_products.through) and \
-            kwargs.get('action') in ('post_add', 'post_clear', 'post_remove') and \
-            len(added_or_removed_keys) > 0:
+    if (sender in [Content.tagged_products.through, Product.similar_products.through]) \
+            and kwargs.get('action') in ('post_add', 'post_clear', 'post_remove') \
+            and len(added_or_removed_keys) > 0:
         # populate set of objects whose tiles need to be refreshed
         instances = []
         if kwargs.get('reverse'):
@@ -70,6 +73,35 @@ def content_m2m_changed(sender, **kwargs):
                     post_save.disconnect(tile_saved, sender=Tile)
                     models.Model.save(tile, update_fields=['ir_cache'])
                     post_save.connect(tile_saved, sender=Tile)
+
+
+@receiver(m2m_changed)
+def tile_m2m_changed(sender, **kwargs):
+    """Re-generate cache for IR tiles if products or content changed on tile."""
+    added_or_removed_keys = kwargs.get('pk_set') or [] # for some signals, pk_set is None
+    
+    if (sender in [Tile.products.through, Tile.content.through]) \
+            and kwargs.get('action') in ('post_add', 'post_clear', 'post_remove') \
+            and len(added_or_removed_keys) > 0:
+        # populate set of objects whose tiles need to be refreshed
+        tiles = []
+        if kwargs.get('reverse'):
+            # update tiles of other side of m2m relationship
+            for pk in added_or_removed_keys:
+                tile = kwargs.get('model').objects.get(pk=pk)
+                tiles.append(tile)
+        else:
+            tile = kwargs.pop('instance', None)
+            tiles.append(tile)
+
+        for tile in tiles:
+            # validation can be skipped because 
+            # only product/content relationships are changed
+            ir_cache, updated = tile.update_ir_cache() # sets tile.ir_cache
+            if updated:
+                post_save.disconnect(tile_saved, sender=Tile)
+                models.Model.save(tile, update_fields=['ir_cache'])
+                post_save.connect(tile_saved, sender=Tile)
 
 
 @receiver(post_save, sender=Tile)
