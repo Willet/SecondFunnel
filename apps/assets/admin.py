3515d5f227778fe7974434cab95207b4e656299d
Fraser Harris <fraser.harris@gmail.com>
2015-10-03 00:36:10 -0700

    Fixed import bugs and import style

--- apps/assets/admin.py
+++ apps/assets/admin.py
@@ -5,8 +5,8 @@ from django.db import models
 from django.forms import SelectMultiple, ModelMultipleChoiceField
 
 from .forms import CategoryForm, TagForm
-from .models import (BaseModel, Category, Store, Page, Tile, Feed, Product,
-                                ProductImage, Image, Content, Theme, Review, Video, Tag, Gif)
+from .models import BaseModel, Category, Store, Page, Tile, Feed, Product, \
+                    ProductImage, Image, Content, Theme, Review, Video, Tag, Gif
 from .utils import disable_tile_serialization
 
 
--- apps/assets/utils.py
+++ apps/assets/utils.py
@@ -1,9 +1,7 @@
+import sys
+
 from contextlib2 import ContextDecorator
 from django.db.models.signals import post_save, m2m_changed
-
-# CIRCULAR IMPORTS - delay loading by not import specific things
-import apps.assets.signals
-import apps.assets.models
 
 
 class disable_tile_serialization(ContextDecorator):
@@ -19,9 +17,13 @@ class disable_tile_serialization(ContextDecorator):
     disabled_counter = 0
 
     def __enter__(self):
+        # lazy load circular import signals -> models -> utils -> signals
+        import apps.assets.signals as signals
+        import apps.assets.models as models
+
         self.disabled_counter += 1
         try:
-            post_save.disconnect(signals.tile_saved, sender=models.Tile)
+            post_save.disconnect(receiver=signals.tile_saved, sender=models.Tile)
             m2m_changed.disconnect(signals.tile_m2m_changed)
             m2m_changed.disconnect(signals.content_m2m_changed)
         except:
@@ -35,10 +37,14 @@ class disable_tile_serialization(ContextDecorator):
             raise
 
     def __exit__(self, type, value, traceback):
+        # lazy load circular import models -> signals -> utils -> models
+        import apps.assets.signals as signals
+        import apps.assets.models as models
+
         self.disabled_counter -= 1
         if self.disabled_counter < 1:
             post_save.connect(signals.tile_saved, sender=models.Tile)
+            m2m_changed.connect(signals.tile_m2m_changed)
             m2m_changed.connect(signals.content_m2m_changed)
-            m2m_changed.connect(signals.tile_m2m_saved)
             # Should never have exit'ed more than enter'ed.
             assert self.disabled_counter == 0
