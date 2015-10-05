6721c8e9850764d8853557e1c05e428beaee8df9
Fraser Harris <fraser.harris@gmail.com>
2015-10-04 23:23:57 -0700

    Nose unittests working, database is finky though

--- secondfunnel/settings/nosetests_runner.py
+++ secondfunnel/settings/nosetests_runner.py
@@ -1,16 +1,16 @@
 """
 Creates a disposable database for running tests.
 
-(venv) python ./manage.py test --settings=secondfunnel.settings.nosetests_runner
+(venv) python ./manage.py test --settings=secondfunnel.settings.nosetests_runner -v 2
 """
 
 
 from dev import *
 
+# Make tests faster
+SOUTH_TESTS_MIGRATE = False
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': from_project_root('test.db'),
-    }
-}
+# Use db port 5432 to avoid pgbouncer, otherwise can't connect to DB
+DATABASES['default']['NAME'] = 'test_sfdb'
+DATABASES['default']['HOST'] = 'localhost'
+DATABASES['default']['PORT'] = 5432
