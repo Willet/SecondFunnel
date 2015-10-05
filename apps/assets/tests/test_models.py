9773d888f24abf8f31e5cbc886c7658bf7ca218d
Fraser Harris <fraser.harris@gmail.com>
2015-10-05 07:04:27 -0700

    Working on testing BaseModel

---
+++ apps/assets/tests/test_models.py
@@ -0,0 +1,27 @@
+from django.db.models.base import ModelBase
+import mock
+from unittest import TestCase
+
+from apps.assets.models import BaseModel, Store, Theme
+
+"""
+
+We are avoiding fixtures because they are so slow:
+http://www.mattjmorrison.com/2011/09/mocking-django.html
+
+"""
+
+class BaseModelTest(TestCase):
+    def setUp(self):
+        # create a dummy model to inherit abstract class BaseModel
+        self.model = ModelBase(BaseModel)
+
+class StoreTest(TestCase):
+    # Store has no methods
+    pass
+
+class ThemeTest(TestCase):
+    # Theme has no methods
+    pass
+
+class 
