f794683698e1ee4ead3c3da3a7b92f13a83befb2
Jeremy Rutledge <jerrutledge@gmail.com>
2015-11-05 20:33:44 -0500

    Create basic mobile styles

--- apps/light/src/clients/ebags/landingpage/lite/scripts/settings.js
+++ apps/light/src/clients/ebags/landingpage/lite/scripts/settings.js
@@ -6,7 +6,119 @@
     },
     "imageTileWideProb":0.2
   },
-  "mobileCategories":[],
+  "mobileCategories":[
+    {
+      "subCategories":[
+        {
+          "label":"Gifts by Price"
+        },
+        {
+          "tileSet":"products",
+          "displayName":"Under $30",
+          "name":"under-30",
+          "title":"Gifts Under $30",
+          "desktopHeroImage":"",
+          "mobileHeroImage":"",
+          "thumbnail":"http://images.secondfunnel.com/store/surlatable/gifting/Under-25_50x50.jpg"
+        },
+        {
+          "tileSet":"products",
+          "displayName":"Under $50",
+          "name":"under-50",
+          "title":"Gifts Under $50",
+          "desktopHeroImage":"",
+          "mobileHeroImage":"",
+          "thumbnail":"http://images.secondfunnel.com/store/surlatable/gifting/Under-50_50x50.jpg"
+        },
+        {
+          "tileSet":"products",
+          "displayName":"Under $70",
+          "name":"under-70",
+          "title":"Gifts Under $70",
+          "desktopHeroImage":"",
+          "mobileHeroImage":"",
+          "thumbnail":"http://images.secondfunnel.com/store/surlatable/gifting/Under-100_50x50.jpg"
+        },
+        {
+          "tileSet":"products",
+          "displayName":"Under $100",
+          "name":"under-100",
+          "title":"Gifts Under $100",
+          "desktopHeroImage":"",
+          "mobileHeroImage":"",
+          "thumbnail":"http://images.secondfunnel.com/store/surlatable/gifting/Under-100_50x50.jpg"
+        },
+        {
+          "tileSet":"products",
+          "displayName":"Splurge!",
+          "name":"splurge",
+          "title":"Gifts over $100",
+          "desktopHeroImage":"",
+          "mobileHeroImage":"",
+          "thumbnail":"http://images.secondfunnel.com/store/surlatable/gifting/Gourmet_50x50.jpg"
+        }
+      ],
+      "displayName":"Gifts by Price",
+      "name":"",
+      "tileSet":"products",
+      "desktopHeroImage":"",
+      "mobileHeroImage":"",
+      "thumbnail":"http://images.secondfunnel.com/store/surlatable/gifting/Gifts-By-Price_111x111.jpg"
+    },
+    {
+      "displayName":"By Recipient",
+      "name":"the-baker",
+      "title":"Gifts for the Baker",
+      "desktopHeroImage":"",
+      "mobileHeroImage":"",
+      "thumbnail":"http://images.secondfunnel.com/store/surlatable/gifting/The-Baker_111x111.jpg"
+    },
+    {
+      "subCategories":[
+        {
+          "label":"Other Gifts"
+        },
+        {
+          "displayName":"Stocking Stuffers",
+          "name":"stocking-stuffers",
+          "title":"Stocking Stuffers",
+          "desktopHeroImage":"",
+          "mobileHeroImage":"",
+          "thumbnail":"http://images.secondfunnel.com/store/surlatable/gifting/The-Family_111x111.jpg"
+        },
+        {
+          "displayName":"Packing Cubes",
+          "name":"packing-cube",
+          "title":"Packing Cubes",
+          "desktopHeroImage":"",
+          "mobileHeroImage":"",
+          "thumbnail":"http://images.secondfunnel.com/store/surlatable/gifting/The-Host_111x111.jpg"
+        },
+        {
+          "displayName":"For Her",
+          "name":"for-her",
+          "title":"Gifts for Her",
+          "desktopHeroImage":"",
+          "mobileHeroImage":"",
+          "thumbnail":"http://images.secondfunnel.com/store/surlatable/gifting/The-Cook_111x111.jpg"
+        },
+        {
+          "displayName":"For Him",
+          "name":"for-him",
+          "title":"Gifts for Him",
+          "desktopHeroImage":"",
+          "mobileHeroImage":"",
+          "thumbnail":"http://images.secondfunnel.com/store/surlatable/gifting/The-Cook_111x111.jpg"
+        }
+      ],
+      "displayName":"Other Gifts",
+      "name":"",
+      "tileSet":"products",
+      "desktopHeroImage":"",
+      "mobileHeroImage":"",
+      "thumbnail":"http://images.secondfunnel.com/store/surlatable/gifting/Gifts-By-Price_111x111.jpg"
+    }
+  ],
   "categoryHome":false,
   "gaAccountNumber":"UA-23764505-25",
   "column_width":341,
