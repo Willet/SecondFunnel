1627bbc18114845e8ed78b4b6dc15144592c9856
Fraser Harris <fraser.harris@gmail.com>
2015-11-09 03:43:14 -0800

    More css changes x2

--- apps/light/src/clients/bodyshop/landingpage/christmas/scripts/main.js
+++ apps/light/src/clients/bodyshop/landingpage/christmas/scripts/main.js
@@ -14,6 +14,7 @@ var Page = require('landingpage'),
 App.module('utils', require('./utils'));
 App.module('core', require('./core.models'));
 App.module('core', require('./core.views'));
+App.module('core', require('./core.views.tiles'));
 
 // Run Application
 App.init.initialize();
