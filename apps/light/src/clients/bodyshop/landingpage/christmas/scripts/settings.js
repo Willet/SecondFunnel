e2f74bd62dc3421e495aa9f33b60fde4edc5bd0b
Fraser Harris <fraser.harris@gmail.com>
2015-11-11 12:06:22 -0800

    Styling for bodyshop

--- apps/light/src/clients/bodyshop/landingpage/christmas/scripts/settings.js
+++ apps/light/src/clients/bodyshop/landingpage/christmas/scripts/settings.js
@@ -26,6 +26,21 @@
   "minImageWidth":400,
   "categories":[
     {
+      "displayName":"Superstar Sister",
+      "name":"sister",
+      "heroTitle":"For <b>SUPERSTAR SISTER:</b> Shower Her With Love With Amazing Skin & Body Care Gifts"
+    },
+    {
+      "displayName":"Best Bestie Ever",
+      "name":"bestie",
+      "heroTitle":"For <b>BEST BESTIE EVER:</b> Bemuse Her With Friendly Skin & Body Care Gifts"
+    },
+    {
+      "displayName":"Marvelous Mama",
+      "name":"mama",
+      "heroTitle":"For <b>MARVELOUS MAMA:</b> Delight Her With The Ultimate Winter Skin & Body Care Gifts"
+    },
+    {
       "displayName":"Secret Santa",
       "name":"secret-santa",
       "heroTitle":"For <b>SECRET SANTA:</b> Surprise Her With Saint Nicolas Skin & Body Care Gifts"
@@ -41,19 +56,9 @@
       "heroTitle":"For <b>TERRIFIC TEACHER:</b> Be The Teacher's Pet With These Cute Seasonal Gifts."
     },
     {
-      "displayName":"Superstar Sister",
-      "name":"sister",
-      "heroTitle":"For <b>SUPERSTAR SISTER:</b> Shower Her With Love With Amazing Skin & Body Care Gifts"
-    },
-    {
-      "displayName":"Best Bestie Ever",
-      "name":"bestie",
-      "heroTitle":"For <b>BEST BESTIE EVER:</b> Bemuse Her With Friendly Skin & Body Care Gifts"
-    },
-    {
-      "displayName":"Marvelous Mama",
-      "name":"mama",
-      "heroTitle":"For <b>MARVELOUS MAMA:</b> Delight Her With The Ultimate Winter Skin & Body Care Gifts"
+      "displayName":"Modern Gent",
+      "name":"gent",
+      "heroTitle":"For <b>MODERN GETN:</b> Suave Suave Suave With These Fine Seasonal Gifts."
     }
   ]
 }
