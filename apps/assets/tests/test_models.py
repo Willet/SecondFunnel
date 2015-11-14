ea28ed513e339032e3dee0b9bdb4ca720ee0139a
Jeremy Rutledge <jerrutledge@gmail.com>
2015-11-13 20:02:29 -0500

    begin implementing factory_boy

--- apps/assets/tests/test_models.py
+++ apps/assets/tests/test_models.py
@@ -3,16 +3,35 @@ from django.db import models
 import mock
 import datetime
 import time
+import factory
 from unittest import TestCase
 
-from apps.assets.models import BaseModel, Store, Theme, Tag, Category, Page, Product, Image, ProductImage, Feed
+from apps.assets.models import BaseModel, Store, Theme, Tag, Category, Page, Product, Image, \
+                               ProductImage, Feed, Tile, Content
 
 """
-
 We are avoiding fixtures because they are so slow:
 http://www.mattjmorrison.com/2011/09/mocking-django.html
-
 """
+
+class ProductFactory(factory.Factory):
+    class Meta:
+        model = Product
+
+class ProductImageFactory(factory.Factory):
+    class Meta:
+        model = ProductImage
+
+class StoreFactory(factory.Factory):
+    class Meta:
+        model = Store
+
+    name = "Test Store"
+    slug = "test_store"
+
+class FeedFactory(factory.Factory):
+    class Meta:
+        model = Feed
 
 class BaseModelTest(TestCase):
     def setUp(self):
@@ -24,7 +43,7 @@ class StoreTest(TestCase):
     def setUp(self):
         self.name = "Test Store"
         self.slug = "test_store"
-        self.store = Store.objects.create(name=self.name, slug=self.slug)
+        self.store = StoreFactory(name=self.name, slug=self.slug)
 
     def properties_test(self):
         self.assertEqual(self.store.slug, self.slug)
@@ -35,9 +54,6 @@ class StoreTest(TestCase):
         self.assertIs(type(self.store.name), str)
         self.assertIs(type(self.store.pk), int)
         self.assertIs(type(self.store.updated_at), datetime.datetime)
-
-    def tearDown(self):
-        self.store.delete()
 
 class ThemeTest(TestCase):
     # Theme has no methods
@@ -55,10 +71,53 @@ class ThemeTest(TestCase):
         self.assertIs(type(t.template), unicode)
         self.assertIs(type(t.updated_at), datetime.datetime)
 
-class TagTest(TestCase):
-    #Tag has no methods
+class ProductTest(TestCase):
+    # Product has no methods
     def setUp(self):
-        self.store = Store.objects.create(name="TestStore", slug="test")
+        self.store = StoreFactory()
+
+    def properties_test(self):
+        pprice = 19.99
+        p = Product.objects.create(store=self.store, price=pprice)
+        self.assertEqual(p.store, self.store)
+        self.assertEqual(p.store_id, self.store.id)
+        self.assertEqual(p.price, pprice)
+        self.assertTrue(p.id > 0)
+        self.assertTrue(p.pk > 0)
+
+    def blank_clean_test(self):
+        p = ProductFactory()
+        Product.clean(p)
+        self.assertEqual(p.attributes, {})
+
+    def one_image_clean_test(self):
+        p = ProductFactory()
+        i = ProductImageFactory()
+        i.url = "/image.jpg"
+        i.original_url = "test.com/image.jpg"
+        p.product_images.add(i)
+        Product.clean(p)
+        self.assertEqual(p.default_image, image)
+
+class ProductImageTest(TestCase):
+    # ProductImage has no methods
+    def properties_test(self):
+        url = "/image.jpg"
+        original_url = "test.com/image.jpg"
+        product_image = ProductImage.objects.create(url=url, original_url=original_url)
+        self.assertIs(type(product_image.cg_created_at), unicode)
+        self.assertIs(type(product_image.cg_updated_at), unicode)
+        self.assertIs(type(product_image.created_at), datetime.datetime)
+        self.assertTrue(product_image.id > 0)
+        self.assertIsNone(product_image.ir_cache)
+        self.assertTrue(product_image.pk > 0)
+        self.assertEqual(product_image.url, url)
+        self.assertEqual(product_image.original_url, original_url)
+
+class TagTest(TestCase):
+    # Tag has no methods
+    def setUp(self):
+        self.store = StoreFactory()
 
     def properties_test(self):
         t = Tag.objects.create(name="TestTag", store=self.store)
@@ -75,18 +134,55 @@ class TagTest(TestCase):
         self.assertEqual(t.store_id, self.store.id)
         self.assertIsNone(t.url)
 
-    def tearDown(self):
-        self.store.delete()
+class ContentTest(TestCase):
+    # Content has no methods
+    def setUp(self):
+        self.url = "/content.jpg"
+        self.source = "upload"
+        self.store = StoreFactory()
+        self.feed = Feed.objects.create(store=self.store)
+
+    def properties_test(self):
+        t = Content.objects.create(url=self.url, source=self.source, store=self.store)
+        self.assertIs(type(t.cg_created_at), unicode)
+        self.assertIs(type(t.cg_updated_at), unicode)
+        self.assertIs(type(t.created_at), datetime.datetime)
+        self.assertTrue(t.id > 0)
+        self.assertIsNone(t.ir_cache)
+        self.assertIs(type(t.pk), int)
+        self.assertTrue(t.pk > 0)
+        self.assertEqual(t.store, self.store)
+        self.assertEqual(t.store_id, self.store.id)
+
+class ImageTest(TestCase):
+    # Image has no methods
+    def setUp(self):
+        self.url = "/content.jpg"
+        self.source = "upload"
+        self.original_url = "test.com/image.jpg"
+        self.store = StoreFactory()
+        self.feed = Feed.objects.create(store=self.store)
+
+    def properties_test(self):
+        t = Image.objects.create(url=self.url, source=self.source, original_url=self.original_url, store=self.store)
+        self.assertIs(type(t.cg_created_at), unicode)
+        self.assertIs(type(t.cg_updated_at), unicode)
+        self.assertIs(type(t.created_at), datetime.datetime)
+        self.assertTrue(t.id > 0)
+        self.assertIsNone(t.ir_cache)
+        self.assertIs(type(t.pk), int)
+        self.assertTrue(t.pk > 0)
+        self.assertEqual(t.store, self.store)
+        self.assertEqual(t.store_id, self.store.id)
 
 class CategoryTest(TestCase):
-    #Category has no methods
+    # Category has no methods
     def setUp(self):
-        Store.objects.create(name="TestStore", slug="test")
+        self.store = StoreFactory()
 
     def properties_test(self):
         name = "TestCategory"
-        s = Store.objects.get(name="TestStore")
-        c = Category.objects.create(name=name, store=s)
+        c = Category.objects.create(name=name, store=self.store)
         self.assertIs(type(c.cg_created_at), unicode)
         self.assertIs(type(c.cg_updated_at), unicode)
         self.assertIs(type(c.created_at), datetime.datetime)
@@ -94,17 +190,14 @@ class CategoryTest(TestCase):
         self.assertIsNone(c.ir_cache)
         self.assertEqual(c.name, name)
         self.assertTrue(c.pk > 0)
-        self.assertEqual(c.store, s)
-        self.assertEqual(c.store_id, s.id)
+        self.assertEqual(c.store, self.store)
+        self.assertEqual(c.store_id, self.store.id)
         self.assertIsNone(c.url)
 
-    def tearDown(self):
-        Store.objects.get(name="TestStore").delete()
-
 class PageTest(TestCase):
-    #Page has no methods
+    # Page has no methods
     def setUp(self):
-        self.store = Store.objects.create(name="TestStore", slug="test")
+        self.store = StoreFactory()
 
     def properties_test(self):
         name = "TestPage"
@@ -129,64 +222,10 @@ class PageTest(TestCase):
         self.assertEqual(p.theme_settings_fields, [('image_tile_wide', 0.0), ('desktop_hero_image', ''), ('mobile_hero_image', ''), ('column_width', 256), ('social_buttons', []), ('enable_tracking', 'true')])
         self.assertEqual(p.url_slug, url_slug)
 
-    def tearDown(self):
-        self.store.delete()
-
-class ProductTest(TestCase):
-    #Product has no methods
-    def setUp(self):
-        self.store = Store.objects.create(name="TestStore", slug="test")
-
-    def properties_test(self):
-        pprice = 19.99
-        p = Product.objects.create(store=self.store, price=pprice)
-        self.assertEqual(p.store, self.store)
-        self.assertEqual(p.store_id, self.store.id)
-        self.assertEqual(p.price, pprice)
-        self.assertTrue(p.id > 0)
-        self.assertTrue(p.pk > 0)
-
-    def blank_clean_test(self):
-        product_config = {'product_images.all.return_value': [],
-                          'attributes': None}
-        p = mock.Mock(spec=Product)
-        p.configure_mock(**product_config)
-        Product.clean(p)
-        self.assertEqual(p.attributes, {})
-
-    def one_image_clean_test(self):
-        image = mock.Mock()
-        product_config = {'product_images.all.return_value': [image],
-                          'attributes': None,
-                          'default_image': None,
-                          'product_images.first.return_value': image}
-        p = mock.Mock(spec=Product)
-        p.configure_mock(**product_config)
-        Product.clean(p)
-        self.assertEqual(p.default_image, image)
-
-    def tearDown(self):
-        self.store.delete()
-
-class ProductImageTest(TestCase):
-    #ProductImage has no methods
-    def properties_test(self):
-        url = "/image.jpg"
-        original_url = "test.com/image.jpg"
-        product_image = ProductImage.objects.create(url=url, original_url=original_url)
-        self.assertIs(type(product_image.cg_created_at), unicode)
-        self.assertIs(type(product_image.cg_updated_at), unicode)
-        self.assertIs(type(product_image.created_at), datetime.datetime)
-        self.assertTrue(product_image.id > 0)
-        self.assertIsNone(product_image.ir_cache)
-        self.assertTrue(product_image.pk > 0)
-        self.assertEqual(product_image.url, url)
-        self.assertEqual(product_image.original_url, original_url)
-
 class FeedTest(TestCase):
-    #Feed has no methods
+    # Feed has no methods
     def setUp(self):
-        self.store = Store.objects.create(name="TestStore", slug="test")
+        self.store = StoreFactory()
 
     def properties_test(self):
         f = Feed.objects.create(store=self.store)
@@ -200,14 +239,11 @@ class FeedTest(TestCase):
         self.assertEqual(f.store, self.store)
         self.assertEqual(f.store_id, self.store.id)
 
-    def tearDown(self):
-        self.store.delete()
-
 class TileTest(TestCase):
-    #Tile has no methods
+    # Tile has no methods
     def setUp(self):
-        self.store = Store.objects.create(name="TestStore", slug="test")
-        self.feed = Feed.objects.create(store=self.store)
+        self.store = StoreFactory()
+        self.feed = FeedFactory(store=self.store)
 
     def properties_test(self):
         t = Tile.objects.create(store=self.store, feed=self.feed)
@@ -220,7 +256,4 @@ class TileTest(TestCase):
         self.assertTrue(t.pk > 0)
         self.assertEqual(t.store, self.store)
         self.assertEqual(t.store_id, self.store.id)
-
-    def tearDown(self):
-        self.store.delete()
 
