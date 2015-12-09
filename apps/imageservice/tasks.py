47e4a9409d7a282af5f5c3433366523f860a3257
Fraser Harris <fraser.harris@gmail.com>
2015-12-08 17:28:27 -0800

    Removed unused import

--- apps/imageservice/tasks.py
+++ apps/imageservice/tasks.py
@@ -11,8 +11,8 @@ import urllib2
 
 from apps.imageservice.models import SizeConf
 from apps.imageservice.utils import create_image, create_image_path, delete_cloudinary_resource, \
-                                    delete_s3_resource, get_filetype, get_public_id, IMAGE_SIZES, \
-                                    upload_to_cloudinary, within_color_range
+                                    get_filetype, get_public_id, IMAGE_SIZES, upload_to_cloudinary, \
+                                    within_color_range
 from apps.utils.aws import upload_to_bucket
 
 
