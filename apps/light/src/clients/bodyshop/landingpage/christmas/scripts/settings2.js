22f211f06c66208a50d9fc034b2d4496c887ffea
Fraser Harris <fraser.harris@gmail.com>
2015-12-18 13:51:10 -0800

    Removed all clickout categories

--- apps/light/src/clients/bodyshop/landingpage/christmas/scripts/settings2.js
+++ apps/light/src/clients/bodyshop/landingpage/christmas/scripts/settings2.js
@@ -26,12 +26,10 @@
       "name":"hand-care-gifts"
     },
     {
-      "url":"http://m.thebodyshop-usa.com/gifts/skin-care-gifts.aspx",
       "displayName":"Skin Care",
       "name":"skin-care-gifts"
     },
     {
-      "url":"http://m.thebodyshop-usa.com/gifts/fragrance-gifts.aspx",
       "displayName":"Fragrance",
       "name":"fragrance-gifts"
     },
@@ -49,7 +47,6 @@
       "name":"gifts-20-to-50"
     },
     {
-      "url":"http://m.thebodyshop-usa.com/gifts/gifts-over-50.aspx",
       "displayName":"Gifts Over $50",
       "name":"gifts-over-50"
     },
@@ -69,7 +66,7 @@
     {
       "heroTitle":"For <b>SUPERSTAR SISTER:</b> Shower Her With Love With Amazing Skin & Body Care Gifts",
       "displayName":"Superstar Sister",
-      "name":"all-sister"
+      "name":"superstar-sister"
     },
     {
       "heroTitle":"For <b>BEST BESTIE EVER:</b> Bemuse Her With Friendly Skin & Body Care Gifts",
@@ -109,7 +106,7 @@
   "social_buttons":null,
   "useAffiliateLinks":true,
   "defaults":{
-    "heroTitle":"For <b>SUPERSTAR SISTER:</b> Indulge her with this selection of feel-good seasonal bath & body treats."
+    "heroTitle":"All Gifts"
   },
   "column_width":341,
   "categoryHome":false,
@@ -121,31 +118,32 @@
   "stickyCategories":true,
   "categories":[
     {
+      "heroTitle":"All Gifts",
       "displayName":"All Gifts",
       "name":"all-gifts"
     },
     {
-      "heroTitle": "Bath & Body Gifts",
+      "heroTitle":"Bath & Body Gifts",
       "displayName":"Bath & Body Gifts",
       "name":"bath-body-gifts"
     },
     {
-      "heroTitle": "Gifts For Her",
+      "heroTitle":"Gifts For Her",
       "displayName":"Gifts For Her",
       "name":"gifts-for-her"
     },
     {
-      "heroTitle": "Gifts For Him",
+      "heroTitle":"Gifts For Him",
       "displayName":"Gifts For Him",
       "name":"modern-gent"
     },
     {
-      "heroTitle": "Gifts Under $20",
+      "heroTitle":"Gifts Under $20",
       "displayName":"Under $20 Gifts",
       "name":"gifts-under-20"
     },
     {
-      "heroTitle": "Gifts $20 To $50",
+      "heroTitle":"Gifts $20 To $50",
       "displayName":"$20 To $50 Gifts",
       "name":"gifts-20-to-50"
     },
@@ -160,7 +158,6 @@
           "label":"By Price"
         },
         {
-          "url":"http://www.thebodyshop-usa.com/gifts/gifts-over-50.aspx",
           "displayName":"Gifts Over $50",
           "name":"gifts-over-50"
         },
@@ -168,12 +165,10 @@
           "label":"By Category"
         },
         {
-          "url":"http://www.thebodyshop-usa.com/gifts/skin-care-gifts.aspx",
           "displayName":"Skin Care",
           "name":"skin-care-gifts"
         },
         {
-          "url":"http://www.thebodyshop-usa.com/gifts/fragrance-gifts.aspx",
           "displayName":"Fragrance",
           "name":"fragrance-gifts"
         },
