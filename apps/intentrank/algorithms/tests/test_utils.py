d6ca69378e2e69a34088ab2df22d0e94bed65a99
Jeremy Rutledge <jerrutledge@gmail.com>
2015-12-23 01:53:35 -0500

    Finishing writing tests for algorithm utils

--- apps/intentrank/algorithms/tests/test_utils.py
+++ apps/intentrank/algorithms/tests/test_utils.py
@@ -3,6 +3,7 @@ import mock
 import logging
 from datetime import datetime
 from django.conf import settings
+from django.core.exceptions import ValidationError, ObjectDoesNotExist, MultipleObjectsReturned
 
 from apps.utils.functional import may_be_json
 from apps.assets.models import BaseModel, Store, Theme, Tag, Category, Page, Product, Image, \
@@ -21,6 +22,21 @@ class IRMagicTest(TestCase):
         self.assertEqual(algorithms.ir_filter(tiles=tiles, feed=feed)[0], tiles[0])
         self.assertEqual(len(algorithms.ir_filter(tiles=tiles, feed=feed)), len(tiles))
 
+    def filter_tiles_pi_tiles_test(self):
+        tiles = [Tile.objects.get(pk=10)]
+        feed = Feed.objects.get(pk=9)
+        f = Feed.objects.get(pk=9)
+        p = Product.objects.get(pk=3) # in stock product
+        i = ProductImage.objects.get(pk=4)
+        p.default_image = i
+        p.product_images.add(i)
+        f.add(p)
+        logging.debug([t.placeholder for t in f.tiles.all()])
+        # only tiles will be used b/c they are specifically specified
+        rslt = algorithms.ir_filter(tiles=tiles, feed=feed)
+        self.assertEqual(len(rslt), len(tiles))
+        self.assertEqual(rslt[0], tiles.first())
+
     def filter_tiles_pi_test(self):
         tiles = [Tile.objects.get(pk=10)]
         feed = Feed.objects.get(pk=9)
@@ -31,7 +47,11 @@ class IRMagicTest(TestCase):
         p.product_images.add(i)
         f.add(p)
         logging.debug([t.placeholder for t in f.tiles.all()])
-        raise Exception(algorithms.ir_filter(tiles=tiles, feed=feed))
+        rslt = algorithms.ir_filter(feed=feed)
+        self.assertEqual(len(rslt), 2)
+        self.assertEqual(len(feed.tiles.all()), 2)
+        self.assertEqual(rslt[0], feed.tiles.first())
+        self.assertEqual(rslt[1], feed.tiles.last())
 
     def filter_tiles_pi_oos_test(self):
         tiles = [Tile.objects.get(pk=10)]
@@ -56,8 +76,20 @@ class IRMagicTest(TestCase):
         p.product_images.add(i)
         f.add(p)
         store.display_out_of_stock = True # includes p in ir_filter
+        store.save()
         logging.debug([t.placeholder for t in f.tiles.all()])
         raise Exception(algorithms.ir_filter(tiles=tiles, feed=feed))
 
     def filter_tiles_no_feed_no_tiles_test(self):
-        algorithms.ir_filter()
+        with self.assertRaises(ValidationError): 
+            algorithms.ir_filter()
+
+class QSForTest(TestCase):
+    fixtures = ['assets_models.json']
+
+    def qs_for_test(self):
+        tiles = [Tile.objects.get(pk=10)]
+        result = algorithms.qs_for(tiles)
+        self.assertEqual(len(result), len(tiles))
+        self.assertEqual(result.first(), tiles[0])
+
