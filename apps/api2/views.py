eea1a13a993694b831d3379493589b939b1a57a8
Nhat Le <nale@uwaterloo.ca>
2016-01-20 15:57:04 -0500

    api_root page now works

--- apps/api2/views.py
+++ apps/api2/views.py
@@ -21,11 +21,19 @@ class StoreViewSet(viewsets.ModelViewSet):
     # permission_classes = (permissions.IsAuthenticatedOrReadOnly,
     #                       IsOwnerOrReadOnly,)
 
+class StoreList(generics.ListAPIView):
+    queryset = Store.objects.all()
+    serializer_class = StoreSerializer
+
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
     # permission_classes = (permissions.IsAuthenticatedOrReadOnly,
     #                       IsOwnerOrReadOnly,)
+
+class ProductList(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
 
 class ContentViewSet(viewsets.ModelViewSet):
     queryset = Content.objects.all()
@@ -33,11 +41,19 @@ class ContentViewSet(viewsets.ModelViewSet):
     # permission_classes = (permissions.IsAuthenticatedOrReadOnly,
     #                       IsOwnerOrReadOnly,)
 
+class ContentList(generics.ListAPIView):
+    queryset = Content.objects.all()
+    serializer_class = ContentSerializer
+
 class ImageViewSet(viewsets.ModelViewSet):
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
     # permission_classes = (permissions.IsAuthenticatedOrReadOnly,
     #                       IsOwnerOrReadOnly,)
+
+class ImageList(generics.ListAPIView):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
 
 class GifViewSet(viewsets.ModelViewSet):
     queryset = Gif.objects.all()
@@ -45,11 +61,19 @@ class GifViewSet(viewsets.ModelViewSet):
     # permission_classes = (permissions.IsAuthenticatedOrReadOnly,
     #                       IsOwnerOrReadOnly,)
 
+class GifList(generics.ListAPIView):
+    queryset = Gif.objects.all()
+    serializer_class = GifSerializer
+
 class ProductImageViewSet(viewsets.ModelViewSet):
     queryset = ProductImage.objects.all()
     serializer_class = ProductImageSerializer
     # permission_classes = (permissions.IsAuthenticatedOrReadOnly,
     #                       IsOwnerOrReadOnly,)
+
+class ProductImageList(generics.ListAPIView):
+    queryset = ProductImage.objects.all()
+    serializer_class = ProductImageSerializer
 
 class VideoViewSet(viewsets.ModelViewSet):
     queryset = Video.objects.all()
@@ -57,17 +81,29 @@ class VideoViewSet(viewsets.ModelViewSet):
     # permission_classes = (permissions.IsAuthenticatedOrReadOnly,
     #                       IsOwnerOrReadOnly,)
 
+class VideoList(generics.ListAPIView):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+
 class PageViewSet(viewsets.ModelViewSet):
     queryset = Page.objects.all()
     serializer_class = PageSerializer
     # permission_classes = (permissions.IsAuthenticatedOrReadOnly,
     #                       IsOwnerOrReadOnly,)
 
+class PageList(generics.ListAPIView):
+    queryset = Page.objects.all()
+    serializer_class = PageSerializer
+
 class TileViewSet(viewsets.ModelViewSet):
     queryset = Tile.objects.all()
     serializer_class = TileSerializer
     # permission_classes = (permissions.IsAuthenticatedOrReadOnly,
     #                       IsOwnerOrReadOnly,)
+
+class TileList(generics.ListAPIView):
+    queryset = Tile.objects.all()
+    serializer_class = TileSerializer
 
 # class TileConfigViewSet(viewsets.ModelViewSet):
 #     queryset = TileConfig.objects.all()
@@ -79,11 +115,12 @@ class TileViewSet(viewsets.ModelViewSet):
 def api_root(request, format=None):
     return Response({
         'store': reverse('store-list', request=request, format=format),
-        'snippets': reverse('product-list', request=request, format=format),
+        'product': reverse('product-list', request=request, format=format),
         'content': reverse('content-list', request=request, format=format),
         'image': reverse('image-list', request=request, format=format),
         'gif': reverse('gif-list', request=request, format=format),
         'productimage': reverse('productimage-list', request=request, format=format),
+        'video': reverse('video-list', request=request, format=format),
         'page': reverse('page-list', request=request, format=format),
         'tile': reverse('tile-list', request=request, format=format)
     })
