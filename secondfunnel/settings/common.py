991487ae28e052cd155a665fc661c206b64f6185
Nhat Le <nale@uwaterloo.ca>
2016-01-25 16:33:24 -0500

    Arranged template dir alphabetically

--- secondfunnel/settings/common.py
+++ secondfunnel/settings/common.py
@@ -287,10 +287,10 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    from_project_root('apps/scrapy/templates'),
     from_project_root('apps/dashboard/templates'),
     from_project_root('apps/intentrank/templates'),
     from_project_root('apps/light/templates'),
+    from_project_root('apps/scrapy/templates'),
 )
 
 FRAMEWORK_APPS = (
