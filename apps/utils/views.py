0a144baeef42e4b0c18364ffaebb1f583521ff1b
Fraser Harris <fraser.harris@gmail.com>
2016-03-29 13:32:59 -0700

    Reduced workload of the healthcheck

--- apps/utils/views.py
+++ apps/utils/views.py
@@ -12,22 +12,10 @@ def healthcheck(request):
     serving from this instance. If this isn't the case, the
     provisioning failed.
 
-    Strategy: open rev-manifest.json and see if the files
-    in it exist in the static directory
+    Strategy: check rev-manifest.json exists in the static directory
     """
-    #if settings.ENVIRONMENT == "production":
-        # relative path is from whereever manage.py is being run
-    try:
-        with open("./apps/light/static/light/rev-manifest.json") as f:
-            manifest = json.load(f)
-            manifest_collected = [os.path.isfile(reved_file) for reved_file in manifest.values()]
-            if False in manifest_collected:
-                return HttpResponse(status=500,
-                                    reason=u"{} out of {} revisioned files not collected".format(
-                                        manifest_collected.count(False), len(manifest)),
-                                    content="SERVER ERROR")
-    except IOError:
+    # relative path is from whereever manage.py is being run
+    if not os.path.isfile("./apps/light/static/light/rev-manifest.json"):
         return HttpResponse(status=500, reason=u"Couldn't open revision manifest", content="SERVER ERROR")
-                
-    # stage or dev
-    return HttpResponse(status=200, content="OK")
+    else:
+        return HttpResponse(status=200, content="OK")
